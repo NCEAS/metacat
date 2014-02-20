b4ad840da93d163c65f39018cd20bf5dd848e784
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2014-02-20 21:37:43 +0000

    add test for invalid dryad content -- should be rejected because it is not schema valid.

--- test/edu/ucsb/nceas/metacattest/DryadTest.java
+++ test/edu/ucsb/nceas/metacattest/DryadTest.java
@@ -32,6 +32,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.apache.commons.io.IOUtils;
+import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.ObjectFormatIdentifier;
 import org.dataone.service.types.v1.Session;
@@ -47,6 +48,8 @@ public class DryadTest
     extends D1NodeServiceTest {
     
     private static final String DRYAD_TEST_DOC = "test/dryad-metadata-profile-sample.xml";
+    
+    private static final String DRYAD_TEST_DOC_INVALID = "test/dryad-metadata-profile-invalid.xml";
 
     /**
      * Constructor to build the test
@@ -65,6 +68,7 @@ public class DryadTest
         suite.addTest(new DryadTest("initialize"));
         // Test basic functions
         suite.addTest(new DryadTest("d1InsertDoc"));
+        suite.addTest(new DryadTest("d1InsertInvalidDoc"));
 
         return suite;
     }
@@ -96,6 +100,37 @@ public class DryadTest
 			InputStream results = MNodeService.getInstance(request).get(session, pid);
 			String resultString = IOUtils.toString(results);
 			assertEquals(documentContents, resultString);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+    	
+    }
+    
+    /**
+     * Insert test doc using D1 API
+     */
+    public void d1InsertInvalidDoc() {
+		try {
+	    	String docid = this.generateDocumentId();
+			String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC_INVALID);
+			Session session = getTestSession();
+			Identifier pid = new Identifier();
+			pid.setValue(docid);
+			SystemMetadata sysmeta = this.createSystemMetadata(pid, session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
+			ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
+			formatId.setValue("http://datadryad.org/profile/v3.1");
+			sysmeta.setFormatId(formatId);
+			sysmeta.setSize(BigInteger.valueOf(IOUtils.toByteArray(documentContents).length));
+			try {
+				MNodeService.getInstance(request).create(session, pid, IOUtils.toInputStream(documentContents, "UTF-8"), sysmeta);
+			} catch (Exception expectedException) {
+
+				// expected exception
+				assertTrue(expectedException instanceof ServiceFailure);
+				return;
+			}
+			fail("Should not allow inserting invalid Dryad content");
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
