ee42c41bbed9348700bcd9ee5df395fefb0c1695
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2014-04-02 22:40:09 +0000

    add "test" for generating annotations based on the entity/attribute details of a datapackage. This iterates through all current EML revisions and either updates or creates annotations based on what it finds. It does add content to your metacat deployment (RDF files) but it can be safely re-run when each time we change our annotation algorithm.

--- test/edu/ucsb/nceas/metacat/annotation/DatapackageSummarizerTest.java
+++ test/edu/ucsb/nceas/metacat/annotation/DatapackageSummarizerTest.java
@@ -20,17 +20,32 @@ package edu.ucsb.nceas.metacat.annotation;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.apache.commons.io.IOUtils;
+import org.dataone.service.exceptions.NotFound;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.ObjectFormatIdentifier;
 import org.dataone.service.types.v1.Session;
 import org.dataone.service.types.v1.SystemMetadata;
 
+import edu.ucsb.nceas.metacat.DBUtil;
+import edu.ucsb.nceas.metacat.DocumentImpl;
+import edu.ucsb.nceas.metacat.IdentifierManager;
+import edu.ucsb.nceas.metacat.McdbDocNotFoundException;
 import edu.ucsb.nceas.metacat.dataone.D1NodeServiceTest;
 import edu.ucsb.nceas.metacat.dataone.MNodeService;
+import edu.ucsb.nceas.metacat.replication.ReplicationService;
+import edu.ucsb.nceas.metacat.util.DocumentUtil;
 
 public class DatapackageSummarizerTest extends D1NodeServiceTest {
 
@@ -62,7 +77,8 @@ public class DatapackageSummarizerTest extends D1NodeServiceTest {
 	 */
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		suite.addTest(new DatapackageSummarizerTest("testGenerateAnnotation"));
+		suite.addTest(new DatapackageSummarizerTest("testGenerateAnnotations"));
+//		suite.addTest(new DatapackageSummarizerTest("testGenerateAnnotation"));
 //		suite.addTest(new DatapackageSummarizerTest("testGenerateRandomAnnotation"));
 		return suite;
 	}
@@ -124,6 +140,100 @@ public class DatapackageSummarizerTest extends D1NodeServiceTest {
 		}
 		
 		// check that it was parsed?
+	}
+	
+	public void testGenerateAnnotations() throws Exception {
+		
+		// summarize the packages
+		DatapackageSummarizer ds = new DatapackageSummarizer();
+		List<Identifier> identifiers = new ArrayList<Identifier>();
+		Map<Integer, String> serverCodes = ReplicationService.getServerCodes();
+
+		// select the metadata ids we want to summarize
+		boolean includeReplicas = false;
+		Iterator<Integer> codeIter = Arrays.asList(new Integer[] {1}).iterator();
+		if (includeReplicas ) {
+			codeIter = serverCodes.keySet().iterator();
+		}
+		
+		Vector<String> idList = new Vector<String>();
+		while (codeIter.hasNext()) {
+			int serverLocation = codeIter.next();
+			Vector<String> idList0 = DBUtil.getAllDocidsByType(DocumentImpl.EML2_0_0NAMESPACE, false, serverLocation);
+			idList.addAll(idList0);
+			Vector<String> idList1 = DBUtil.getAllDocidsByType(DocumentImpl.EML2_0_1NAMESPACE, false, serverLocation);
+			idList.addAll(idList1);
+			Vector<String> idList2 = DBUtil.getAllDocidsByType(DocumentImpl.EML2_1_0NAMESPACE, false, serverLocation);
+			idList.addAll(idList2);
+			Vector<String> idList3 = DBUtil.getAllDocidsByType(DocumentImpl.EML2_1_1NAMESPACE, false, serverLocation);
+			idList.addAll(idList3);
+		
+		}
+		
+		// go through all the identifiers now
+		for (String localId : idList) {
+			try {
+				String guid = IdentifierManager.getInstance().getGUID(
+						DocumentUtil.getDocIdFromAccessionNumber(localId), 
+						DocumentUtil.getRevisionFromAccessionNumber(localId));
+				Identifier pid = new Identifier();
+				pid.setValue(guid);
+				identifiers.add(pid);
+				
+				String annotation = ds.generateAnnotation(pid);
+				Identifier annotationPid = new Identifier();
+				annotationPid.setValue("http://annotation/" + guid);
+				Session session = getTestSession();
+				
+				SystemMetadata sysmeta = null;
+				// look for the latest version of the annotation, if there is one
+				do {
+					try {
+						sysmeta = MNodeService.getInstance(request).getSystemMetadata(annotationPid);
+						if (sysmeta.getObsoletedBy() != null) {
+							annotationPid.setValue(sysmeta.getObsoletedBy().getValue());
+						}
+					} catch (NotFound nf) {
+						break;
+					}
+				} while (sysmeta != null && sysmeta.getObsoletedBy() != null);
+
+				boolean exists = (sysmeta != null);
+				
+				InputStream object = null;
+				object = IOUtils.toInputStream(annotation, "UTF-8");
+				sysmeta = createSystemMetadata(annotationPid, session.getSubject(), object);
+				ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
+				formatId.setValue("http://www.w3.org/TR/rdf-syntax-grammar");
+				sysmeta.setFormatId(formatId);
+				sysmeta.setSize(BigInteger.valueOf(annotation.getBytes("UTF-8").length));
+				
+				// get the stream fresh for inserting/updating
+				object = IOUtils.toInputStream(annotation, "UTF-8");
+
+				if (!exists) {
+					MNodeService.getInstance(request).create(session, annotationPid, object, sysmeta);
+				} else {
+					Identifier newAnnotationPid = new Identifier();
+					// use an old-style revision scheme for updating the annotation identifier
+					String value = annotationPid.getValue();
+					int rev = DocumentUtil.getRevisionFromAccessionNumber(value);
+					String partialId = DocumentUtil.getSmartDocId(value);
+					rev++;
+					newAnnotationPid.setValue(partialId + "." + rev);
+					sysmeta.setIdentifier(newAnnotationPid);
+					sysmeta.setObsoletes(annotationPid);
+					MNodeService.getInstance(request).update(session, annotationPid, object, newAnnotationPid, sysmeta);
+				}
+				
+				System.out.println("Generated annotation for pid: " + guid);
+				
+			} catch (McdbDocNotFoundException nfe) {
+				// just skip it
+				continue;
+			}
+		}
+		//System.exit(0);
 	}
 
 }
