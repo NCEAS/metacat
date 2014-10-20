943a4a2d0312d165f856d7c70f3d713cb6015488
Jing Tao <tao@nceas.ucsb.edu>
2014-10-20 16:42:45 +0000

    Used variables to replace the hard code.

--- src/scripts/bash/install-java7-tomcat7.sh
+++ src/scripts/bash/install-java7-tomcat7.sh
@@ -5,28 +5,34 @@
 #It modifies the workers.properties file for apache-tomcat connector.
 #The user running the script should have the sudo permission.
 
+NEW_JDK_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+NEW_CATALINA_PROPERTIES=/etc/tomcat7/catalina.properties
+JK_CONF=/etc/apache2/mods-enabled/jk.conf
+NEW_TOMCAT_HOME=/usr/share/tomcat7
+NEW_TOMCAT_BASE=/var/lib/tomcat7
+
 echo "install openjdk-7-jdk"
 sudo apt-get install openjdk-7-jdk
 sleep 3
 echo "configure java, java, keytool and javaws"
-sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-7-openjdk-amd64/bin/javac
-sudo update-alternatives --set keytool /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/keytool
-sudo update-alternatives --set javaws /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/javaws
+sudo update-alternatives --set java ${NEW_JDK_HOME}/jre/bin/java
+sudo update-alternatives --set javac ${NEW_JDK_HOME}/bin/javac
+sudo update-alternatives --set keytool ${NEW_JDK_HOME}/jre/bin/keytool
+sudo update-alternatives --set javaws ${NEW_JDK_HOME}/jre/bin/javaws
 
 echo "install tomcat7"
 sudo apt-get install tomcat7
-sudo sed -i.bak '$ a\org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true' /etc/tomcat7/catalina.properties 
-sudo sed -i '$ a\org.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true' /etc/tomcat7/catalina.properties
+sudo sed -i.bak '$ a\org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true' ${NEW_CATALINA_PROPERTIES} 
+sudo sed -i '$ a\org.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true' ${NEW_CATALINA_PROPERTIES}
 
 echo "read the location of the workers.properties file from the jk_conf"
 while read f1 f2 
 do
         if [ "$f1" = "JkWorkersFile" ]; then
-		JKWORKERPATH="$f2"	
+		JK_WORKER_PATH="$f2"	
 	fi
-done < /etc/apache2/mods-enabled/jk.conf
-echo "the jk workers.properties location is $JKWORKERPATH"
+done < ${JK_CONF}
+echo "the jk workers.properties location is $JK_WORKER_PATH"
 
 echo "update the tomcat home and java home in workers.properties file"
 sudo sed -i.bak --regexp-extended "s/(workers\.tomcat_home=).*/\1\/usr\/share\/tomcat7/;
