4e4b77ed00e16e6f2789a6f35d0ca59e04a2f0ed
Jing Tao <tao@nceas.ucsb.edu>
2014-10-22 02:28:07 +0000

    Fixed a typo.

--- src/scripts/bash/vacuum-db.sh
+++ src/scripts/bash/vacuum-db.sh
@@ -5,5 +5,5 @@ DB_VERSION=9.3
 echo "start to vacuum the db at" >> /tmp/vacuumdb.out
 echo `date` >> /tmp/vacuumdb.out
 su - postgres /usr/lib/postgresql/$DB_VERSION/bin/vacuumdb --all >> /tmp/vacuumdb.out 2> /tmp/vacuumdb.err < /dev/null
-echo "start to vacuum the db at " >> /tmp/vacuumdb.out
+echo "end to vacuum the db at " >> /tmp/vacuumdb.out
 echo `date` >>  /tmp/vacuumdb.out
