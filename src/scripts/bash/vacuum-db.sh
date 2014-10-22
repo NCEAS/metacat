a640e10a33a2ab9dd0c9dbfba9d4e86479414a43
Jing Tao <tao@nceas.ucsb.edu>
2014-10-22 02:26:42 +0000

    put the date to an output file.

--- src/scripts/bash/vacuum-db.sh
+++ src/scripts/bash/vacuum-db.sh
@@ -3,7 +3,7 @@
 #Usage: nohup ./
 DB_VERSION=9.3
 echo "start to vacuum the db at" >> /tmp/vacuumdb.out
-echo `date`
+echo `date` >> /tmp/vacuumdb.out
 su - postgres /usr/lib/postgresql/$DB_VERSION/bin/vacuumdb --all >> /tmp/vacuumdb.out 2> /tmp/vacuumdb.err < /dev/null
 echo "start to vacuum the db at " >> /tmp/vacuumdb.out
 echo `date` >>  /tmp/vacuumdb.out
