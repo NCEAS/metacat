251e0e8412988e392ef5c03a5d94dc7ec678f962
Jing Tao <tao@nceas.ucsb.edu>
2014-11-18 00:30:03 +0000

    Add delete log for data objects on CNs.

--- src/edu/ucsb/nceas/metacat/dataone/D1NodeService.java
+++ src/edu/ucsb/nceas/metacat/dataone/D1NodeService.java
@@ -729,6 +729,8 @@ public abstract class D1NodeService {
             
             if(localId != null && EventLog.getInstance().isDeleted(localId)) {
                 error = DELETEDMESSAGE;
+            } else if (localId == null && EventLog.getInstance().isDeleted(pid.getValue())) {
+                error = DELETEDMESSAGE;
             }
             throw new NotFound("1420", "No record found for: " + pid.getValue()+". "+error);
         }
@@ -1016,6 +1018,8 @@ public abstract class D1NodeService {
         }
         
         if(localId != null && EventLog.getInstance().isDeleted(localId)) {
+            error = error + ". "+DELETEDMESSAGE;
+        } else if (localId == null && EventLog.getInstance().isDeleted(pid.getValue())) {
             error = error + ". "+DELETEDMESSAGE;
         }
         throw new NotFound("1800", error);
