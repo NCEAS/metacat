2c87c2206fddc68992e68ba2b5620dc4f78ac4d2
Jing Tao <tao@nceas.ucsb.edu>
2014-11-20 05:08:53 +0000

    Adjust the size of the schema fields.

--- metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
+++ metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
@@ -48,7 +48,7 @@ public class SolrQueryServiceControllerTest extends MetacatCommonTestBase {
     public void testgetIndexSchemaFields() throws Exception {
        Map<String, SchemaField> fields = SolrQueryServiceController.getInstance().getIndexSchemaFields();
        assertTrue(fields != null);
-       assertTrue("The number of index schema fields should be 102 rather than "+fields.size(), fields.size() == 102);
+       assertTrue("The number of index schema fields should be 111 rather than "+fields.size(), fields.size() == 111);
     }
     
     /**
