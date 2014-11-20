b7d4634626f1a930c22a6d384fbbf89ce3e50394
Chris Jones <cjones@nceas.ucsb.edu>
2014-11-20 17:21:23 +0000

    Merge changes in the 2.4 branch into the trunk for the ORNL Mercury schema support.  References https://projects.ecoinformatics.org/ecoinfo/issues/6619.
    This schema is maintained by Jim Green (jgreen@iiaweb.com) and Ranjeet Devarakonda (devarakondar@ornl.gov). The schema uses a subset of the FGDC 1998 CSDGM elements, along with custom elements specific to the Mercury Metadata web application.  Support is included in Metacat for Coordinating Node operations in the DataONE network.

--- src/loaddtdschema-oracle.sql
+++ src/loaddtdschema-oracle.sql
@@ -119,6 +119,8 @@ INSERT INTO xml_catalog (entry_type, public_id, system_id)
   VALUES ('Schema', 'http://purl.org/dc/dcmitype/', '/schema/dc/dcmitype.xsd');
 INSERT INTO xml_catalog (entry_type, public_id, system_id)
   VALUES ('Schema', 'http://rs.tdwg.org/dwc/terms/', '/schema/dwc/tdwg_dwcterms.xsd');
+INSERT INTO xml_catalog (entry_type, public_id, system_id)
+  VALUES ('Schema', 'http://purl.org/ornl/schema/mercury/terms/v1.0', '/schema/ornl/ornl-mercury-v1.0.xsd');
 
 INSERT INTO db_version (version, status, date_created) 
-  VALUES ('2.5.0',1,CURRENT_DATE);
+  VALUES ('2.4.2',1,CURRENT_DATE);
