5bc7548edce6426614697af26832a933deaa5b9b
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2014-12-12 21:30:06 +0000

    use input stream instead of Document for resource map processing test

--- metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/resourcemap/ResourceMapSubprocessorIT.java
+++ metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/resourcemap/ResourceMapSubprocessorIT.java
@@ -4,20 +4,16 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.dataone.cn.indexer.parser.IDocumentSubprocessor;
 import org.dataone.cn.indexer.solrhttp.SolrDoc;
 import org.dataone.cn.indexer.solrhttp.SolrElementField;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import edu.ucsb.nceas.metacat.index.SolrIndex;
 import edu.ucsb.nceas.metacat.index.SolrIndexIT;
@@ -32,17 +28,14 @@ public class ResourceMapSubprocessorIT {
     public void testProcessDocument() throws Exception {
         //Settings.getConfiguration().setProperty("dataone.hazelcast.configFilePath", "../lib/hazelcast.xml");
         String id = "resourceMap_urn:uuid:04780847-6082-455b-9831-22269c9ec0a5";
-        Document doc = getResourceMapDoc();
-        System.out.println("the lenght of child is "+doc.getChildNodes().getLength());
-        System.out.println("the name of the childs is "+doc.getChildNodes().item(0).getLocalName());
-        Node child = doc.getChildNodes().item(0);
+        InputStream is = getResourceMapDoc();
         List<SolrElementField> sysSolrFields = new ArrayList<SolrElementField>();
         SolrDoc indexDocument = new SolrDoc(sysSolrFields);
         Map<String, SolrDoc> docs = new HashMap<String, SolrDoc>();
         docs.put(id, indexDocument);
         ResourceMapSubprocessor processor = getResourceMapSubprocessor();
         try {
-            processor.processDocument(id, docs, doc);
+            processor.processDocument(id, docs, is);
             assertTrue("ResourceMapProcessor should throw an exception because the resource map has a component which doesn't exist.", false);
         } catch (Exception e) {
             assertTrue("ResourceMapProcessor should throw a ResourceMapException because the resource map has a component which doesn't exist.", e instanceof ResourceMapException);
@@ -53,13 +46,10 @@ public class ResourceMapSubprocessorIT {
     /*
      * Get the document format of the test resource map file 
      */
-    private Document getResourceMapDoc() throws Exception{
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        documentBuilderFactory.setNamespaceAware(true);
-        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-        File file = new File("src/test/resources/resorcemap-example");
-        Document doc = builder.parse(new FileInputStream(file));
-        return doc;
+    private InputStream getResourceMapDoc() throws Exception{
+    	File file = new File("src/test/resources/resorcemap-example");
+        InputStream is = new FileInputStream(file);
+        return is;
     }
     
     /*
