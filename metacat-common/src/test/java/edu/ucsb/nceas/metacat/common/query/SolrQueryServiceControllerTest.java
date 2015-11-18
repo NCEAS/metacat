b6074a7b8251c4a08ec658cbd927b3600bcacfbb
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-11-18 23:46:59 +0000

    added 4 new schema fields so need to account for them in the test case.

--- metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
+++ metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
@@ -48,7 +48,7 @@ public class SolrQueryServiceControllerTest extends MetacatCommonTestBase {
     public void testgetIndexSchemaFields() throws Exception {
        Map<String, SchemaField> fields = SolrQueryServiceController.getInstance().getIndexSchemaFields();
        assertTrue(fields != null);
-       assertTrue("The number of index schema fields should be 130 rather than "+fields.size(), fields.size() == 130);
+       assertTrue("The number of index schema fields should be 130 rather than "+fields.size(), fields.size() == 134);
     }
     
     /**
