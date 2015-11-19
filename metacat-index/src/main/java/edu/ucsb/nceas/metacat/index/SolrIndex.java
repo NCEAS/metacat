37681f47537f4cd3364971d7639bb3d3e98424cc
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-11-19 19:44:01 +0000

    add checks on archived flag to avoid NPE.

--- metacat-index/src/main/java/edu/ucsb/nceas/metacat/index/SolrIndex.java
+++ metacat-index/src/main/java/edu/ucsb/nceas/metacat/index/SolrIndex.java
@@ -539,7 +539,7 @@ public class SolrIndex {
         }
         String objectPath = null;
         try {
-            if(!systemMetadata.getArchived()) {
+            if (systemMetadata.getArchived() == null || !systemMetadata.getArchived()) {
                 objectPath = DistributedMapsFactory.getObjectPathMap().get(pid);
             }
             update(pid, systemMetadata, objectPath);
@@ -1064,7 +1064,7 @@ public class SolrIndex {
             action = Event.CREATE.xmlValue();
             event.setAction(Event.CREATE);
         }
-        else if(systemMetadata.getArchived()) {
+        else if(systemMetadata.getArchived() != null && systemMetadata.getArchived()) {
             action = Event.DELETE.xmlValue();
             event.setAction(Event.DELETE);
         } else {
