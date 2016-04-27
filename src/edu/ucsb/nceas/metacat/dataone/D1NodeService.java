9ea2136f624b4c3348c6328e9620eb4a70777cee
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2016-04-27 18:56:23 +0000

    merge from 2.6 branch: use group subject (not name) when translating Session.subjectInfo into group list for Metacat's DocumentImpl. https://github.nceas.ucsb.edu/KNB/arctic-data/issues/163

--- src/edu/ucsb/nceas/metacat/dataone/D1NodeService.java
+++ src/edu/ucsb/nceas/metacat/dataone/D1NodeService.java
@@ -1372,7 +1372,7 @@ public abstract class D1NodeService {
     		if (groupList != null) {
     			groupnames = new String[groupList.size()];
     			for (int i = 0; i < groupList.size(); i++ ) {
-    				groupnames[i] = groupList.get(i).getGroupName();
+    				groupnames[i] = groupList.get(i).getSubject().getValue();
     			}
     		}
     	}
@@ -1414,7 +1414,7 @@ public abstract class D1NodeService {
     		if (groupList != null) {
     			groupnames = new String[groupList.size()];
     			for (int i = 0; i < groupList.size(); i++ ) {
-    				groupnames[i] = groupList.get(i).getGroupName();
+    				groupnames[i] = groupList.get(i).getSubject().getValue();
     			}
     		}
     	}
