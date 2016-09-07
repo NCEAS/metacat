3c8c49f0628243c28ceae54e8723fa9adcd1e6d3
Jing Tao <tao@nceas.ucsb.edu>
2016-09-07 18:07:40 +0000

    Add the code to add a new column in the xml_catalog table. Also register some new format ids for no_namespace schema.

--- src/upgrade-db-to-2.7.2-postgres.sql
+++ src/upgrade-db-to-2.7.2-postgres.sql
@@ -4,6 +4,23 @@
  */
 SELECT setval('xml_catalog_id_seq', (SELECT max(catalog_id) from xml_catalog));
 
+/*
+ * Add a new column no_namespace_schema_location in xml_catalog table
+ */
+ALTER TABLE xml_catalog ADD COLUMN no_namespace_schema_location VARCHAR(2000);
+
+/*
+ * Add a formatid indicates it is a fgdc document without namespace
+ */
+INSERT INTO xml_catalog (entry_type, format_id, system_id) VALUES ('Schema', 'FGDC-STD-001-1998', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
+
+/*
+ * Add the columns with the no_namespace_schema_location referring the fgdc documents
+ */
+INSERT INTO xml_catalog (entry_type, no_namespace_schema_location, system_id) VALUES ('Schema', 'https://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
+INSERT INTO xml_catalog (entry_type, no_namespace_schema_location, system_id) VALUES ('Schema', 'http://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
+INSERT INTO xml_catalog (entry_type, no_namespace_schema_location, system_id) VALUES ('Schema', 'https://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
+INSERT INTO xml_catalog (entry_type, no_namespace_schema_location, system_id) VALUES ('Schema', 'http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
 
 /*
  * update the database version
