ed84e39c2c814e083f9ecdbcba505a4463b2564e
Jing Tao <tao@nceas.ucsb.edu>
2016-09-08 23:38:48 +0000

    Change the type for the no-name-namespace schema from Schema to NoNamespaceSchema.

--- src/upgrade-db-to-2.7.2-postgres.sql
+++ src/upgrade-db-to-2.7.2-postgres.sql
@@ -13,10 +13,10 @@ ALTER TABLE xml_catalog ADD COLUMN no_namespace_schema_location VARCHAR(2000);
 /*
  * Add the columns with the no_namespace_schema_location and format id referring the fgdc documents
  */
-INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('Schema', 'FGDC-STD-001-1998', 'https://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
-INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('Schema', 'FGDC-STD-001-1998', 'http://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
-INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('Schema', 'FGDC-STD-001-1998', 'https://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
-INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('Schema', 'FGDC-STD-001-1998', 'http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
+INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('NoNamespaceSchema', 'FGDC-STD-001-1998', 'https://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
+INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('NoNamespaceSchema', 'FGDC-STD-001-1998', 'http://www.fgdc.gov/schemas/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
+INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('NoNamespaceSchema', 'FGDC-STD-001-1998', 'https://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd'); 
+INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) VALUES ('NoNamespaceSchema', 'FGDC-STD-001-1998', 'http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd', '/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd');
 
 /*
  * update the database version
