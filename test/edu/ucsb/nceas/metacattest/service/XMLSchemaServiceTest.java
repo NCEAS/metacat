78b4e64fffe1a3053c0179004b5615e9e57f6f3f
Jing Tao <tao@nceas.ucsb.edu>
2016-09-09 22:15:16 +0000

    Add the test methods for methods which can figure out the schema location.

--- test/edu/ucsb/nceas/metacattest/service/XMLSchemaServiceTest.java
+++ test/edu/ucsb/nceas/metacattest/service/XMLSchemaServiceTest.java
@@ -1,7 +1,12 @@
 package edu.ucsb.nceas.metacattest.service;
 
+import org.apache.commons.io.FileUtils;
+import edu.ucsb.nceas.MCTestCase;
 import edu.ucsb.nceas.metacat.service.XMLSchemaParser;
 import edu.ucsb.nceas.metacat.service.XMLSchemaService;
+
+import java.io.File;
+import java.io.StringReader;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -13,9 +18,13 @@ import junit.framework.TestSuite;
  * @author tao
  *
  */
-public class XMLSchemaServiceTest extends TestCase
+public class XMLSchemaServiceTest extends MCTestCase
 {
-  
+    public static String withPrefix = "<eml:eml packageId=\"eml.2.1\" system=\"knb2\" xmlns:eml=\"eml://ecoinformatics.org/eml-2.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:stmml=\"http://www.xml-cml.org/schema/stmml\" xsi:schemaLocation=\"eml://ecoinformatics.org/eml-2.0.1 eml.xsd\"><tile>title</title></eml>";
+    public static String withCommentAndPrefix="<!-- comment -->  <!-- comment -->"+withPrefix;              
+    public static String defaultNamespace = "<root xmlns=\"eml://ecoinformatics.org/eml-2.1.1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:stmml=\"http://www.xml-cml.org/schema/stmml\" xsi:schemaLocation=\"eml://ecoinformatics.org/eml-2.0.1 eml.xsd\"><title>title1</title></root>";
+    public static String noNamespace = "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:stmml=\"http://www.xml-cml.org/schema/stmml\" xsi:schemaLocation=\"eml://ecoinformatics.org/eml-2.0.1 eml.xsd\"><title>title1</title></root>";
+    
   /**
    * Constructor to build the test
    *
@@ -49,6 +58,11 @@ public class XMLSchemaServiceTest extends TestCase
     TestSuite suite = new TestSuite();
     suite.addTest(new XMLSchemaServiceTest("initialize"));
     suite.addTest(new XMLSchemaServiceTest("testGetBaseUrlFromSchemaURL"));
+    suite.addTest(new XMLSchemaServiceTest("testFindNamespaceInDocuments"));
+    suite.addTest(new XMLSchemaServiceTest("testFindNamespaceAndSchemaLocalLocation"));
+    suite.addTest(new XMLSchemaServiceTest("testFindNoNamespaceSchemaLocalLocation"));
+    suite.addTest(new XMLSchemaServiceTest("testIsNamespaceRegistered"));
+    suite.addTest(new XMLSchemaServiceTest("testFindNoNamespaceSchemaLocationAttr"));
     return suite;
   }
 
@@ -73,5 +87,129 @@ public class XMLSchemaServiceTest extends TestCase
       url="http://www.example.com/metacat/";
       baseURL = XMLSchemaService.getBaseUrlFromSchemaURL(url);
       assertTrue("The base url should be "+url, baseURL.equals(url));
+  }
+  
+  public void testFindNamespaceInDocuments() throws Exception{
+      StringReader reader = new StringReader(withPrefix);
+      String namespace = XMLSchemaService.findDocumentNamespace(reader);
+      assertTrue(namespace.equals("eml://ecoinformatics.org/eml-2.0.0"));
+      
+      reader = new StringReader(withCommentAndPrefix);
+      namespace = XMLSchemaService.findDocumentNamespace(reader);
+      assertTrue(namespace.equals("eml://ecoinformatics.org/eml-2.0.0"));
+      
+      File file = new File("./test/eml-sample.xml");
+      reader = new StringReader(FileUtils.readFileToString(file));
+      namespace = XMLSchemaService.findDocumentNamespace(reader);
+      assertTrue(namespace.equals("eml://ecoinformatics.org/eml-2.0.1"));
+      
+      reader = new StringReader(defaultNamespace);
+      String namespace2 = XMLSchemaService.findDocumentNamespace(reader);
+      assertTrue(namespace2.equals("eml://ecoinformatics.org/eml-2.1.1"));
+      
+      reader = new StringReader(noNamespace);
+      namespace = XMLSchemaService.findDocumentNamespace(reader);
+      assertTrue(namespace == null);
+  }
+  
+  public void testFindNamespaceAndSchemaLocalLocation() throws Exception {
+      String formatId = null;
+      String namespace = null;
+      String location = XMLSchemaService.getInstance().findNamespaceAndSchemaLocalLocation(formatId, namespace);
+      assertTrue(location == null);
+      
+      //registered formatid
+      formatId = "http://www.isotc211.org/2005/gmd-noaa";
+      namespace = null;
+      location = XMLSchemaService.getInstance().findNamespaceAndSchemaLocalLocation(formatId, namespace);
+      assertTrue(location.contains("/schema/isotc211-noaa/gmd/gmd.xsd"));
+      
+    //registered formatid
+      formatId = "http://www.isotc211.org/2005/gmd-noaa";
+      namespace = "http://www.isotc211.org/2005/gmd";
+      location = XMLSchemaService.getInstance().findNamespaceAndSchemaLocalLocation(formatId, namespace);
+      assertTrue(location.contains("/schema/isotc211-noaa/gmd/gmd.xsd"));
+      
+      //unregistered formatid
+      formatId = "eml://ecoinformatics.org/eml-2.1.0";
+      namespace = "eml://ecoinformatics.org/eml-2.1.0";
+      location = XMLSchemaService.getInstance().findNamespaceAndSchemaLocalLocation(formatId, namespace);
+      assertTrue(location.contains("/schema/eml-2.1.0/eml.xsd "));
+      
+      //unregistered formatid
+      formatId = "http://www.isotc211.org/2005/gco";
+      namespace = "http://www.isotc211.org/2005/gco";
+      location = XMLSchemaService.getInstance().findNamespaceAndSchemaLocalLocation(formatId, namespace);
+      assertTrue(location.contains("/schema/isotc211/gco/gco.xsd"));
+      
+  }
+  
+  public void testFindNoNamespaceSchemaLocalLocation() throws Exception {
+      String formatId = null;
+      String nonamespaceURI = null;
+      String location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location == null);
+      
+      formatId = "FGDC-STD-001-1998";
+      nonamespaceURI = null;
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      formatId = "FGDC-STD-001-1998";
+      nonamespaceURI = "unkown";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      formatId = "FGDC-STD-001-1998";
+      nonamespaceURI = "https://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      formatId = "FGDC-STD-001-1998";
+      nonamespaceURI = "http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      
+      formatId = "FGDC-STD-001";
+      nonamespaceURI = "http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      formatId = "FGDC-STD-001";
+      nonamespaceURI = "https://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location.contains("/schema/fgdc-std-001-1998/fgdc-std-001-1998.xsd"));
+      
+      formatId = "FGDC-STD-001";
+      nonamespaceURI = "https://www.fgdc.gov/metadata-std-001-1998.xsd";
+      location = XMLSchemaService.getInstance().findNoNamespaceSchemaLocalLocation(formatId, nonamespaceURI);
+      assertTrue(location == null);
+  }
+  
+  public void testIsNamespaceRegistered() throws Exception {
+      String namespace = null;
+      boolean registered = XMLSchemaService.getInstance().isNamespaceRegistered(namespace);
+      assertTrue(registered == false);
+      
+      namespace = "http://foo";
+      registered = XMLSchemaService.getInstance().isNamespaceRegistered(namespace);
+      assertTrue(!registered);
+      
+      namespace = "eml://ecoinformatics.org/eml-2.0.1";
+      registered = XMLSchemaService.getInstance().isNamespaceRegistered(namespace);
+      assertTrue(registered);
+  }
+  
+  public void testFindNoNamespaceSchemaLocationAttr() throws Exception {
+      File file = new File("./test/eml-sample.xml");
+      StringReader reader = new StringReader(FileUtils.readFileToString(file));
+      String noNamespaceLocation = XMLSchemaService.findNoNamespaceSchemaLocationAttr(reader);
+      assertTrue(noNamespaceLocation == null);
+      
+      file = new File("./test/fgdc.xml");
+      reader = new StringReader(FileUtils.readFileToString(file));
+      noNamespaceLocation = XMLSchemaService.findNoNamespaceSchemaLocationAttr(reader);
+      assertTrue(noNamespaceLocation.equals("http://www.fgdc.gov/metadata/fgdc-std-001-1998.xsd"));
   }
 }
