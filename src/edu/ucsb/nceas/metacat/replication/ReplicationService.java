e000f81444a7812b0a1539c879129d3040cefd6a
Jing Tao <tao@nceas.ucsb.edu>
2016-09-10 04:37:43 +0000

    Removed a unused parameter - formatId from method writeReplication.

--- src/edu/ucsb/nceas/metacat/replication/ReplicationService.java
+++ src/edu/ucsb/nceas/metacat/replication/ReplicationService.java
@@ -733,12 +733,12 @@ public class ReplicationService extends BaseService {
 			}
 			logReplication.warn("ReplicationService.handleForceReplicateRequest - The parserBase is: " + parserBase);
 			
-			String formatId = null;
+			/*String formatId = null;
 		    //get the format id from the system metadata 
 		    if(sysMeta != null && sysMeta.getFormatId() != null) {
 		          logMetacat.debug("ReplicationService.handleForceReplicateRequest - the format id will be got from the system metadata for the object "+docid);
 		          formatId = sysMeta.getFormatId().getValue();
-		    }
+		    }*/
 		      
 			// Get DBConnection from pool
 			dbConn = DBConnectionPool
@@ -750,7 +750,7 @@ public class ReplicationService extends BaseService {
 			try {
 				wrapper.writeReplication(dbConn, xmldoc, xmlBytes, null, null,
 						dbaction, docid, null, null, homeServer, server, createdDate,
-						updatedDate, formatId);
+						updatedDate);
 			} finally {
 				if(sysMeta != null) {
 					// submit for indexing. When the doc writing process fails, the index process will fail as well. But this failure
