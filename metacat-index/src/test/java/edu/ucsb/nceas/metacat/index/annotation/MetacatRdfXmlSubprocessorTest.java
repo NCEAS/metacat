38561c747717865d9c066c99a6b6a86b304e0fc8
Jing Tao <tao@nceas.ucsb.edu>
2016-12-03 00:17:37 +0000

    Add the prefix "prov_" the solr fields.

--- metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/annotation/MetacatRdfXmlSubprocessorTest.java
+++ metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/annotation/MetacatRdfXmlSubprocessorTest.java
@@ -48,27 +48,27 @@ public class MetacatRdfXmlSubprocessorTest {
             for(String newId: ids) {
                 SolrDoc resultSolrDoc = result.get(newId);
                 if(newId.equals("urn:uuid:2c20432e-116a-4085-b8d6-abfc4b2dada2")){
-                    SolrElementField field1 = resultSolrDoc.getField("wasDerivedFrom");
+                    SolrElementField field1 = resultSolrDoc.getField("prov_wasDerivedFrom");
                     assertTrue(" the wasDerivedFrom value should be urn:uuid:621a115f-f1ed-4bef-bca5-f8741793a540. But the real value is "+field1.getValue(), field1.getValue().equals("urn:uuid:621a115f-f1ed-4bef-bca5-f8741793a540"));
-                    SolrElementField field2 = resultSolrDoc.getField("wasGeneratedBy");
+                    SolrElementField field2 = resultSolrDoc.getField("prov_wasGeneratedBy");
                     assertTrue("the wasGeneratedBy value should be urn:uuid:9ceeaeb3-6ef3-4b1e-bc4d-96e299fab3a4. But the real value is"+field2.getValue(), field2.getValue().equals("urn:uuid:9ceeaeb3-6ef3-4b1e-bc4d-96e299fab3a4"));
                 } else if (newId.equals("urn:uuid:9ceeaeb3-6ef3-4b1e-bc4d-96e299fab3a4")) {
-                    SolrElementField field1 = resultSolrDoc.getField("used");
+                    SolrElementField field1 = resultSolrDoc.getField("prov_used");
                     assertTrue(" the used value should be urn:uuid:621a115f-f1ed-4bef-bca5-f8741793a540. But the real value is "+field1.getValue(), field1.getValue().equals("urn:uuid:621a115f-f1ed-4bef-bca5-f8741793a540"));
                    
                 } else if (newId.equals("urn:uuid:621a115f-f1ed-4bef-bca5-f8741793a540")) {
-                    List<String>list = resultSolrDoc.getAllFieldValues("wasDerivedFrom");
+                    List<String>list = resultSolrDoc.getAllFieldValues("prov_wasDerivedFrom");
                     assertTrue("The list must contain urn:uuid:ee635a61-c930-444d-8214-db65178a1a47", list.contains("urn:uuid:ee635a61-c930-444d-8214-db65178a1a47"));
                     assertTrue("The list must contain urn:uuid:23495598-d50b-4317-b0e1-05a9d9e52632", list.contains("urn:uuid:23495598-d50b-4317-b0e1-05a9d9e52632"));
                     assertTrue("The list must contain urn:uuid:urn:uuid:672ba6c5-8812-4c05-a324-246af172c67a", list.contains("urn:uuid:672ba6c5-8812-4c05-a324-246af172c67a"));
                     assertTrue("The list must contain urn:uuid:e534b2ab-3a1b-44ed-8a99-ce54a0487aea", list.contains("urn:uuid:e534b2ab-3a1b-44ed-8a99-ce54a0487aea"));
-                    list = resultSolrDoc.getAllFieldValues("wasGeneratedBy");
+                    list = resultSolrDoc.getAllFieldValues("prov_wasGeneratedBy");
                     assertTrue("The list must contain urn:uuid:d8e46217-2650-42b2-896f-0f006b1a2d3b", list.contains("urn:uuid:d8e46217-2650-42b2-896f-0f006b1a2d3b"));
                 } else if (newId.equals("urn:uuid:a0e104da-c925-4765-af60-29310de1b99a")) {
-                    List<String>list = resultSolrDoc.getAllFieldValues("wasGeneratedBy");
+                    List<String>list = resultSolrDoc.getAllFieldValues("prov_wasGeneratedBy");
                     assertTrue("The list must contain urn:uuid:9ceeaeb3-6ef3-4b1e-bc4d-96e299fab3a4", list.contains("urn:uuid:9ceeaeb3-6ef3-4b1e-bc4d-96e299fab3a4"));
                 }  else if (newId.equals("urn:uuid:d8e46217-2650-42b2-896f-0f006b1a2d3b")) {
-                    List<String>list = resultSolrDoc.getAllFieldValues("used");
+                    List<String>list = resultSolrDoc.getAllFieldValues("prov_used");
                     assertTrue("The list must contain urn:uuid:ee635a61-c930-444d-8214-db65178a1a47", list.contains("urn:uuid:ee635a61-c930-444d-8214-db65178a1a47"));
                     assertTrue("The list must contain urn:uuid:23495598-d50b-4317-b0e1-05a9d9e52632", list.contains("urn:uuid:23495598-d50b-4317-b0e1-05a9d9e52632"));
                     assertTrue("The list must contain urn:uuid:672ba6c5-8812-4c05-a324-246af172c67a", list.contains("urn:uuid:672ba6c5-8812-4c05-a324-246af172c67a"));
