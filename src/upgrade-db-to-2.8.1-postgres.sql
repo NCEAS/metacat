bc49b42fbd9a1ea929cd01eddc01b7c2066920d3
Jing Tao <tao@nceas.ucsb.edu>
2016-12-16 03:30:28 +0000

    Add the sql to register schema files for the format id fgdc-st-001.1-1999.

--- src/upgrade-db-to-2.8.1-postgres.sql
+++ src/upgrade-db-to-2.8.1-postgres.sql
@@ -3,6 +3,8 @@
  */
 SELECT setval('xml_catalog_id_seq', (SELECT max(catalog_id) from xml_catalog));
 
+INSERT INTO xml_catalog (entry_type, format_id, no_namespace_schema_location, system_id) SELECT 'NoNamespaceSchema', 'FGDC-STD-001.1-1999', 'https://water.usgs.gov/GIS/metadata/usgswrd/fgdc-std-001-1998.xsd', '/schema/fgdc-bdp/fgdc-std-001.1-1999.xsd' WHERE NOT EXISTS (SELECT * FROM xml_catalog WHERE entry_type='NoNamespaceSchema' AND format_id='FGDC-STD-001.1-1999' AND no_namespace_schema_location='https://water.usgs.gov/GIS/metadata/usgswrd/fgdc-std-001-1998.xsd'  AND system_id='/schema/fgdc-bdp/fgdc-std-001.1-1999.xsd');
+
 /*
  * update the database version
  */
