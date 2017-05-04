f54fb6c731e9904f2bdb659c80453d21deff663f
Matt Jones <jones@nceas.ucsb.edu>
2017-05-04 06:25:54 +0000

    Modified backup script to improve efficiency.
    Now, lists of metadata and data files that were last backed up is stored in the /var/metacat/metacat-backup folder, and
    these are diffed with the current file lists to discover new files.  These are then copied to AWS S3.  The AWS
    commands were made more efficient as well by using aws s3 cp with a parallel option, and xargs to launch multiple copies of
    the aws client to maximize throughput.  These changes reduced backups from taking ~3days to a few minutes, depending on how
    much new data is added.

--- src/scripts/bash/backup-aws.sh
+++ src/scripts/bash/backup-aws.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Backup files needed for metacat.  This script creates a directory in /var/metacat/backup,
@@ -35,6 +35,7 @@ KEYLOCATION=/etc/ssl/private
 
 #Location of the server certificate
 CERTLOCATION=/etc/ssl/certs/www_arcticdata_io.crt
+
 #
 # Below here lie demons
 #
@@ -53,8 +54,26 @@ chmod g+rwxs $ARCHROOT
 ARCHDIR="$ARCHROOT"
 mkdir -p $ARCHDIR
 
+# Locations of file lists already archived
+DATALIST=${ARCHDIR}/data-list
+METALIST=${ARCHDIR}/meta-list
+
 # Shut down the tomcat server so nobody else changes anything while we backup
 #/etc/init.d/tomcat7 stop
+
+echo Generate a list of new metadata files since the last backup
+if [ ! -e ${METALIST} ];
+then
+	aws s3 ls ${BUCKET}/documents/ |awk -F" " '{print $4}' > ${METALIST}
+fi
+diff --old-line-format="" --unchanged-line-format="" <(sort ${METALIST}) <(ls -1 ${DATADIR}/documents | sort) > ${METALIST}-new
+
+echo Generate a list of new data files since the last backup
+if [ ! -e ${DATALIST} ];
+then
+	aws s3 ls ${BUCKET}/data/ |awk -F" " '{print $4}' > ${DATALIST}
+fi
+diff --old-line-format="" --unchanged-line-format="" <(sort ${DATALIST}) <(ls -1 ${DATADIR}/data | sort) > ${DATALIST}-new
 
 echo Copy the metacat.properties file to /var/metacat
 cp $METACATPROPERTIESPATH $ARCHDIR
@@ -66,7 +85,24 @@ echo Copy the apache configuration files
 tar czhf $ARCHDIR/apache-config-backup.tgz $APACHECONF $KEYLOCATION $CERTLOCATION
 
 echo Sync the backup directory to Amazon S3
-aws s3 sync $DATADIR $BUCKET
+echo Handle each of the subdirectories independently
+aws s3 sync $DATADIR/certs $BUCKET/certs
+aws s3 sync $DATADIR/dataone $BUCKET/dataone
+aws s3 sync $DATADIR/inline-data $BUCKET/inline-data
+aws s3 sync $DATADIR/logs $BUCKET/logs
+aws s3 sync $DATADIR/.metacat $BUCKET/.metacat
+aws s3 sync $DATADIR/metacat-backup $BUCKET/metacat-backup
+#aws s3 sync $DATADIR/solr-home $BUCKET/solr-home
+#aws s3 sync $DATADIR/tdb $BUCKET/tdb
+#aws s3 sync $DATADIR/temporary $BUCKET/temporary
+
+echo Backup metadata files to S3
+cat ${METALIST}-new | xargs -n1 -P30 -I {} aws s3 cp ${DATADIR}/documents/{} $BUCKET/documents/{}
+cat ${METALIST}-new >> ${METALIST}
+
+echo Backup data files to S3
+cat ${DATALIST}-new | xargs -n1 -P30 -I {} aws s3 cp ${DATADIR}/data/{} $BUCKET/data/{}
+cat ${DATALIST}-new >> ${DATALIST}
 
 # Restart tomcat
 #/etc/init.d/tomcat7 start
