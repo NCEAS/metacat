515b608df47c446d1fa57e33a2aa8f5bdae0a48f
Jing Tao <tao@nceas.ucsb.edu>
2018-05-02 17:25:03 -0700

    Change the field number to 146.

--- metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
+++ metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
@@ -48,7 +48,7 @@ public class SolrQueryServiceControllerTest extends MetacatCommonTestBase {
     public void testgetIndexSchemaFields() throws Exception {
        Map<String, SchemaField> fields = SolrQueryServiceController.getInstance().getIndexSchemaFields();
        assertTrue(fields != null);
-       assertTrue("The number of index schema fields should be 144 rather than "+fields.size(), fields.size() == 144);
+       assertTrue("The number of index schema fields should be 146 rather than "+fields.size(), fields.size() == 146);
     }
     
     /**
