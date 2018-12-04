39c19fa92abc701ecab0c39eeb23ea31a1c576d5
Jing Tao <tao@nceas.ucsb.edu>
2018-12-04 12:59:34 -0800

    In the create index statement, drop the clause "if not exists" which is a new feature supporting after 9.5.

--- src/upgrade-db-to-2.10.0-postgres.sql
+++ src/upgrade-db-to-2.10.0-postgres.sql
@@ -5,7 +5,7 @@ SELECT setval('xml_catalog_id_seq', (SELECT max(catalog_id) from xml_catalog));
 
 INSERT INTO xml_catalog (entry_type, public_id, system_id) SELECT 'Schema', 'http://www.openarchives.org/OAI/2.0/oai_dc/', '/schema/oai_dc/oai_dc.xsd'  WHERE NOT EXISTS (SELECT * FROM xml_catalog WHERE public_id='http://www.openarchives.org/OAI/2.0/oai_dc/');
 
-CREATE INDEX IF NOT EXISTS systemMetadata_series_id on systemMetadata(series_id);
+CREATE INDEX systemMetadata_series_id on systemMetadata(series_id);
 
 /*
  * update the database version
