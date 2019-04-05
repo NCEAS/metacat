842e9a5130c0aec96122515fb8d3888a26e972b1
Jing Tao <tao@nceas.ucsb.edu>
2019-04-05 16:26:54 -0700

    Start to use the DOM tree to build the datacite xml object.

--- src/edu/ucsb/nceas/metacat/doi/datacite/DataCiteMetadataFactory.java
+++ src/edu/ucsb/nceas/metacat/doi/datacite/DataCiteMetadataFactory.java
@@ -22,16 +22,35 @@
  */
 package edu.ucsb.nceas.metacat.doi.datacite;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v2.SystemMetadata;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+
+
 
 /**
- * A factory interface to generate the datacite metadata (xml format) for an DOI object
+ * A factory abstract class to generate the datacite metadata (xml format) for an DOI object
  * @author tao
  *
  */
-public interface DataCiteMetadataFactory {
+public abstract class DataCiteMetadataFactory {
     
     public static final String XML_DECLARATION = "<?xml version=\"1.0\"?> ";
     public static final String OPEN_RESOURCE =  "<resource xmlns=\"http://datacite.org/schema/kernel-3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://datacite.org/schema/kernel-3 https://schema.datacite.org/meta/kernel-3.1/metadata.xsd\">";
@@ -77,14 +96,124 @@ public interface DataCiteMetadataFactory {
     
     public static final String CLOSE_ATT="\">";
     public static final String EN = "en";
+    
+    public static final String NAMESPACE = "http://datacite.org/schema/kernel-3";
+    public static final String SCHEMALOCATION = "https://schema.datacite.org/meta/kernel-3.1/metadata.xsd";
+    public static final String RESOURCE = "resource";
+    public static final String CREATORS = "creators";
+    public static final String CREATOR = "creator";
+    public static final String CREATORNAME = "creatorName";
 
   
+    protected static XPath xpath = null;
+    static {
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        xpath = xPathfactory.newXPath();
+    }
+    
     
     /**
      * Method to generate the datacite meta data xml string for an object with the given system meta data.
      * @param sysmeta  the system meta data information of an given object
      * @return the xml string of the datacite meta data. 
      */
-    public String generateMetadata(Identifier identifier, SystemMetadata sysmeta) throws ServiceFailure;
+    abstract public String generateMetadata(Identifier identifier, SystemMetadata sysmeta) throws ServiceFailure;
+    
+    /**
+     * Generate the blank DOM document for the datacite name space
+     * @return
+     * @throws Exception
+     */
+    protected Document generateROOTDoc() throws Exception {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
+        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+        Document doc = builder.newDocument();
+        Element root = doc.createElementNS(NAMESPACE, RESOURCE);
+        root.setAttributeNS("http://www.w3.org/2001/XMLSchema-instance", 
+            "xsi:schemaLocation", NAMESPACE + " "+SCHEMALOCATION);
+        doc.appendChild(root);
+        return doc;
+    }
+    
+    /**
+     * Add the identifier element to the root document.
+     * This method must be called after we calling the generateROOTDoc method and it can be called only once
+     * @param doc
+     * @param identifier
+     * @return
+     */
+    protected Document addIdentifier(Document doc, String identifier) {
+        Element identifierEle = doc.createElement("identifier");
+        identifierEle.setAttribute("identifierType", "DOI");
+        identifierEle.appendChild(doc.createTextNode(identifier));
+        doc.getFirstChild().appendChild(identifierEle);
+        return doc;
+    }
+    
+    /**
+     * Append a creator element to the root element.
+     * This method should be called immediately after we calling the method addIdentifier. It can be called more than one time.
+     * @param creatorName the name of the creator. If it is a person, it should be this format - lastName, firstName.
+     * @param doc
+     * @param nameIdentifier the value of the name identifier, e.g., 0000-0003-0003-2515
+     * @param nameIdentifierSchemeURI the URI of the name identifier scheme, e.g., http://orcid.org/
+     * @param nameIdentifierScheme the name of the name identifier scheme, e.g., ORCID 
+     * @param affilication the affiliation the creator associates with
+     * @return
+     * @throws XPathExpressionException 
+     */
+    protected Document appendCreator(String creatorName, Document doc, String affiliation, String nameIdentifier, String nameIdentifierSchemeURI, String nameIdentifierScheme) 
+                                                                                                        throws XPathExpressionException {
+        //generate the creator node
+        Element creator = doc.createElement(CREATOR);
+        Element creatorNameEle = doc.createElement(CREATORNAME);
+        creatorNameEle.appendChild(doc.createTextNode(creatorName));
+        creator.appendChild(creatorNameEle);
+        
+        //name identifier is optional
+        if((nameIdentifier != null && !nameIdentifier.trim().equals("") ) && (nameIdentifierScheme != null && !nameIdentifierScheme.trim().equals("")) ) {
+            Element nameIdentifierEle = doc.createElement("nameIdentifier");
+            if(nameIdentifierSchemeURI != null && !nameIdentifierSchemeURI.trim().equals("")) {
+                nameIdentifierEle.setAttribute("schemeURI", nameIdentifierSchemeURI);
+            }
+            nameIdentifierEle.setAttribute("nameIdentifierScheme", nameIdentifierScheme);
+            nameIdentifierEle.appendChild(doc.createTextNode(nameIdentifier));
+            creator.appendChild(nameIdentifierEle);
+        }
+        
+        //affiliation is optional
+        if(affiliation != null && !affiliation.trim().equals("")) {
+            Element affiliationEle = doc.createElement("affiliation");
+            affiliationEle.appendChild(doc.createTextNode(affiliation));
+            creator.appendChild(affiliationEle);
+        }
+        String path = "//"+CREATORS;
+        XPathExpression expr = xpath.compile(path);
+        NodeList creatorsList = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+        if(creatorsList == null ||creatorsList.getLength() == 0) {
+            //we need to create the creators element
+            Element creatorsEle = doc.createElement(CREATORS);
+            doc.getFirstChild().appendChild(creatorsEle);
+            creatorsEle.appendChild(creator);
+        } else {
+            //we don't need to create the creators since it exists
+            int first = 0;
+            creatorsList.item(first).appendChild(creator);
+        }
+        return doc;
+    }
+    
+    /**
+     * Serialize the given doc object to a string
+     * @param doc
+     * @return
+     */
+    protected String serializeDoc(Document doc) {
+        DOMImplementationLS domImplementation = (DOMImplementationLS) doc.getImplementation();
+        LSSerializer lsSerializer = domImplementation.createLSSerializer();
+        return lsSerializer.writeToString(doc);   
+    }
+  
 
 }
