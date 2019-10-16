dd1b216782bcf41aac31a306abfb35659d3b5947
gothub <slaughter@nceas.ucsb.edu>
2019-10-15 20:05:34 -0700

    Add additional tests to PortalSubprocessorIT

--- metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/PortalSubprocessorIT.java
+++ metacat-index/src/test/java/edu/ucsb/nceas/metacat/index/PortalSubprocessorIT.java
@@ -27,43 +27,62 @@ public class PortalSubprocessorIT {
     @Test
     public void testProcessDocument() throws Exception {
         String id = "urn:uuid:349aa330-4645-4dab-a02d-3bf950cf708i";
-        String collectionQuery = null;
-        InputStream is = getPortalDoc();
-        List<SolrElementField> sysSolrFields = new ArrayList<SolrElementField>();
-        SolrDoc indexDocument = new SolrDoc(sysSolrFields);
-        Map<String, SolrDoc> docs = new HashMap<String, SolrDoc>();
-        docs.put(id, indexDocument);
-        IDocumentSubprocessor processor = getPortalSubprocessor();
-        try {
-            // Read in the query string that the processor should create. This is read in
-            // from disk so that we don't have to bother with special character escaping.
-            File file = new File("src/test/resources/portal-collectionQuery.txt");
-            collectionQuery = FileUtils.readFileToString(file);
 
-            docs = processor.processDocument(id, docs, is);
-            // Extract the processed document we just created
-            SolrDoc myDoc = docs.get("urn:uuid:349aa330-4645-4dab-a02d-3bf950cf708i");
-            // Extract fields and check the values
-            String title = myDoc.getField("title").getValue();
-            String queryStr = myDoc.getField("collectionQuery").getValue();
-            //System.out.println("query field value: " + queryStr);
+        ArrayList<String> portalFiles = new ArrayList<String>();
+        portalFiles.add("src/test/resources/portal-example-full.xml");
+        portalFiles.add("src/test/resources/portal-example-seriesId.xml");
+        portalFiles.add("src/test/resources/portal-example-sasap.xml");
 
-            // Did the index sub processor correctly extract the 'title' field from the portal document?
-            assertTrue("The portalSubprocessor correctly build the document with the correct value in the title field.", title.equalsIgnoreCase("My Portal"));
+        ArrayList<String> collectionQueryResultFiles = new ArrayList<String>();
+        collectionQueryResultFiles.add("src/test/resources/collectionQuery-result-example-full.txt");
+        collectionQueryResultFiles.add("src/test/resources/collectionQuery-result-example-seriesId.txt");
+        collectionQueryResultFiles.add("src/test/resources/collectionQuery-result-example-sasap.txt");
 
-            // Did the index sub processor correctly extract the 'collectionQuery' field from the portal document?
-            assertTrue("The portalSubprocessor correctly built the document with the correct value in the \"collectionQuery\" field.", queryStr.equalsIgnoreCase(collectionQuery));
+        ArrayList<String> portalNames = new ArrayList<String>();
+        portalNames.add("My Portal");
+        portalNames.add("Another test portal");
+        portalNames.add("Lauren's test project - updated");
 
-        } catch (Exception e) {
-            System.out.println("Error: " +e.getMessage());
+        for(int i=0; i < portalFiles.size(); i++) {
+            String collectionQuery = null;
+            InputStream is = getPortalDoc(portalFiles.get(i));
+            List<SolrElementField> sysSolrFields = new ArrayList<SolrElementField>();
+            SolrDoc indexDocument = new SolrDoc(sysSolrFields);
+            Map<String, SolrDoc> docs = new HashMap<String, SolrDoc>();
+            docs.put(id, indexDocument);
+            IDocumentSubprocessor processor = getPortalSubprocessor();
+
+            String queryResult = null;
+            try {
+                // Read in the query string that the processor should create. This is read in
+                // from disk so that we don't have to bother with special character escaping.
+                File file = new File(collectionQueryResultFiles.get(i));
+                collectionQuery = FileUtils.readFileToString(file);
+                docs = processor.processDocument(id, docs, is);
+                // Extract the processed document we just created
+                SolrDoc myDoc = docs.get("urn:uuid:349aa330-4645-4dab-a02d-3bf950cf708i");
+                // Extract fields and check the values
+                String title = myDoc.getField("title").getValue();
+                String queryStr = myDoc.getField("collectionQuery").getValue();
+                queryStr = queryStr.trim();
+                System.out.println("query field value:  " + "\"" + queryStr + "\"");
+
+                // Did the index sub processor correctly extract the 'title' field from the portal document?
+                assertTrue("The portalSubprocessor correctly build the document with the correct value in the title field.", title.equalsIgnoreCase(portalNames.get(i)));
+                // Did the index sub processor correctly extract the 'collectionQuery' field from the portal document?
+                assertTrue("The portalSubprocessor correctly built the document with the correct value in the \"collectionQuery\" field.", queryStr.equalsIgnoreCase(collectionQuery.trim()));
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
         }
     }
 
     /*
      * Get the document format of the test resource map file
      */
-    private InputStream getPortalDoc() throws Exception{
-        File file = new File("src/test/resources/portal-example-full.xml");
+    private InputStream getPortalDoc(String filename) throws Exception{
+        File file = new File(filename);
         InputStream is = new FileInputStream(file);
         return is;
     }
