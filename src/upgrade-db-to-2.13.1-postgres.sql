7edfe754e11b8028d12e4696b5e19aef56e1e848
Jing Tao <tao@nceas.ucsb.edu>
2020-06-02 13:42:33 -0700

    Add the usages table.

--- src/upgrade-db-to-2.13.1-postgres.sql
+++ src/upgrade-db-to-2.13.1-postgres.sql
@@ -1,15 +1,16 @@
 /*
  * Create the usages table
  */
-CREATE SEQUENCE usages_usage_id_seq;
-CREATE TABLE usages (
-	usage_id INT8 default nextval('usages_usage_id_seq'),  -- the unique usage id (pk)
+CREATE SEQUENCE quota_usages_usage_id_seq;
+CREATE TABLE quota_usages (
+	usage_id INT8 default nextval('quota_usages_usage_id_seq'),  -- the unique usage id (pk)
   quota_id TEXT,  -- the identifier of the quota
   instance_id TEXT,  -- storage - pid of object; portal - sid of portal document
   quantity FLOAT8, -- the amount of the usage
   date_reported TIMESTAMP,  -- the time stamp that the quota usage was reported to the quota service 
    CONSTRAINT quotas_pk PRIMARY KEY (usage_id)
 );
+CREATE INDEX quota_usages_idx1 ON quota_usages (date_reported);
 
 /*
  * Ensure xml_catalog sequence is at table max
--- src/xmltables-postgres.sql
+++ src/xmltables-postgres.sql
@@ -561,3 +561,17 @@ CREATE TABLE scheduled_job_params (
   CONSTRAINT scheduled_job_params_fk
         FOREIGN KEY (job_id) REFERENCES scheduled_job(id)
 );
+
+/*                                                                                                                                                                                                                                                                                                                   
+ * quota_usages -- table to store quota usages
+ */
+CREATE SEQUENCE quota_usages_usage_id_seq;
+CREATE TABLE quota_usages (
+  usage_id INT8 default nextval('quota_usages_usage_id_seq'),  -- the unique usage id (pk)
+  quota_id TEXT,  -- the identifier of the quota
+  instance_id TEXT,  -- storage - pid of object; portal - sid of portal document
+  quantity FLOAT8, -- the amount of the usage
+  date_reported TIMESTAMP,  -- the time stamp that the quota usage was reported to the quota service 
+   CONSTRAINT quotas_pk PRIMARY KEY (usage_id)
+);
+CREATE INDEX quota_usages_idx1 ON quota_usages (date_reported);
