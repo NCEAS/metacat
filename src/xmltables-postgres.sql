29b1dca4f09e3a47805c64e70a5a61fc5584ea03
Jing Tao <tao@nceas.ucsb.edu>
2020-06-11 10:17:10 -0700

    Add a new field status and some not null constrains in the quota_usages table.

--- src/xmltables-postgres.sql
+++ src/xmltables-postgres.sql
@@ -568,10 +568,15 @@ CREATE TABLE scheduled_job_params (
 CREATE SEQUENCE quota_usages_usage_id_seq;
 CREATE TABLE quota_usages (
   usage_id INT8 default nextval('quota_usages_usage_id_seq'),  -- the unique usage id (pk)
-  quota_id INT,  -- the identifier of the quota
-  instance_id TEXT,  -- storage - pid of object; portal - sid of portal document
-  quantity FLOAT8, -- the amount of the usage
-  date_reported TIMESTAMP,  -- the time stamp that the quota usage was reported to the quota service 
-   CONSTRAINT quotas_pk PRIMARY KEY (usage_id)
+  object text NOT NULL,  -- it should always be usage
+  quota_id INT NOT NULL,  -- the identifier of the quota
+  instance_id TEXT NOT NULL,  -- storage - pid of object; portal - sid of portal document
+  quantity FLOAT8 NOT NULL, -- the amount of the usage
+  date_reported TIMESTAMP,  -- the time stamp that the quota usage was reported to the quota service
+  status text, -- the status of the usage 
+   CONSTRAINT quota_usages_pk PRIMARY KEY (usage_id),
+   CONSTRAINT quota_usages_uk UNIQUE (quota_id, instance_id)
 );
 CREATE INDEX quota_usages_idx1 ON quota_usages (date_reported);
+CREATE INDEX quota_usages_idx2 ON quota_usages (quota_id);
+CREATE INDEX quota_usages_idx3 ON quota_usages (instance_id);
