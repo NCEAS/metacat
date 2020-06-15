8b3b36aeda5b3118e9648a6448862515358548ce
Jing Tao <tao@nceas.ucsb.edu>
2020-06-15 11:25:06 -0700

    Rename the table to quota_usage_events and add an index on the column status.

--- src/xmltables-postgres.sql
+++ src/xmltables-postgres.sql
@@ -565,18 +565,19 @@ CREATE TABLE scheduled_job_params (
 /*                                                                                                                                                                                                                                                                                                                   
  * quota_usages -- table to store quota usages
  */
-CREATE SEQUENCE quota_usages_usage_id_seq;
-CREATE TABLE quota_usages (
-  usage_id INT8 default nextval('quota_usages_usage_id_seq'),  -- the unique usage id (pk)
+CREATE SEQUENCE quota_usage_events_usage_id_seq;
+CREATE TABLE quota_usage_events (
+    usage_id INT8 default nextval('quota_usage_events_usage_id_seq'),  -- the unique usage id (pk)
   object text NOT NULL,  -- it should always be usage
   quota_id INT NOT NULL,  -- the identifier of the quota
   instance_id TEXT NOT NULL,  -- storage - pid of object; portal - sid of portal document
   quantity FLOAT8 NOT NULL, -- the amount of the usage
   date_reported TIMESTAMP,  -- the time stamp that the quota usage was reported to the quota service
   status text, -- the status of the usage 
-   CONSTRAINT quota_usages_pk PRIMARY KEY (usage_id),
-   CONSTRAINT quota_usages_uk UNIQUE (quota_id, instance_id)
+   CONSTRAINT quota_usage_events_pk PRIMARY KEY (usage_id),
+   CONSTRAINT quota_usage_events_uk UNIQUE (quota_id, instance_id, status)
 );
-CREATE INDEX quota_usages_idx1 ON quota_usages (date_reported);
-CREATE INDEX quota_usages_idx2 ON quota_usages (quota_id);
-CREATE INDEX quota_usages_idx3 ON quota_usages (instance_id);
+CREATE INDEX quota_usage_events_idx1 ON quota_usage_events (date_reported);
+CREATE INDEX quota_usage_events_idx2 ON quota_usage_events (quota_id);
+CREATE INDEX quota_usage_events_idx3 ON quota_usage_events (instance_id);
+CREATE INDEX quota_usage_events_idx4 ON quota_usage_events (status);
