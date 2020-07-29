cc1fa80dabe180b108963ee23d53a98af93d07c4
Jing Tao <tao@nceas.ucsb.edu>
2020-07-29 10:49:55 -0700

    Add more fields, such as subscriber and quota type for the class.

--- src/edu/ucsb/nceas/metacat/dataone/quota/LocalUsage.java
+++ src/edu/ucsb/nceas/metacat/dataone/quota/LocalUsage.java
@@ -28,7 +28,58 @@ import org.dataone.bookkeeper.api.Usage;
  */
 public class LocalUsage extends Usage {
     private int localId = -1;
+    private String subscriber = null;
+    private String quotaType = null;
+    private String requestor = null;
     
+    /**
+     * Get the subscriber of the usage
+     * @return the subscriber
+     */
+    public String getSubscriber() {
+        return subscriber;
+    }
+
+    /**
+     * Set the subscriber associated with the usage
+     * @param subscriber  the value will be set
+     */
+    public void setSubscriber(String subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    /**
+     * Get the quota type of the usage
+     * @return the quota type
+     */
+    public String getQuotaType() {
+        return quotaType;
+    }
+
+    /**
+     * Set the given quota type to the usage
+     * @param quotaType  the type will be set
+     */
+    public void setQuotaType(String quotaType) {
+        this.quotaType = quotaType;
+    }
+
+    /**
+     * Get the requestor associated with the usage
+     * @return the requestor
+     */
+    public String getRequestor() {
+        return requestor;
+    }
+
+    /**
+     * Set requestor for this usage
+     * @param requestor the value will be set
+     */
+    public void setRequestor(String requestor) {
+        this.requestor = requestor;
+    }
+
     /**
      * Get the local id associated with the local usage
      * @return the local id
