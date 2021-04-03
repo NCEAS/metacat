a06dea1e63ef676d8d642e8bc319b3dab0e97641
gothub <slaughter@nceas.ucsb.edu>
2021-04-02 17:15:22 -0700

    Update unit test MNodeQueryTest for collectionQuery changes
    Issue #1494 Update this unit test with the new solution for collectionQuery, based on updates for collect/portal 1.1.0 schema changes and Solr negation query handling.

--- test/edu/ucsb/nceas/metacat/dataone/MNodeQueryTest.java
+++ test/edu/ucsb/nceas/metacat/dataone/MNodeQueryTest.java
@@ -81,7 +81,7 @@ public class MNodeQueryTest extends D1NodeServiceTest {
     private static String portalFilePath = "test/example-portal.xml";
     private int tryAcccounts = 50;
     
-    private static final String collectionResult = "<str name=\"collectionQuery\">(((text:*soil* AND (keywords:\"soil layer\" AND attribute:\"soil layer\") AND (dateUploaded:[1800-01-01T00:00:00Z TO 2009-01-01T00:00:00Z] AND beginDate:[1800-01-01T00:00:00Z TO 2009-01-01T00:00:00Z]) AND isPublic:true AND numberReplicas:[1 TO *]) AND (-obsoletedBy:* AND formatType:METADATA)))</str>";
+    private static final String collectionResult = "<str name=\"collectionQuery\">((text:*soil* AND (keywords:\"soil layer\" AND attribute:\"soil layer\") AND (dateUploaded:[1800-01-01T00:00:00Z TO 2009-01-01T00:00:00Z] AND beginDate:[1800-01-01T00:00:00Z TO 2009-01-01T00:00:00Z]) AND isPublic:true AND numberReplicas:[1 TO *])) AND (-obsoletedBy:* AND formatType:METADATA)</str>";
     private static final String baseURI = "https://cn.dataone.org/cn/v2/resolve";
     private static final String longQueryFile = "test/test-queries/long-solr-query-partial.txt";
     
