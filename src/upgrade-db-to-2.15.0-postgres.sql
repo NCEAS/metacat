6851f8a03f19b3f8e3b84bb228437f9c8c896c50
Jing Tao <tao@nceas.ucsb.edu>
2021-04-28 14:40:31 -0700

    Add the index for the columns of uploaded_date and modified_date in the systemmetadata table.

--- src/upgrade-db-to-2.15.0-postgres.sql
+++ src/upgrade-db-to-2.15.0-postgres.sql
@@ -6,6 +6,9 @@ SELECT setval('xml_catalog_id_seq', (SELECT max(catalog_id) from xml_catalog));
 
 INSERT INTO xml_catalog (entry_type, public_id, format_id) SELECT 'NonXML', 'science-on-schema.org/Dataset/1.2;ld+json', 'science-on-schema.org/Dataset/1.2;ld+json'  WHERE NOT EXISTS (SELECT * FROM xml_catalog WHERE public_id='science-on-schema.org/Dataset/1.2;ld+json');
 
+CREATE INDEX systemMetadata_date_uploaded on systemMetadata(date_uploaded);
+CREATE INDEX systemMetadata_date_modified on systemMetadata(date_modified);
+
 /*
  * update the database version
  */
