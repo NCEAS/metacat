836d02fa57aa48652805acedb7c34e1674eebc42
Jing Tao <tao@nceas.ucsb.edu>
2023-03-21 17:38:44 -0700

    Add some util scripts.

---
+++ src/scripts/bash/checksum-data-file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+directory="/var/metacat/data"
+prefix="$directory/" 
+
+rm data-checksum.txt
+for file in "$directory"/*; do
+    if [[  "$file" =~ ^$directory/auto ]]; then
+        #checksum=$(md5sum "$file" | awk '{print $1}')
+        checksum=$(md5 "$file" | awk '{print $4}')
+
+        # Extract the "foo.2" and "1" parts of the filename
+        fullfilename=$(echo "$file" | cut -d. -f1-2)
+        filename=${fullfilename#"$prefix"}
+        fileversion=$(echo "$file" | cut -d. -f3)
+
+        # Write the filename, version, and checksum to the output file
+        echo "$filename $fileversion $checksum" >> data-checksum.txt
+    fi
+done
---
+++ src/scripts/bash/checksum-document-file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+directory="/var/metacat/documents"
+prefix="$directory/"
+
+rm document-checksum.txt
+for file in "$directory"/*; do
+        #checksum=$(md5sum "$file" | awk '{print $1}')
+        checksum=$(md5 "$file" | awk '{print $4}')
+
+        # Extract the "foo.2" and "1" parts of the filename
+        fullfilename=$(echo "$file" | cut -d. -f1-2)
+        filename=${fullfilename#"$prefix"}
+        fileversion=$(echo "$file" | cut -d. -f3)
+
+        # Write the filename, version, and checksum to the output file
+        echo "$filename $fileversion $checksum" >> document-checksum.txt
+done
