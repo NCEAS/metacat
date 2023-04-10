fb6ab16c341f59d2967a23331ed55ae9f98f88f2
Matthew B <106352182+artntek@users.noreply.github.com>
2023-04-10 16:59:50 -0700

    split out into individual tests & junit4

--- test/edu/ucsb/nceas/metacattest/PropertyServiceTest.java
+++ test/edu/ucsb/nceas/metacattest/PropertyServiceTest.java
@@ -1,194 +1,176 @@
 package edu.ucsb.nceas.metacattest;
 
+import edu.ucsb.nceas.MCTestCase;
+import edu.ucsb.nceas.metacat.properties.PropertyService;
+import edu.ucsb.nceas.utilities.GeneralPropertyException;
+import edu.ucsb.nceas.utilities.PropertyNotFoundException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import java.util.Map;
 import java.util.Random;
 import java.util.Vector;
 
-import edu.ucsb.nceas.MCTestCase;
-
-import edu.ucsb.nceas.metacat.properties.PropertyService;
-import edu.ucsb.nceas.utilities.PropertyNotFoundException;
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import static org.junit.Assert.fail;
 
 /**
  * A JUnit test for testing Access Control in Metacat
  */
-public class PropertyServiceTest extends MCTestCase {
+@RunWith(JUnit4.class)
+public class PropertyServiceTest { // don't extend MCTestCase for JUnit 4
 
-    /**
-     * Constructor to build the test
-     *
-     * @param name the name of the test method
-     */
-    public PropertyServiceTest(String name) {
-        super(name);
+    private static final TestUtils testUtils = new TestUtils();
+    private final Random random = new Random();
+
+    public PropertyServiceTest() {
+        super();
     }
 
     /**
      * Establish a testing framework by initializing appropriate objects
      */
+    @Before
     public void setUp() {
-
     }
 
     /**
      * Release any objects after tests are complete
      */
+    @After
     public void tearDown() {
     }
 
-    /**
-     * Create a suite of tests to be run together
-     */
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(new PropertyServiceTest("initialize"));
-        // Test basic functions
-        suite.addTest(new PropertyServiceTest("mainPropertiesTest"));
 
-        return suite;
-    }
-
-    /**
-     * Run an initial test that always passes to check that the test harness is working.
-     */
-    public void initialize() {
-        assertTrue(1 == 1);
-    }
-
-
-    /**
-     * Tests reading and writing from properties using the PropertyService class 1. read a single
-     * property from the properties file that should exist Read a test property from the
-     * PropertyService.  The value should be read successfully.  If PropertyNotFoundException is
-     * thrown, the test case fails. Also, if the value returned is null, the test case fails.
-     * Strictly speaking, it is not a failure for getProperty to return null, but we know that
-     * 'test.mcUser' should have a value. 2. read a single property from the main properties that
-     * shouldn't exist Expect a PropertyNotFoundException here.  If the exception is not thrown, the
-     * test case fails. 3. read group property names from the main properties read an existing group
-     * of property names.  If the result set is zero length, the test case fails. 4. read group
-     * properties from the main properties Get the property values for the group names retrieved in
-     * test case 3.  If the properties map retrieved is empty, the test case fails. 5. write
-     * property to main properties Write a property that already exists to the metacat.properties
-     * file.  The value is randomly generated.  The value is then retrieved from the
-     * metacat.properties file.  If the retrieved value does not match the generated value, or if an
-     * exception is thrown, the test case fails. 6. set property and persist to main properties This
-     * does the same as test case 5, but instead of using setProperty which sets the property in
-     * memory and persists it to file, this case uses setPropertyNoPersist and persistProperties to
-     * do the set and persist atomically. 7. try to write property to main properties that doesn't
-     * exist Attempt to set a property that doesn't exist in the properties file.  This should throw
-     * a PropertyNotFoundException.  Anything else causes the test case to fail. 8. try to write
-     * property nonPersistent to main properties that doesn't exist This does the same as test case
-     * 7, but uses the setPropertyNoPersist method.
-     */
-    public void mainPropertiesTest() {
-        Random random = new Random();
+    @Test
+    public void readOneProperty() {
+        //====1 read a single property from the properties file that should exist
+        testUtils.debugMsg("Test 1: read a single property from the properties file that should exist");
         try {
-            debug("\nRunning: mainPropertiesTest test");
-
-            //====1 read a single property from the properties file that should exist
-            debug("Test 1: read a single property from the properties file that should exist");
-            try {
-                String user = PropertyService.getProperty("test.mcUser");
-                if (user == null || user.equals("")) {
-                    fail("Test 1: Reading property 'test.mcUser' returned no value.");
-                }
-                debug("Test 1: read property 'test.mcUser': " + user);
-            } catch (PropertyNotFoundException pnfe) {
-                fail("Test 1: Could not read property 'test.mcUser' : " + pnfe.getMessage());
+            String user = PropertyService.getProperty("test.mcUser");
+            if (user == null || user.equals("")) {
+                fail("Test 1: Reading property 'test.mcUser' returned no value.");
             }
+            testUtils.debugMsg("Test 1: read property 'test.mcUser': " + user);
+        } catch (PropertyNotFoundException pnfe) {
+            fail("Test 1: Could not read property 'test.mcUser' : " + pnfe.getMessage());
+        }
+    }
 
-            //====2 read a single property from the main properties  that shouldn't exist
-            debug("Test 2: read a single property from the main properties  that shouldn't exist");
-            try {
-                String metacatURL = PropertyService.getProperty("test.this.doesn't.exist");
-                fail("Test 2: shouldn't have successfully read property: test.this.doesn't.exist : "
-                    + metacatURL);
-            } catch (PropertyNotFoundException pnfe) {
-                debug("Test 2: expected failure reading property:'test.this.doesn't.exist' : "
-                    + pnfe.getMessage());
+
+    @Test
+    public void readNonExistentProperty() {
+        //====2 read a single property from the main properties  that shouldn't exist
+        testUtils.debugMsg("Test 2: read a single property from the main properties  that shouldn't exist");
+        try {
+            String metacatURL = PropertyService.getProperty("test.this.doesn't.exist");
+            fail("Test 2: shouldn't have successfully read property: test.this.doesn't.exist : "
+                + metacatURL);
+        } catch (PropertyNotFoundException pnfe) {
+            testUtils.debugMsg("Test 2: expected failure reading property:'test.this.doesn't.exist' : "
+                + pnfe.getMessage());
+        }
+    }
+
+    @Test
+    public void getPropertyNamesByGroup() {
+        //====3 read group property names from the main properties
+        testUtils.debugMsg("Test 3: read property group names 'organization.org'");
+        Vector<String> orgList = PropertyService.getPropertyNamesByGroup("organization.org");
+        if (orgList == null || orgList.size() == 0) {
+            fail("Test 3: Empty vector returned when reading property group names "
+                + "'organization.org'");
+        }
+    }
+
+    @Test
+    public void getPropertiesByGroup() {
+        // ====4 read group properties from the main properties
+        try {
+            testUtils.debugMsg("Test 4: read property group 'organization.org'");
+            Map<String, String> metacatProps =
+                PropertyService.getPropertiesByGroup("organization.org");
+            if (metacatProps == null || metacatProps.size() == 0) {
+                fail("Test 4: Empty map returned when reading property group names "
+                    + "'organization.org'");
             }
+        } catch (PropertyNotFoundException pnfe) {
+            fail("Test 4: Could not read property group names 'organization.org': "
+                + pnfe.getMessage());
+        }
+    }
 
-            //====3 read group property names from the main properties  
-            debug("Test 3: read property group names 'organization.org'");
-            Vector<String> orgList = PropertyService.getPropertyNamesByGroup("organization.org");
-            if (orgList == null || orgList.size() == 0) {
-                fail(
-                    "Test 3: Empty vector returned when reading property group names "
-                        + "'organization.org'");
+    @Test
+    public void setProperty() {
+        // ====5 write property to main properties
+        try {
+            String testValue = "testing" + random.nextInt();
+            testUtils.debugMsg("Test 5: set property 'test.testProperty' : " + testValue);
+            PropertyService.setProperty("test.testProperty", testValue);
+            String testValue2 = PropertyService.getProperty("test.testProperty");
+            if (!testValue.equals(testValue2)) {
+                fail("Test 5: couldn't set 'test.testProperty' to " + testValue);
             }
+        } catch (GeneralPropertyException pnfe) {
+            fail("Test 5: Could not set property 'test.testProperty' : " + pnfe.getMessage());
+        }
+    }
 
-            // ====4 read group properties from the main properties
-            try {
-                debug("Test 4: read property group 'organization.org'");
-                Map<String, String> metacatProps =
-                    PropertyService.getPropertiesByGroup("organization.org");
-                if (metacatProps == null || metacatProps.size() == 0) {
-                    fail(
-                        "Test 4: Empty map returned when reading property group names "
-                            + "'organization.org'");
-                }
-            } catch (PropertyNotFoundException pnfe) {
-                fail("Test 4: Could not read property group names 'organization.org': "
-                    + pnfe.getMessage());
+    @Test
+    public void persistProperties() {
+        // ====6 set property and persist to main properties
+        try {
+            String testValue = "testing" + random.nextInt();
+            testUtils.debugMsg("Test 6: set property 'test.testProperty' : " + testValue);
+            PropertyService.setPropertyNoPersist("test.testProperty", testValue);
+            PropertyService.persistProperties();
+            String testValue2 = PropertyService.getProperty("test.testProperty");
+            if (!testValue.equals(testValue2)) {
+                fail("Test 6: couldn't set 'test.testProperty' to " + testValue);
             }
+        } catch (GeneralPropertyException pnfe) {
+            fail("Test 6: Could not set property 'test.testProperty' : " + pnfe.getMessage());
+        }
+    }
 
-            // ====5 write property to main properties
-            try {
-                String testValue = "testing" + random.nextInt();
-                debug("Test 5: set property 'test.testProperty' : " + testValue);
-                PropertyService.setProperty("test.testProperty", testValue);
-                String testValue2 = PropertyService.getProperty("test.testProperty");
-                if (!testValue.equals(testValue2)) {
-                    fail("Test 5: couldn't set 'test.testProperty' to " + testValue);
-                }
-            } catch (PropertyNotFoundException pnfe) {
-                fail("Test 5: Could not set property 'test.testProperty' : " + pnfe.getMessage());
-            }
+    @Test
+    public void setPropertyNonExistent() {
+        // ====7 try to write property to main properties that doesn't exist
+        String testValue;
+        try {
+            testValue = "testing" + random.nextInt();
+            testUtils.debugMsg("Test 7: set property 'test.property.nonexistent' : " + testValue);
+            PropertyService.setProperty("test.property.nonexistent", testValue);
+            fail("Test 7: shouldn't have been able to set 'test.property.nonexistent' to "
+                + testValue + " since 'test.property.nonexistent' doesn't exist.");
+        } catch (GeneralPropertyException pnfe) {
+            testUtils.debugMsg("Test 7: expected failure writing to property:'test.property.nonexistent' : "
+                + pnfe.getMessage());
+        }
+    }
 
-            // ====6 set property and persist to main properties
-            try {
-                String testValue = "testing" + random.nextInt();
-                debug("Test 6: set property 'test.testProperty' : " + testValue);
-                PropertyService.setPropertyNoPersist("test.testProperty", testValue);
-                PropertyService.persistProperties();
-                String testValue2 = PropertyService.getProperty("test.testProperty");
-                if (!testValue.equals(testValue2)) {
-                    fail("Test 6: couldn't set 'test.testProperty' to " + testValue);
-                }
-            } catch (PropertyNotFoundException pnfe) {
-                fail("Test 6: Could not set property 'test.testProperty' : " + pnfe.getMessage());
-            }
+    @Test
+    public void setPropertyNoPersist() {
+        // ====8 try to write property nonPersistent to main properties that doesn't exist
+        String testValue;
+        try {
+            testValue = "testing" + random.nextInt();
+            testUtils.debugMsg("Test 8: set property 'test.property.nonexistent' : " + testValue);
+            PropertyService.setPropertyNoPersist("test.property.nonexistent", testValue);
+            fail("Test 8: shouldn't have been able to set 'test.property.nonexistent' to "
+                + testValue + " since 'test.property.nonexistent' doesn't exist.");
+        } catch (GeneralPropertyException pnfe) {
+            testUtils.debugMsg("Test 8: expected failure writing to property:'test.property.nonexistent' : "
+                + pnfe.getMessage());
+        }
+    }
 
-            // ====7 try to write property to main properties that doesn't exist
-            String testValue;
-            try {
-                testValue = "testing" + random.nextInt();
-                debug("Test 7: set property 'test.property.nonexistent' : " + testValue);
-                PropertyService.setProperty("test.property.nonexistent", testValue);
-                fail("Test 7: shouldn't have been able to set 'test.property.nonexistent' to "
-                    + testValue + " since 'test.property.nonexistent' doesn't exist.");
-            } catch (PropertyNotFoundException pnfe) {
-                debug("Test 7: expected failure writing to property:'test.property.nonexistent' : "
-                    + pnfe.getMessage());
-            }
+    public static class TestUtils extends MCTestCase {
 
-            // ====8 try to write property nonPersistent to main properties that doesn't exist
-            try {
-                testValue = "testing" + random.nextInt();
-                debug("Test 8: set property 'test.property.nonexistent' : " + testValue);
-                PropertyService.setPropertyNoPersist("test.property.nonexistent", testValue);
-                fail("Test 8: shouldn't have been able to set 'test.property.nonexistent' to "
-                    + testValue + " since 'test.property.nonexistent' doesn't exist.");
-            } catch (PropertyNotFoundException pnfe) {
-                debug("Test 8: expected failure writing to property:'test.property.nonexistent' : "
-                    + pnfe.getMessage());
-            }
-
-
-        } catch (Exception e) {
-            fail("General exception:\n" + e.getMessage());
+        void debugMsg(String msg) {
+            MCTestCase.debug(msg);
         }
     }
 }
