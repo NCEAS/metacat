e69b4a862181f487dfb9ae62b3a57f4bcc53298c
Matthew B <106352182+artntek@users.noreply.github.com>
2023-06-07 14:55:52 -0700

    docker cleanup before PR

--- docker/build.sh
+++ docker/build.sh
@@ -3,11 +3,18 @@
 set -e
 TAG=$1
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <tag>"
+if [ -z "$1" ] || [[ "$1" == "TRUE" ]]; then
+    echo "Usage: $0 <tag> <debug>"
     TAG="2.19.0"
-    echo "where <tag> is typically set to the metacat version. Setting to default: ${TAG}"
+    echo "where: <tag>   is typically set to the metacat version. Setting to default: ${TAG}"
+    echo "       <debug> is ignored unless the value is \"TRUE\"" 
 fi
+
+DEBUG="FALSE"
+if [[ "$1" == "TRUE" ]] || [[ "$2" == "TRUE" ]]; then
+    DEBUG="TRUE"
+fi
+echo "Building with \"DEBUG\" set to: ${DEBUG}"
 
 RELEASE="metacat-bin-${TAG}.tar.gz"
 
@@ -16,6 +23,6 @@ if [ ! -f "../${RELEASE}" ]; then
     echo "You must first build the metacat release with 'ant distbin'"
 fi
 
-# Launch docker
 cp ../"${RELEASE}" .
-docker image build --no-cache --tag metacat:"$TAG" --build-arg TAG="$TAG" .
+
+docker image build --no-cache --tag metacat:"$TAG" --build-arg TAG="$TAG" --build-arg DEBUG="$DEBUG" .
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,19 +7,18 @@ if [ "$1" = 'catalina.sh' ]; then
         unzip webapps/metacat-index.war -d webapps/metacat-index
     fi
 
-    # set the env vars.TC_HOME and METACAT_APP_CONTEXT are set in Dockerfile
+    # set the env vars. Note that TC_HOME and METACAT_APP_CONTEXT are set in Dockerfile
     METACAT_DEFAULT_WAR=${TC_HOME}/webapps/metacat.war
     METACAT_DIR=${TC_HOME}/webapps/${METACAT_APP_CONTEXT}
     METACAT_WAR=${METACAT_DIR}.war
 
-    echo "Installing metacat to context ${METACAT_APP_CONTEXT}"
-
     # Check the context
-    if [ "${METACAT_WAR}" != "${METACAT_DEFAULT_WAR}" ] &&
-        [ -f "$METACAT_DEFAULT_WAR" ]; then
+    if [ "${METACAT_WAR}" != "${METACAT_DEFAULT_WAR}" ] && [ -f "$METACAT_DEFAULT_WAR" ]; then
         # Move the application to match the context
-        echo "Changing context to ${METACAT_APP_CONTEXT}"
-        mv $METACAT_DEFAULT_WAR "$METACAT_WAR"
+        echo "Changing metacat context to ${METACAT_APP_CONTEXT}"
+        mv "$METACAT_DEFAULT_WAR" "$METACAT_WAR"
+    else
+        echo "Installing metacat to context ${METACAT_APP_CONTEXT}"
     fi
 
     # Expand the WAR file
@@ -30,8 +29,9 @@ if [ "$1" = 'catalina.sh' ]; then
     # change the context in the web.xml file
     apply_context.py "$METACAT_DIR"/WEB-INF/web.xml metacat "${METACAT_APP_CONTEXT}"
 
-    # Show KNB skin if nothing else configured. TODO MB - make this work with props config later
-    mkdir ${TC_HOME}/webapps/config
+    # Show KNB skin if nothing else configured.
+    # TODO: deploy metacatui separately, or make this work with props config later
+    mkdir "${TC_HOME}"/webapps/config
     {
         echo "MetacatUI.AppConfig = {"
         echo "  theme: \"knb\","
@@ -39,7 +39,7 @@ if [ "$1" = 'catalina.sh' ]; then
         echo "  metacatContext: \"/${METACAT_APP_CONTEXT}\","
         echo "  baseUrl: \"http://localhost:8080\""
         echo "}"
-    } > ${TC_HOME}/webapps/config/config.js
+    } > "${TC_HOME}"/webapps/config/config.js
 
 
     # Make sure all default directories are available
@@ -53,27 +53,32 @@ if [ "$1" = 'catalina.sh' ]; then
 
     # if DEBUG, set the root log level accordingly
     if [[ "$DEBUG" == "TRUE" ]]; then
-      sed -i 's/rootLogger\.level[^\n]*/rootLogger\.level=DEBUG/g' ${TC_HOME}/webapps/metacat/WEB-INF/classes/log4j2.properties;
+      sed -i 's/rootLogger\.level[^\n]*/rootLogger\.level=DEBUG/g' "${TC_HOME}"/webapps/metacat/WEB-INF/classes/log4j2.properties;
       echo "* * * * * * set Log4J rootLogger level to DEBUG * * * * * *"
     fi
 
+    # TODO: need a more-elegant way to handle this, without manipulating files
     # If env has an admin/password set, but it does not exist in the passwords file, then add it
     if [ -n "$METACAT_ADMINISTRATOR_USERNAME" ]; then
         USER_PWFILE="/var/metacat/users/password.xml"
 
         if [ -z "$METACAT_ADMINISTRATOR_PASSWORD" ]; then
-            echo "ERROR: The admin user (METACAT_ADMINISTRATOR_USERNAME) was set but no password value was set."
-            echo "       You may use METACAT_ADMINISTRATOR_PASSWORD to set the administrator password"
+            echo "ERROR:  The admin user (METACAT_ADMINISTRATOR_USERNAME) environment variable was"
+            echo "        set, but no password value was set."
+            echo "        You may use the METACAT_ADMINISTRATOR_PASSWORD environment variable to"
+            echo "        set the administrator password"
             exit 2
         fi
         # look specifically for the user password file, as it is expected if the configuration is completed
-        if [ ! -s "$USER_PWFILE" ] || [ $(grep -c "$METACAT_ADMINISTRATOR_USERNAME" "$USER_PWFILE") -eq 0 ]; then
+        # shellcheck disable=SC2046
+        if [ ! -s "$USER_PWFILE" ] ||
+            [ $(grep -c "$METACAT_ADMINISTRATOR_USERNAME" "$USER_PWFILE") -eq 0 ]; then
             # Note: the Java bcrypt library only supports '2a' format hashes, so override the default python behavior
             # so that the hashes created start with '2a' rather than '2y'
             cd "${METACAT_DIR}"/WEB-INF/scripts/bash
             PASS=$(python -c "import bcrypt; print bcrypt.hashpw('$METACAT_ADMINISTRATOR_PASSWORD', bcrypt.gensalt(10,prefix='2a'))")
             bash ./authFileManager.sh useradd -h "$PASS" -dn "$METACAT_ADMINISTRATOR_USERNAME"
-            cd $TC_HOME
+            cd "$TC_HOME"
             echo
             echo '*************************************'
             echo 'Added administrator to passwords file'
@@ -90,13 +95,21 @@ if [ "$1" = 'catalina.sh' ]; then
     echo "Waiting for Tomcat to start before"
     echo "checking upgrade/initialization status"
     echo '**************************************'
-    sleep 5
+    while ! nc -z localhost 8080; do
+        echo -n "."
+        sleep 1
+    done
+    echo
 
-#
-# TODO: MB - Replace DB config check with metacat code to check for "autoconfig" flag at startup
-#
+    #
+    # TODO: Replace DB config check with internal metacat check for an "autoconfig" flag at startup
+    #
     # Login to Metacat Admin and start a session (cookie.txt)
-    echo "doing  curl -X POST with password=${METACAT_ADMINISTRATOR_PASSWORD}&username=${METACAT_ADMINISTRATOR_USERNAME} to localhost admin page"
+    echo "doing  curl -X POST to localhost admin page"
+    if [[ "$DEBUG" == "TRUE" ]]; then
+        echo "using password=${METACAT_ADMINISTRATOR_PASSWORD}\
+          & username=${METACAT_ADMINISTRATOR_USERNAME}"
+    fi
     curl -X POST \
         --data "loginAction=Login&configureType=login&processForm=true&password=${METACAT_ADMINISTRATOR_PASSWORD}&username=${METACAT_ADMINISTRATOR_USERNAME}" \
         --cookie-jar ./cookie.txt http://localhost:8080/"${METACAT_APP_CONTEXT}"/admin > /tmp/login_result.txt 2>&1
@@ -112,7 +125,7 @@ if [ "$1" = 'catalina.sh' ]; then
 
     ## If the DB needs to be updated run the migration scripts
     DB_CONFIGURED=$(grep -c "configureType=database" /tmp/login_result.txt || true)
-    if [ $DB_CONFIGURED -ne 0 ]; then
+    if [ "$DB_CONFIGURED" -ne 0 ]; then
         echo "Database needs configuring..."
         # Run the database initialization to create or upgrade tables
         # /${METACAT_APP_CONTEXT}/admin?configureType=database must have an authenticated session, then run
@@ -135,5 +148,5 @@ if [[ "$DEBUG" == "TRUE" ]]; then
     sh -c 'trap "exit" TERM; while true; do sleep 1; done'
 else
     echo "tailing logs in: $TC_HOME/logs/*"
-    exec tail -f $TC_HOME/logs/*
+    exec tail -f "$TC_HOME"/logs/*
 fi
