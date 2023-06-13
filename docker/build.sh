67f90b15177b9cae56e6d99a15ee807c3ad203e0
Matthew B <106352182+artntek@users.noreply.github.com>
2023-06-13 13:56:42 -0700

    docker build bug fixes & cleanup

--- docker/build.sh
+++ docker/build.sh
@@ -4,31 +4,33 @@ set -e
 TAG=$1
 DEFAULT_TAG="2.19.0"
 
-if [ -z "$1" ] || [[ "$1" == "true" ]]; then
+if [ -z "$1" ] || [[ "$1" == "-devtools" ]]; then
     TAG=${DEFAULT_TAG}
-    echo "Usage: $0  <tag>  <use_devtools>"
-    echo "or:    $0         <use_devtools>    (omitting TAG defaults it to ${$DEFAULT_TAG})"
-    echo "where: <tag>          is typically set to the metacat version. Setting to default: ${TAG}"
-    echo "       <use_devtools> FOR DEV & DEBUGGING ONLY - NOT FOR PRODUCTION USE!"
-    echo "                      Ignored unless the value is \"true\". Installs dev tools such as"
-    echo "                      vim, procps, lsof, and telnet in the container, thus REDUCING"
-    echo "                      ITS SECURITY (see Dockerfile for complete list)"
+    echo
+    echo "Usage: ${0} [tag] [-devtools]"
+    echo " or:   ${0}       [-devtools]    (omitting tag defaults it to ${DEFAULT_TAG})"
+    echo
+    echo "where:  tag       is typically set to the metacat version #. Setting to default: ${TAG}"
+    echo "       -devtools  is FOR DEV/DEBUGGING ONLY - NOT FOR PRODUCTION USE! Installs dev tools:"
+    echo "                    vim, procps, lsof, and telnet in the container (see Dockerfile for"
+    echo "                    complete list), thus REDUCING ITS SECURITY!"
+    echo
 fi
 
 DEVTOOLS="false"
-if [[ "$1" == "true" ]] || [[ "$2" == "true" ]]; then
+if [[ "$1" == "-devtools" ]] || [[ "$2" == "-devtools" ]]; then
     DEVTOOLS="true"
-    echo "DEVTOOLS mode is for DEV & DEBUGGING ONLY - NOT FOR PRODUCTION USE!"
-    echo "Installing dev tools such as vim, procps, lsof, and telnet in the container,"
-    echo "thus REDUCING ITS SECURITY (see Dockerfile for complete list)"
 fi
 echo "Building with \"DEVTOOLS\" set to: ${DEVTOOLS}"
+echo
 
 RELEASE="metacat-bin-${TAG}.tar.gz"
 
 # Grab the Metacat release
 if [ ! -f "../${RELEASE}" ]; then
-    echo "You must first build the metacat release with 'ant distbin'"
+    echo "Could not find ../${RELEASE}"
+    echo "You must first build the metacat release with 'ant distbin'. Exiting..."
+    exit 1
 fi
 
 cp ../"${RELEASE}" .
