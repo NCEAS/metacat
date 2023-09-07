32dd2d16492b751367873caefd75c058b6de4879
Matthew B <106352182+artntek@users.noreply.github.com>
2023-09-06 17:35:26 -0700

    fixed test to work without assuming property 'dataone.nodeName' is still default value

--- test/edu/ucsb/nceas/metacat/doi/datacite/DefaultDataCiteFactoryTest.java
+++ test/edu/ucsb/nceas/metacat/doi/datacite/DefaultDataCiteFactoryTest.java
@@ -3,6 +3,7 @@ package edu.ucsb.nceas.metacat.doi.datacite;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import org.dataone.configuration.Settings;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v2.SystemMetadata;
 import org.dataone.service.util.TypeMarshaller;
@@ -12,8 +13,18 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class DefaultDataCiteFactoryTest extends MCTestCase {
-    private static final String RESULT= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resource xmlns=\"http://datacite.org/schema/kernel-4\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://datacite.org/schema/kernel-4 https://schema.datacite.org/meta/kernel-4.3/metadata.xsd\"><identifier identifierType=\"DOI\">10.5063/F1X34VQ5</identifier><creators><creator><creatorName>CN=Monica Ihli A139616,O=Google,C=US,DC=cilogon,DC=org</creatorName></creator></creators><titles><title xml:lang=\"en\">(:unkn)</title></titles><publisher>My Metacat Node</publisher><publicationYear>2017</publicationYear><resourceType resourceTypeGeneral=\"Dataset\"/><formats><format>text/xml</format></formats></resource>";
-    
+    private static String RESULT =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resource xmlns=\"http://datacite"
+            + ".org/schema/kernel-4\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+            + "xsi:schemaLocation=\"http://datacite.org/schema/kernel-4 https://schema.datacite"
+            + ".org/meta/kernel-4.3/metadata.xsd\"><identifier identifierType=\"DOI\">10"
+            + ".5063/F1X34VQ5</identifier><creators><creator><creatorName>CN=Monica Ihli A139616,"
+            + "O=Google,C=US,DC=cilogon,DC=org</creatorName></creator></creators><titles><title "
+            + "xml:lang=\"en\">(:unkn)</title></titles><publisher>PUBLISHER_PLACEHOLDER"
+            + "</publisher><publicationYear>2017</publicationYear><resourceType "
+            + "resourceTypeGeneral=\"Dataset\"/><formats><format>text/xml</format></formats"
+            + "></resource>";
+
     public DefaultDataCiteFactoryTest (String name)  {
         super(name);
     }
@@ -25,21 +36,24 @@ public class DefaultDataCiteFactoryTest extends MCTestCase {
         suite.addTest(new DefaultDataCiteFactoryTest("testGenerateMetadata"));
         return suite;
     }
-    
+
     public void initialize() {
         assertTrue(true);
     }
-    
-    public void testGenerateMetadata() throws Exception{
+
+    public void testGenerateMetadata() throws Exception {
         Identifier guid = new Identifier();
         guid.setValue("doi:10.5063/F1X34VQ5");
         InputStream sysmetaInput = new FileInputStream("test/sysmeta-pangaea.xml");
-        SystemMetadata sysmeta = TypeMarshaller.unmarshalTypeFromStream(SystemMetadata.class, sysmetaInput);
+        SystemMetadata sysmeta =
+            TypeMarshaller.unmarshalTypeFromStream(SystemMetadata.class, sysmetaInput);
         sysmeta.setIdentifier(guid);
         DefaultDataCiteFactory factory = new DefaultDataCiteFactory();
         String metadata = factory.generateMetadata(guid, sysmeta);
-        System.out.println(""+metadata);
-        assertTrue(metadata.equals(RESULT));
+        System.out.println("" + metadata);
+        RESULT = RESULT.replace("PUBLISHER_PLACEHOLDER",
+                                Settings.getConfiguration().getString("dataone.nodeName"));
+        assertEquals("mismatch. \n\nEXPECTED:\n\n" + RESULT + "\n\nACTUAL:\n\n" + metadata + "\n\n",
+                     metadata, RESULT);
     }
-
 }
