8cb5b2a923a08eadec351562e5cf0557b38a7853
Matthew B <106352182+artntek@users.noreply.github.com>
2023-09-26 13:50:28 -0700

    whitespace only

--- src/edu/ucsb/nceas/metacat/admin/upgrade/UpdateDOI.java
+++ src/edu/ucsb/nceas/metacat/admin/upgrade/UpdateDOI.java
@@ -50,144 +50,144 @@ import edu.ucsb.nceas.metacat.util.DocumentUtil;
  *
  */
 public class UpdateDOI implements UpgradeUtilityInterface {
-    
-	private static Log log = LogFactory.getLog(UpdateDOI.class);
 
-	private int serverLocation = 1;
-	private String nodeId = null;
-	private String DOISCHEME = "doi:";
-	
-	/**
-	 * Public constructor
-	 * @throws Exception
-	 */
-	public UpdateDOI() throws Exception{
-	    Node node = MNodeService.getInstance(null).getCapabilities();
-	    nodeId = node.getIdentifier().getValue();
-	}
+    private static Log log = LogFactory.getLog(UpdateDOI.class);
 
-	public int getServerLocation() {
-		return serverLocation;
-	}
+    private int serverLocation = 1;
+    private String nodeId = null;
+    private String DOISCHEME = "doi:";
 
-	public void setServerLocation(int serverLocation) {
-		this.serverLocation = serverLocation;
-	}
-	
-	/** 
-	 * Update the registration of a list of DOIs
-	 * @param identifiers - DOIs to update
-	 */
-	private void updateDOIRegistration(List<String> identifiers) {
-		for (String pid: identifiers) {
-			try {
-				//Create an identifier and retrieve the SystemMetadata for this guid
-				Identifier identifier = new Identifier();
-				identifier.setValue(pid);
+    /**
+     * Public constructor
+     * @throws Exception
+     */
+    public UpdateDOI() throws Exception {
+        Node node = MNodeService.getInstance(null).getCapabilities();
+        nodeId = node.getIdentifier().getValue();
+    }
+
+    public int getServerLocation() {
+        return serverLocation;
+    }
+
+    public void setServerLocation(int serverLocation) {
+        this.serverLocation = serverLocation;
+    }
+
+    /**
+     * Update the registration of a list of DOIs
+     * @param identifiers - DOIs to update
+     */
+    private void updateDOIRegistration(List<String> identifiers) {
+        for (String pid : identifiers) {
+            try {
+                //Create an identifier and retrieve the SystemMetadata for this guid
+                Identifier identifier = new Identifier();
+                identifier.setValue(pid);
 				//SystemMetadata sysMeta = HazelcastService.getInstance().getSystemMetadataMap().get(identifier);
-				SystemMetadata sysMeta = SystemMetadataManager.getInstance().get(identifier);
-				if(sysMeta == null) {
+                SystemMetadata sysMeta = SystemMetadataManager.getInstance().get(identifier);
+                if (sysMeta == null) {
 				    //The identifier can be a sid, so the sysMeta can be null. we need to check if it is a sid.
-				    Identifier sid = new Identifier();
-				    sid.setValue(pid);
-				    Identifier head = IdentifierManager.getInstance().getHeadPID(sid);
-				    if(head != null) {
+                    Identifier sid = new Identifier();
+                    sid.setValue(pid);
+                    Identifier head = IdentifierManager.getInstance().getHeadPID(sid);
+                    if (head != null) {
 				        //sysMeta= HazelcastService.getInstance().getSystemMetadataMap().get(head);
-				        sysMeta= SystemMetadataManager.getInstance().get(head);
-				    }
-				}
-				
-				//Update the registration
-				if(sysMeta != null) {
-				    DOIServiceFactory.getDOIService().registerDOI(sysMeta);
-				}
-			} catch (Exception e) {
-				// what to do? nothing
-				e.printStackTrace();
-				continue;
-			}
-			
-		}
-	}
-	
-	/**
-	 * Update the DOI registration of all ids in this server with EML formatIds
-	 */
+                        sysMeta = SystemMetadataManager.getInstance().get(head);
+                    }
+                }
+
+                //Update the registration
+                if (sysMeta != null) {
+                    DOIServiceFactory.getDOIService().registerDOI(sysMeta);
+                }
+            } catch (Exception e) {
+                // what to do? nothing
+                e.printStackTrace();
+                continue;
+            }
+
+        }
+    }
+
+    /**
+     * Update the DOI registration of all ids in this server with EML formatIds
+     */
     public boolean upgrade() throws AdminException {
         boolean success = true;
         try {
-        	// get only local ids for this server
+            // get only local ids for this server
             List<String> idList = null;
-            
+
             idList = IdentifierManager.getInstance().getGUIDs(DocumentImpl.EML2_0_0NAMESPACE, nodeId, DOISCHEME);
             //Collections.sort(idList);
             updateDOIRegistration(idList);
-            
+
             idList = IdentifierManager.getInstance().getGUIDs(DocumentImpl.EML2_0_1NAMESPACE, nodeId, DOISCHEME);
             //Collections.sort(idList);
             updateDOIRegistration(idList);
-            
+
             idList = IdentifierManager.getInstance().getGUIDs(DocumentImpl.EML2_1_0NAMESPACE, nodeId, DOISCHEME);
             //Collections.sort(idList);
             updateDOIRegistration(idList);
-            
+
             idList = IdentifierManager.getInstance().getGUIDs(DocumentImpl.EML2_1_1NAMESPACE, nodeId, DOISCHEME);
             //Collections.sort(idList);
             updateDOIRegistration(idList);
-            
+
             idList = IdentifierManager.getInstance().getGUIDs(DocumentImpl.EML2_2_0NAMESPACE, nodeId, DOISCHEME);
             updateDOIRegistration(idList);
-            
-		} catch (Exception e) {
-			String msg = "Problem updating DOIs: " + e.getMessage();
-			log.error(msg, e);
-			success = false;
-			throw new AdminException(msg);
-		}
-        
-        
+
+        } catch (Exception e) {
+            String msg = "Problem updating DOIs: " + e.getMessage();
+            log.error(msg, e);
+            success = false;
+            throw new AdminException(msg);
+        }
+
+
         return success;
     }
-    
-	/**
-	 * Update the registration of all DOIs with the specified guids in this server
-	 * @param ids - a List of DOIs to update
-	 */
+
+    /**
+     * Update the registration of all DOIs with the specified guids in this server
+     * @param ids - a List of DOIs to update
+     */
     public boolean upgradeById(List<String> ids) throws AdminException {
         boolean success = true;
-        
-        try{
-        	updateDOIRegistration(ids);
-	    } catch (Exception e) {
-			String msg = "Problem updating DOIs: " + e.getMessage();
-			log.error(msg, e);
-			success = false;
-			throw new AdminException(msg);
-		}
+
+        try {
+            updateDOIRegistration(ids);
+        } catch (Exception e) {
+            String msg = "Problem updating DOIs: " + e.getMessage();
+            log.error(msg, e);
+            success = false;
+            throw new AdminException(msg);
+        }
         return success;
     }
-    
+
     /**
      * Update the registration of all DOIs in this server with the specified formatId
-     * @param formatIds - a List of formatIDs used to filter DOI selection 
+     * @param formatIds - a List of formatIDs used to filter DOI selection
      */
     public boolean upgradeByFormatId(List<String> formatIds) throws AdminException {
-        boolean success = true;  
-        try{
-        	for (String formatId: formatIds) {        		
-	        	//Get all the docids with this formatId
+        boolean success = true;
+        try {
+            for (String formatId : formatIds) {
+                //Get all the docids with this formatId
         		List<String> idList = IdentifierManager.getInstance().getGUIDs(formatId, nodeId, DOISCHEME);
-	        	//Update the registration for all these guids
-	        Collections.sort(idList);
-	        updateDOIRegistration(idList);
-        	}
-	    } catch (Exception e) {
-			String msg = "Problem updating DOIs: " + e.getMessage();
-			log.error(msg, e);
-			success = false;
-			throw new AdminException(msg);
-		}
+                //Update the registration for all these guids
+                Collections.sort(idList);
+                updateDOIRegistration(idList);
+            }
+        } catch (Exception e) {
+            String msg = "Problem updating DOIs: " + e.getMessage();
+            log.error(msg, e);
+            success = false;
+            throw new AdminException(msg);
+        }
         return success;
     }
-    
+
 }
