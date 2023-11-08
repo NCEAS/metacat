a89605399b97922afdd56e3136b3cff8d964e119
Matt Jones <gitcode@magisa.org>
2023-11-07 21:37:22 -0900

    Remove mermaid from build for now.

--- docs/user/metacat/source/conf.py
+++ docs/user/metacat/source/conf.py
@@ -30,7 +30,8 @@ with open("../../../../lib/metacat.properties") as lines:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.mermaid']
+#extensions = ['sphinxcontrib.mermaid']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
