a606e7dab6fb308b67250b3fcf6952d4d92f4f50
Matthew B <106352182+artntek@users.noreply.github.com>
2023-12-18 16:29:44 -0800

    mocking Settings for k8s env compatibility

--- test/edu/ucsb/nceas/metacat/startup/MetacatInitializerIT.java
+++ test/edu/ucsb/nceas/metacat/startup/MetacatInitializerIT.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
+import org.apache.commons.configuration.Configuration;
 import org.dataone.configuration.Settings;
 import org.junit.After;
 import org.junit.Before;
@@ -13,7 +14,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import edu.ucsb.nceas.LeanTestUtils;
@@ -104,29 +110,57 @@ public class MetacatInitializerIT {
         String originHost = PropertyService.getProperty("index.rabbitmq.hostname");
         long time = System.currentTimeMillis();
         String newHost = originHost + time;
-        Settings.getConfiguration().setProperty("index.rabbitmq.hostname", newHost);
-        IndexGenerator.refreshInstance();
-        MetacatInitializer metacatInitializer = new MetacatInitializer();
-        try {
+
+        Configuration mockD1Config = createMockConfig(newHost);
+
+        try (MockedStatic<Settings> mock = Mockito.mockStatic(Settings.class)) {
+
+            mock.when(Settings::getConfiguration).thenReturn(mockD1Config);
+
+            IndexGenerator.refreshInstance();
+            MetacatInitializer metacatInitializer = new MetacatInitializer();
+            try {
+                metacatInitializer.contextInitialized(event);
+                fail("The initialization should fail and the test cannot get here");
+            } catch (RuntimeException e) {
+                assertTrue(e.getMessage().contains(Long.toString(time)));
+            }
+
+            // set Metacat non-configured temporarily.
+            // Non-configured Metacat wouldn't throw an exception.
+            PropertyService.setPropertyNoPersist(
+                "configutil.skinsConfigured", PropertyService.UNCONFIGURED);
+            metacatInitializer = new MetacatInitializer();
             metacatInitializer.contextInitialized(event);
-            fail("The initialization should fail and the test cannot get here");
-        } catch (RuntimeException e) {
-            assertTrue(e.getMessage().contains(Long.toString(time)));
+            assertFalse(metacatInitializer.isFullyInitialized());
         }
-
-        // set Metacat non-configured temporarily.
-        // Non-configured Metacat wouldn't throw an exception.
-        PropertyService
-             .setPropertyNoPersist("configutil.skinsConfigured", PropertyService.UNCONFIGURED);
-        metacatInitializer = new MetacatInitializer();
-        metacatInitializer.contextInitialized(event);
-        assertFalse(metacatInitializer.isFullyInitialized());
-
-        // set the original properties back
-        Settings.getConfiguration().setProperty("index.rabbitmq.hostname", originHost);
         PropertyService
             .setPropertyNoPersist("configutil.skinsConfigured", PropertyService.CONFIGURED);
         IndexGenerator.refreshInstance();
+    }
+
+    private static Configuration createMockConfig(String newHost) {
+
+        Configuration mockD1Config = Mockito.mock(Configuration.class);
+
+        // override hostname...
+        when(mockD1Config.getString(eq("index.rabbitmq.hostname"), anyString())).thenReturn(
+            newHost);
+
+        // ...but for remaining values, retrieve those that are already in Settings
+        int port = Settings.getConfiguration().getInt("index.rabbitmq.hostport", 5672);
+        String username = Settings.getConfiguration().getString("index.rabbitmq.username", "guest");
+        String password = Settings.getConfiguration().getString("index.rabbitmq.password", "guest");
+        int priority = Settings.getConfiguration().getInt("index.rabbitmq.max.priority");
+
+        when(mockD1Config.getInt(eq("index.rabbitmq.hostport"), anyInt())).thenReturn(port);
+        when(mockD1Config.getString(eq("index.rabbitmq.username"), anyString())).thenReturn(
+            username);
+        when(mockD1Config.getString(eq("index.rabbitmq.password"), anyString())).thenReturn(
+            password);
+        when(mockD1Config.getInt(eq("index.rabbitmq.max.priority"))).thenReturn(priority);
+
+        return mockD1Config;
     }
 
     /**
@@ -136,17 +170,17 @@ public class MetacatInitializerIT {
      */
     private ServletContextEvent getMockServletContextEvent() throws PropertyNotFoundException {
         ServletContext scMock = Mockito.mock(ServletContext.class);
-        Mockito.when(scMock.getRealPath("/")).thenReturn(SystemUtil.getContextDir());
-        Mockito.when(scMock.getRealPath("/WEB-INF"))
+        when(scMock.getRealPath("/")).thenReturn(SystemUtil.getContextDir());
+        when(scMock.getRealPath("/WEB-INF"))
                                         .thenReturn(SystemUtil.getContextDir() + "/WEB-INF");
-        Mockito.when(scMock.getRealPath("WEB-INF"))
+        when(scMock.getRealPath("WEB-INF"))
                                         .thenReturn(SystemUtil.getContextDir() + "/WEB-INF");
-        Mockito.when(scMock.getRealPath("/style/skins"))
+        when(scMock.getRealPath("/style/skins"))
                                         .thenReturn(SystemUtil.getContextDir() + "/style/skins");
-        Mockito.when(scMock.getAttribute("APPLICATION_NAME"))
+        when(scMock.getAttribute("APPLICATION_NAME"))
                                                 .thenReturn(MetacatInitializer.APPLICATION_NAME);
         ServletContextEvent servletContextEventMock = Mockito.mock(ServletContextEvent.class);
-        Mockito.when(servletContextEventMock.getServletContext()).thenReturn(scMock);
+        when(servletContextEventMock.getServletContext()).thenReturn(scMock);
         return servletContextEventMock;
     }
 
