ea91c92f3b4ea7b1c6bce63e1d91b7fca11111f4
Jing Tao <tao@nceas.ucsb.edu>
2024-01-22 14:46:25 -0800

    Remove some class variables and methods which were not used.

--- src/edu/ucsb/nceas/metacat/accesscontrol/AccessControlList.java
+++ src/edu/ucsb/nceas/metacat/accesscontrol/AccessControlList.java
@@ -1,30 +1,16 @@
 package edu.ucsb.nceas.metacat.accesscontrol;
 
 import edu.ucsb.nceas.metacat.BasicNode;
-import edu.ucsb.nceas.metacat.DBEntityResolver;
-import edu.ucsb.nceas.metacat.DocumentImpl;
-import edu.ucsb.nceas.metacat.McdbException;
 import edu.ucsb.nceas.metacat.database.DBConnection;
 import edu.ucsb.nceas.metacat.database.DBConnectionPool;
-import edu.ucsb.nceas.metacat.properties.PropertyService;
-import edu.ucsb.nceas.utilities.PropertyNotFoundException;
 import edu.ucsb.nceas.utilities.access.AccessControlInterface;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.xml.sax.Attributes;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.DTDHandler;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
 import org.xml.sax.ext.LexicalHandler;
 import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.helpers.XMLReaderFactory;
 
-import java.io.IOException;
-import java.io.StringReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -46,25 +32,14 @@ public class AccessControlList extends DefaultHandler
     private String sep;
 
     private boolean processingDTD;
-    private String user;
-    private String[] groups;
     private String aclid;
-    private int rev;
     private String docname;
-    private String doctype;
-    private String systemid;
-
-    private String docurl;
-    private Vector resourceURL;
-    private Vector resourceID;
     private Vector principal;
     private int permission;
-    private String permType;
     private String permOrder;
     private String beginTime;
     private String endTime;
     private int ticketCount;
-    private int serverCode = 1;
 
     private Vector aclObjects = new Vector();
     private boolean instarttag = true;
@@ -72,95 +47,6 @@ public class AccessControlList extends DefaultHandler
 
     private static Log logMetacat = LogFactory.getLog(AccessControlList.class);
 
-    /**
-     * Construct an instance of the AccessControlList class.
-     * It is used by the permission check up from DBQuery or DocumentImpl
-     * and from "getaccesscontrol" action
-     *
-     * @param conn the JDBC connection where acl info is get
-     */
-    public AccessControlList(DBConnection conn) throws SQLException {
-        this.connection = conn;
-    }
-
-
-    /**
-     * Construct an instance of the AccessControlList class.
-     * It parse acl file and loads acl data into db connection.
-     *
-     * @param conn the JDBC connection where acl data are loaded
-     * @param aclid the Accession# of the document with the acl data
-     * @param acl the acl file containing acl data
-     * @param user the user connected to MetaCat servlet and owns the document
-     * @param groups the groups to which user belongs
-     * @param serverCode the serverid from xml_replication on which this document
-     *        resides.
-     */
-    public AccessControlList(
-        DBConnection conn, String aclid, int rev, String user, String[] groups, int serverCode)
-        throws SAXException, IOException, McdbException, PropertyNotFoundException {
-        String parserName = PropertyService.getProperty("xml.saxparser");
-        //this.server = SystemUtil.getSecureServerURL();
-        this.sep = PropertyService.getProperty("document.accNumSeparator");
-
-        this.connection = conn;
-        this.parserName = parserName;
-        this.processingDTD = false;
-        this.elementStack = new Stack();
-
-        this.user = user;
-        this.groups = groups;
-        this.aclid = aclid;
-        this.resourceURL = new Vector();
-        this.resourceID = new Vector();
-        this.principal = new Vector();
-        this.permission = 0;
-        this.ticketCount = 0;
-        // this.publicAcc = null;
-        this.serverCode = serverCode;
-
-        // read the access file from db connection
-        DocumentImpl acldoc = new DocumentImpl(aclid + sep + rev);
-        String acl = acldoc.toString();
-        this.rev = acldoc.getRev();
-
-        // Initialize the parse
-        XMLReader parser = initializeParser();
-        // parse the access file and write the info to xml_access
-        if (acl != null) {
-            parser.parse(new InputSource(new StringReader(acl)));
-        } else {
-            throw new McdbException(
-                "Could not retrieve access control list for:  " + aclid + sep + rev);
-        }
-    }
-
-    /* Set up the SAX parser for reading the XML serialized ACL */
-    private XMLReader initializeParser() throws SAXException {
-        XMLReader parser = null;
-
-        // Get an instance of the parser
-        parser = XMLReaderFactory.createXMLReader(parserName);
-
-        // Turn off validation
-        parser.setFeature("http://xml.org/sax/features/validation", true);
-        parser.setProperty("http://xml.org/sax/properties/lexical-handler", this);
-
-        // Set Handlers in the parser
-        // Set the ContentHandler to this instance
-        parser.setContentHandler((ContentHandler) this);
-
-        // make a DBEntityResolver instance
-        // Set the EntityResolver to DBEntityResolver instance
-        EntityResolver eresolver = new DBEntityResolver(connection, this, null);
-        parser.setEntityResolver((EntityResolver) eresolver);
-        parser.setDTDHandler((DTDHandler) this);
-
-        // Set the ErrorHandler to this instance
-        parser.setErrorHandler((ErrorHandler) this);
-
-        return parser;
-    }
 
     /**
      * Callback method used by the SAX Parser when beginning of the document
@@ -308,8 +194,6 @@ public class AccessControlList extends DefaultHandler
         logMetacat.debug("AccessControlList.startDTD - Setting processingDTD to true");
         logMetacat.debug("AccessControlList.startDTD - start DTD");
         docname = name;
-        doctype = publicId;
-        systemid = systemId;
     }
 
     /**
@@ -462,49 +346,6 @@ public class AccessControlList extends DefaultHandler
         }
     }
 
-    /* Get permissions with permission order different than @permOrder. */
-    private int getPermissions(
-        int permission, String principal, String docid, String permOrder) throws SQLException {
-        PreparedStatement pstmt = null;
-        DBConnection conn = null;
-        int serialNumber = -1;
-        try {
-            //check out DBConnection
-            conn = DBConnectionPool.getDBConnection("AccessControlList.getPermissions");
-            serialNumber = conn.getCheckOutSerialNumber();
-            pstmt = conn.prepareStatement("SELECT permission FROM xml_access " + "WHERE docid = ? "
-                                              + "AND principal_name = ? "
-                                              + "AND perm_order NOT = ?");
-            pstmt.setString(1, docid);
-            pstmt.setString(2, principal);
-            pstmt.setString(3, permOrder);
-            logMetacat.debug("running sql: " + pstmt);
-            pstmt.execute();
-            ResultSet rs = pstmt.getResultSet();
-            boolean hasRow = rs.next();
-            int perm = 0;
-            while (hasRow) {
-                perm = rs.getInt(1);
-                perm = permission & perm;
-                if (perm != 0) {
-                    pstmt.close();
-                    return perm;
-                }
-                hasRow = rs.next();
-            }
-        }//try
-        catch (SQLException e) {
-            throw e;
-        } finally {
-            try {
-                pstmt.close();
-            } finally {
-                DBConnectionPool.returnDBConnection(conn, serialNumber);
-            }
-        }
-        return 0;
-    }
-
     /* Get the int value of READ, WRITE, CHMOD or ALL. */
     public static int intValue(String permission) {
 
@@ -561,43 +402,6 @@ public class AccessControlList extends DefaultHandler
         return txtPerm.toString();
     }
 
-  /* Get the flag for public "read" access for @docid from db conn. */
-  private String getPublicAccess(String docid) throws SQLException {
-
-    int publicAcc = 0;
-    PreparedStatement pstmt = null;
-    DBConnection conn = null;
-    int serialNumber = -1;
-    try
-    {
-      //check out DBConnection
-      conn=DBConnectionPool.getDBConnection("AccessControlList.getPublicAcces");
-      serialNumber=conn.getCheckOutSerialNumber();
-      pstmt = conn.prepareStatement("SELECT public_access FROM xml_documents " +
-                                  "WHERE docid = ?");
-      pstmt.setString(1, docid);
-      pstmt.execute();
-      ResultSet rs = pstmt.getResultSet();
-      boolean hasRow = rs.next();
-      if ( hasRow ) {
-        publicAcc = rs.getInt(1);
-      }
-
-      return (publicAcc == 1) ? "yes" : "no";
-    }
-    finally
-    {
-      try
-      {
-        pstmt.close();
-      }
-      finally
-      {
-        DBConnectionPool.returnDBConnection(conn, serialNumber);
-      }
-    }
-  }
-
 
   /**
      * SAX Handler that receives notification of comments in the DTD
@@ -619,71 +423,6 @@ public class AccessControlList extends DefaultHandler
      */
     public void endCDATA() throws SAXException {
         logMetacat.trace("AccessControlList.endCDATA - end CDATA");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("text value for CHMOD (" + CHMOD + "): " + txtValue(CHMOD));
-        System.out.println("text value for READ: (" + READ + "): " + txtValue(READ));
-        System.out.println("text value for WRITE: (" + WRITE + "): " + txtValue(WRITE));
-        System.out.println("text value for ALL: (" + ALL + "): " + txtValue(ALL));
-        int chmod_read = CHMOD | READ;
-        System.out.println(
-            "text value for CHMOD|READ: (" + chmod_read + "): " + txtValue(CHMOD | READ));
-        int chmod_write = CHMOD | WRITE;
-        System.out.println(
-            "text value for CHMOD|WRITE: (" + chmod_write + "): " + txtValue(CHMOD | WRITE));
-        int chmod_all = CHMOD | ALL;
-        System.out.println(
-            "text value for CHMOD|ALL: (" + chmod_all + "): " + txtValue(CHMOD | ALL));
-        int read_write = READ | WRITE;
-        System.out.println(
-            "text value for READ|WRITE: (" + read_write + "): " + txtValue(READ | WRITE));
-        int read_all = READ | ALL;
-        System.out.println("text value for READ|ALL: (" + read_all + "): " + txtValue(READ | ALL));
-        int write_all = WRITE | ALL;
-        System.out.println(
-            "text value for WRITE|ALL: (" + write_all + "): " + txtValue(WRITE | ALL));
-        int chmod_read_write = CHMOD | READ | WRITE;
-        System.out.println(
-            "text value for CHMOD|READ|WRITE: (" + chmod_read_write + "): " + txtValue(
-                CHMOD | READ | WRITE));
-        int chmod_read_all = CHMOD | READ | ALL;
-        System.out.println("text value for CHMOD|READ|ALL: (" + chmod_read_all + "): " + txtValue(
-            CHMOD | READ | ALL));
-        int chmod_write_all = CHMOD | WRITE | ALL;
-        System.out.println("text value for CHMOD|WRITE|ALL: (" + chmod_write_all + "): " + txtValue(
-            CHMOD | WRITE | ALL));
-        int read_write_all = READ | WRITE | ALL;
-        System.out.println("text value for READ|WRITE|ALL: (" + read_write_all + "): " + txtValue(
-            READ | WRITE | ALL));
-        int chmod_read_write_all = CHMOD | READ | WRITE | ALL;
-        System.out.println(
-            "text value for CHMOD|READ|WRITE|ALL: (" + chmod_read_write_all + "): " + txtValue(
-                CHMOD | READ | WRITE | ALL));
-        System.out.println();
-        System.out.println("int value for GOOBER: " + intValue("GOOBER"));
-        System.out.println("int value for CHANGEPERMISSION: " + intValue("CHANGEPERMISSION"));
-        System.out.println("int value for READ: " + intValue("READ"));
-        System.out.println("int value for WRITE: " + intValue("WRITE"));
-        System.out.println("int value for ALL: " + intValue("ALL"));
-        System.out.println(
-            "int value for CHANGEPERMISSION,READ: " + intValue("CHANGEPERMISSION,READ"));
-        System.out.println(
-            "int value for CHANGEPERMISSION,WRITE: " + intValue("CHANGEPERMISSION,WRITE"));
-        System.out.println(
-            "int value for CHANGEPERMISSION,ALL: " + intValue("CHANGEPERMISSION,ALL"));
-        System.out.println("int value for READ,WRITE: " + intValue("READ,WRITE"));
-        System.out.println("int value for READ,ALL: " + intValue("READ,ALL"));
-        System.out.println("int value for WRITE,ALL: " + intValue("WRITE,ALL"));
-        System.out.println("int value for CHANGEPERMISSION,READ,WRITE: " + intValue(
-            "CHANGEPERMISSION,READ,WRITE"));
-        System.out.println(
-            "int value for CHANGEPERMISSION,READ,ALL: " + intValue("CHANGEPERMISSION,READ,ALL"));
-        System.out.println(
-            "int value for CHANGEPERMISSION,READ,ALL: " + intValue("CHANGEPERMISSION,WRITE,ALL"));
-        System.out.println("int value for READ,WRITE,ALL: " + intValue("READ,WRITE,ALL"));
-        System.out.println("int value for CHANGEPERMISSION,READ,WRITE,ALL: " + intValue(
-            "CHANGEPERMISSION,READ,WRITE,ALL"));
     }
 
 }
