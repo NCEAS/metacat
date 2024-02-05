676c4933a9c73fbbb04fc11f12d3a354a6949c0f
Jing Tao <tao@nceas.ucsb.edu>
2024-02-05 13:58:22 -0800

    Add the test for updateIdMetadata.

--- test/edu/ucsb/nceas/metacat/restservice/v2/MNResourceHandlerTest.java
+++ test/edu/ucsb/nceas/metacat/restservice/v2/MNResourceHandlerTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+
 import static org.mockito.ArgumentMatchers.any;
 
 import java.util.List;
@@ -49,7 +50,11 @@ public class MNResourceHandlerTest {
     private static final String DECODED_PID =
                              "http://dx.doi.org/10.5061/dryad.12?ver=2017-08-29T11:52:08.075-05:00";
     private static final String URN_PID = "urn:uuid:de8528af-3636-44e7-8db5-ce5c6ac95770";
-    
+    private static final String DOI1 = "doi:10.5072/FK2FR01T7X1";
+    private static final String DOI2 = "doi:10.5072/FK2T155C0Q3";
+    private static final String EML2_NAMESPACE = "eml://ecoinformatics.org/eml-2.0.0";
+    private static final String EML201_NAMESPACE = "eml://ecoinformatics.org/eml-2.0.1";
+
     /**
      * Setup
      */
@@ -78,7 +83,7 @@ public class MNResourceHandlerTest {
         id.setValue(DECODED_PID);
         ids = new Vector<Identifier>();
         ids.add(id);
-        request.setURL("/"+ index+ "/" + ENCODED_PID);
+        request.setURL("/" + index + "/" + ENCODED_PID);
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -102,7 +107,7 @@ public class MNResourceHandlerTest {
         Identifier id1 = new Identifier();
         id1.setValue(URN_PID);
         ids.add(id1);
-        request.setURL("/"+ index+ "?pid=" + ENCODED_PID + "&pid=" + URN_PID);
+        request.setURL("/" + index + "?pid=" + ENCODED_PID + "&pid=" + URN_PID);
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -119,7 +124,7 @@ public class MNResourceHandlerTest {
 
         //test /index/?pid=pid1&pid=pid2
         request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
-        request.setURL("/"+ index+ "/?pid=" + ENCODED_PID + "&pid=" + URN_PID);
+        request.setURL("/" + index + "/?pid=" + ENCODED_PID + "&pid=" + URN_PID);
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -136,7 +141,7 @@ public class MNResourceHandlerTest {
 
         //test /index/?all=false&pid=pid1&pid=pid2
         request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
-        request.setURL("/"+ index+ "/?all=false&pid=" + ENCODED_PID + "&pid=" + URN_PID);
+        request.setURL("/" + index + "/?all=false&pid=" + ENCODED_PID + "&pid=" + URN_PID);
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -170,7 +175,7 @@ public class MNResourceHandlerTest {
 
         //test /index/?all=true
         request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
-        request.setURL("/"+ index+ "/?all=true");
+        request.setURL("/" + index + "/?all=true");
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -188,7 +193,7 @@ public class MNResourceHandlerTest {
 
         //test /index?all=true
         request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
-        request.setURL("/"+ index+ "?all=true");
+        request.setURL("/" + index + "?all=true");
         try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
             MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
             Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
@@ -211,6 +216,167 @@ public class MNResourceHandlerTest {
      */
     @Test
     public void testUpdateIdMetadata() throws Exception {
+        String identifiers = "identifiers";
+        String[] ids = null;
+        String[] formats = null;
+        Session session = null;
+
+        // test /identifiers/pid
+        ids = new String[1];
+        ids[0] = DOI1;
+        request.setURL("/" + identifiers + "/" + DOI1);
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            //updateIdMetadata doesn't have GET and POST method
+            resourceHandler.handle(GET);
+            Mockito.verify(mockMNodeService, Mockito.times(0))
+                                                        .updateIdMetadata(session, ids, formats);
+            resourceHandler.handle(POST);
+            Mockito.verify(mockMNodeService, Mockito.times(0))
+                                                        .updateIdMetadata(session, ids, formats);
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers/?pid=pid1&pid=pid2
+        ids = new String[2];
+        ids[0] = DOI1;
+        ids[1] = DOI2;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "/?pid=" + DOI1 + "&pid=" + DOI2);
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers?pid=pid1&pid=pid2
+        ids = new String[2];
+        ids[0] = DOI1;
+        ids[1] = DOI2;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "?pid=" + DOI1 + "&pid=" + DOI2);
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers?formatId=format1&formatId=format2
+        ids = null;
+        formats = new String[2];
+        formats[0] = EML2_NAMESPACE;
+        formats[1] = EML201_NAMESPACE;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "?formatId=" + EML2_NAMESPACE
+                                                    + "&formatId=" + EML201_NAMESPACE);
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers?formatId=format1&formatId=format2&pid=pid1
+        ids = new String[1];
+        ids[0] = DOI1;
+        formats = new String[2];
+        formats[0] = EML2_NAMESPACE;
+        formats[1] = EML201_NAMESPACE;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "?formatId=" + EML2_NAMESPACE
+                                           + "&formatId=" + EML201_NAMESPACE + "&pid=" + DOI1);
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers?formatId=format1&formatId=format2&pid=pid1&all=false;
+        ids = new String[1];
+        ids[0] = DOI1;
+        formats = new String[2];
+        formats[0] = EML2_NAMESPACE;
+        formats[1] = EML201_NAMESPACE;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "?formatId=" + EML2_NAMESPACE
+                             + "&formatId=" + EML201_NAMESPACE + "&pid=" + DOI1 + "&all=false");
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(1))
+                                                        .updateIdMetadata(session, ids, formats);
+            Mockito.verify(mockMNodeService, Mockito.times(0)).updateAllIdMetadata(session);
+        }
+
+        // test /identifiers?all=true;
+        request = new MockHttpServletRequest(null, new MockHttpSession(context), context);
+        request.setURL("/" + identifiers + "?all=true");
+        try (MockedStatic<MNodeService> ignored = Mockito.mockStatic(MNodeService.class)) {
+            MNodeService mockMNodeService = Mockito.mock(MNodeService.class);
+            Mockito.when(MNodeService.getInstance(request)).thenReturn(mockMNodeService);
+            Mockito.when(mockMNodeService.updateIdMetadata(any(Session.class), any(String[].class),
+                                                    any(String[].class))).thenReturn(Boolean.TRUE);
+            Mockito.when(mockMNodeService.updateAllIdMetadata(any(Session.class)))
+                                                                        .thenReturn(Boolean.TRUE);
+            resourceHandler =
+                    new MNResourceHandler(context, request, new MockHttpServletResponse(request));
+            resourceHandler.handle(PUT);
+            Mockito.verify(mockMNodeService, Mockito.times(0))
+                    .updateIdMetadata(any(Session.class), any(String[].class), any(String[].class));
+            Mockito.verify(mockMNodeService, Mockito.times(1)).updateAllIdMetadata(session);
+        }
     }
 
 }
