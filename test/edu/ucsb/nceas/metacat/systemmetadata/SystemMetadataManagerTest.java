a2705d7fd816666f6db27de798fe3d6c95ce7a3a
Jing Tao <tao@nceas.ucsb.edu>
2024-02-27 12:59:31 -0800

    Change them to junit 4.

--- test/edu/ucsb/nceas/metacat/systemmetadata/SystemMetadataManagerTest.java
+++ test/edu/ucsb/nceas/metacat/systemmetadata/SystemMetadataManagerTest.java
@@ -1,21 +1,3 @@
-/**
- *  Copyright: 2023 Regents of the University of California and the
- *             National Center for Ecological Analysis and Synthesis
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 package edu.ucsb.nceas.metacat.systemmetadata;
 
 import java.io.ByteArrayInputStream;
@@ -23,6 +5,7 @@ import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.wicket.protocol.http.mock.MockHttpServletRequest;
 import org.dataone.service.exceptions.InvalidSystemMetadata;
 import org.dataone.service.types.v1.AccessPolicy;
 import org.dataone.service.types.v1.AccessRule;
@@ -33,38 +16,40 @@ import org.dataone.service.types.v1.Subject;
 import org.dataone.service.types.v2.MediaType;
 import org.dataone.service.types.v2.MediaTypeProperty;
 import org.dataone.service.types.v2.SystemMetadata;
+import org.junit.Before;
+import org.junit.Test;
 
+import edu.ucsb.nceas.LeanTestUtils;
 import edu.ucsb.nceas.metacat.IdentifierManager;
 import edu.ucsb.nceas.metacat.database.DBConnection;
 import edu.ucsb.nceas.metacat.database.DBConnectionPool;
 import edu.ucsb.nceas.metacat.dataone.D1NodeServiceTest;
 import edu.ucsb.nceas.metacat.dataone.MNodeService;
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
-public class SystemMetadataManagerTest extends D1NodeServiceTest {
-    
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class SystemMetadataManagerTest {
+
+    private D1NodeServiceTest d1NodeTester;
+    private MockHttpServletRequest request;
+
     /**
-     * Constructor
-     * @param name
+     * Set up
+     * @throws Exception
      */
-    public SystemMetadataManagerTest(String name) {
-        super(name);
+    @Before
+    public void setUp() throws Exception {
+        LeanTestUtils.initializePropertyService(LeanTestUtils.PropertiesMode.LIVE_TEST);
+        d1NodeTester = new D1NodeServiceTest("initialize");
+        request = (MockHttpServletRequest)d1NodeTester.getServletRequest();
     }
-    
-    /**
-     * Create a suite of tests to be run together
-     */
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(new SystemMetadataManagerTest("testMediaType"));
-        suite.addTest(new SystemMetadataManagerTest("testUpdateSystemmetadata"));
-        return suite;
-    }
-    
+
+
     /**
      * Method to test new system metadata field such as media type and file name.
      */
+    @Test
     public void testMediaType() throws Exception {
         String fileName = "new file name";
         String name = "text/plain";
@@ -73,30 +58,29 @@ public class SystemMetadataManagerTest extends D1NodeServiceTest {
         String p2Name = "n2";
         String p2Value = "v2";
         IdentifierManager im = IdentifierManager.getInstance();
-        
+
         //test system metadata write/read without mediatype and file name.
         String docid = "test." + new Date().getTime() + ".1";
         String guid = "guid:" + docid;
         //create a mapping (identifier-docid)
         im.createMapping(guid, docid);
-        Session session = getTestSession();
+        Session session = d1NodeTester.getTestSession();
         Identifier id = new Identifier();
         id.setValue(guid);
         InputStream object = new ByteArrayInputStream("test".getBytes("UTF-8"));
-        SystemMetadata sysmeta = createSystemMetadata(id, session.getSubject(), object);
-        //sysmeta.setFileName(fileName);
+        SystemMetadata sysmeta =
+                           D1NodeServiceTest.createSystemMetadata(id, session.getSubject(), object);
         SystemMetadataManager.getInstance().store(sysmeta);
         SystemMetadata read = im.getSystemMetadata(guid);
         assertTrue(read.getIdentifier().equals(id));
         assertTrue(read.getFileName() == null);
         assertTrue(read.getMediaType() == null);
         //remove the system metadata
-        im.deleteSystemMetadata(guid);
+        SystemMetadataManager.getInstance().delete(id);
         //remove the mapping
         im.removeMapping(guid, docid);
-        
-        
-      //test system metadata write/read with mediatype and file name.
+
+       //test system metadata write/read with mediatype and file name.
         Thread.sleep(1000);
         docid = "test." + new Date().getTime() + ".1";
         guid = "guid:" + docid;
@@ -105,7 +89,7 @@ public class SystemMetadataManagerTest extends D1NodeServiceTest {
         id = new Identifier();
         id.setValue(guid);
         object = new ByteArrayInputStream("test".getBytes("UTF-8"));
-        sysmeta = createSystemMetadata(id, session.getSubject(), object);
+        sysmeta = D1NodeServiceTest.createSystemMetadata(id, session.getSubject(), object);
         sysmeta.setFileName(fileName);
         MediaType media = new MediaType();
         media.setName(name);
@@ -132,27 +116,29 @@ public class SystemMetadataManagerTest extends D1NodeServiceTest {
         MediaTypeProperty item2 = list.get(1);
         assertTrue(item2.getName().equals(p2Name));
         assertTrue(item2.getValue().equals(p2Value));
-        
+
         //Thread.sleep(100000);
         //remove the system metadata
-        im.deleteSystemMetadata(guid);
+        SystemMetadataManager.getInstance().delete(id);
         //remove the mapping
         im.removeMapping(guid, docid);
     }
-    
+
     /**
      * Test the updateSystemMetadata method should throw an IvalidSystemMetadata exception 
      * if the permission is wrongly spelled. 
      * https://github.com/NCEAS/metacat/issues/1323
      * @throws Exception
      */
+    @Test
     public void testUpdateSystemmetadata() throws Exception {
         String typoPermission = "typo";
-        Session session = getTestSession();
+        Session session = d1NodeTester.getTestSession();
         Identifier guid = new Identifier();
-        guid.setValue(generateDocumentId());
+        guid.setValue(d1NodeTester.generateDocumentId());
         InputStream object = new ByteArrayInputStream("test".getBytes("UTF-8"));
-        SystemMetadata sysmeta = createSystemMetadata(guid, session.getSubject(), object);
+        SystemMetadata sysmeta =
+                        D1NodeServiceTest.createSystemMetadata(guid, session.getSubject(), object);
         object = new ByteArrayInputStream("test".getBytes("UTF-8"));
         MNodeService.getInstance(request).create(session, guid, object, sysmeta);
         AccessPolicy policy = new AccessPolicy();
@@ -177,13 +163,13 @@ public class SystemMetadataManagerTest extends D1NodeServiceTest {
             } catch (InvalidSystemMetadata e) {
                 assertTrue(e.getMessage().contains(typoPermission));
             }
-            
+
         } finally {
             // Return db connection
             DBConnectionPool.returnDBConnection(dbConn, serialNumber);
         }
-        
+
     }
-    
+
 
 }
