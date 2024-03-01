848ffc131f9c4d1809d4fddb62dd870cac580f39
Jing Tao <tao@nceas.ucsb.edu>
2024-02-29 19:08:33 -0800

    Use the value from the test.properties file rather hard_coded values.

--- metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
+++ metacat-common/src/test/java/edu/ucsb/nceas/metacat/common/query/SolrQueryServiceControllerTest.java
@@ -12,6 +12,7 @@ import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.schema.SchemaField;
 import org.apache.solr.servlet.SolrRequestParsers;
+import org.dataone.configuration.Settings;
 import org.dataone.service.exceptions.NotFound;
 import org.dataone.service.exceptions.UnsupportedType;
 import org.junit.Test;
@@ -31,9 +32,11 @@ public class SolrQueryServiceControllerTest extends MetacatCommonTestBase {
     @Test
     public void testGetSolrSpecVersion() throws UnsupportedType, NotFound,
                             ParserConfigurationException, IOException, SAXException {
+        String targetVersion = Settings.getConfiguration().getString("solr.targetSpecVersion");
         String version = SolrQueryServiceController.getInstance().getSolrSpecVersion();
         assertTrue(version != null);
-        assertTrue("The version should be 8.11.3 rather than " + version, version.equals("8.11.3"));
+        assertTrue("The version should be " + targetVersion + " rather than " + version,
+                                                                    version.equals(targetVersion));
     }
 
 
@@ -47,11 +50,12 @@ public class SolrQueryServiceControllerTest extends MetacatCommonTestBase {
      */
     @Test
     public void testgetIndexSchemaFields() throws Exception {
-       Map<String, SchemaField> fields = SolrQueryServiceController
+        int targetSize = Settings.getConfiguration().getInt("solr.schemField.size");
+        Map<String, SchemaField> fields = SolrQueryServiceController
                                                        .getInstance().getIndexSchemaFields();
-       assertTrue(fields != null);
-       assertTrue("The number of index schema fields should be 165 rather than "
-                   + fields.size(), fields.size() == 165);
+        assertTrue(fields != null);
+        assertTrue("The number of index schema fields should be " + targetSize + " rather than "
+                   + fields.size(), fields.size() == targetSize);
     }
 
     /**
