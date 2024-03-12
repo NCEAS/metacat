2b5e67545bc4381036d1184eb1160570c5f0c0ce
Jing Tao <tao@nceas.ucsb.edu>
2024-03-12 11:36:17 -0700

    Format changes.

--- test/edu/ucsb/nceas/metacattest/DryadTest.java
+++ test/edu/ucsb/nceas/metacattest/DryadTest.java
@@ -1,28 +1,3 @@
-/**
- *  '$RCSfile$'
- *  Copyright: 2004 Regents of the University of California and the
- *              National Center for Ecological Analysis and Synthesis
- *  Purpose: To test the Access Controls in metacat by JUnit
- *
- *   '$Author$'
- *     '$Date$'
- * '$Revision$'
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 package edu.ucsb.nceas.metacattest;
 
 import java.io.InputStream;
@@ -33,7 +8,6 @@ import junit.framework.TestSuite;
 
 import org.apache.commons.io.IOUtils;
 import org.dataone.service.exceptions.InvalidRequest;
-import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.ObjectFormatIdentifier;
 import org.dataone.service.types.v1.Session;
@@ -81,60 +55,63 @@ public class DryadTest
     public void initialize() {
         assertTrue(1 == 1);
     }
-    
+
     /**
      * Insert test doc using D1 API
      */
     public void d1InsertDoc() {
-		try {
-	    	String docid = this.generateDocumentId();
-			String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC);
-			Session session = getTestSession();
-			Identifier pid = new Identifier();
-			pid.setValue(docid);
-			SystemMetadata sysmeta = this.createSystemMetadata(pid, session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
-			ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
-			formatId.setValue("http://datadryad.org/profile/v3.1");
-			sysmeta.setFormatId(formatId);
-			sysmeta.setSize(BigInteger.valueOf(documentContents.getBytes().length));
-			MNodeService.getInstance(request).create(session, pid, IOUtils.toInputStream(documentContents, "UTF-8"), sysmeta);
-			InputStream results = MNodeService.getInstance(request).get(session, pid);
-			String resultString = IOUtils.toString(results, "UTF-8");
-			assertEquals(documentContents, resultString);
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-    	
+        try {
+            String docid = this.generateDocumentId();
+            String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC);
+            Session session = getTestSession();
+            Identifier pid = new Identifier();
+            pid.setValue(docid);
+            SystemMetadata sysmeta = this.createSystemMetadata(pid,
+                       session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
+            ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
+            formatId.setValue("http://datadryad.org/profile/v3.1");
+            sysmeta.setFormatId(formatId);
+            sysmeta.setSize(BigInteger.valueOf(documentContents.getBytes().length));
+            MNodeService.getInstance(request).create(session, pid,
+                                    IOUtils.toInputStream(documentContents, "UTF-8"), sysmeta);
+            InputStream results = MNodeService.getInstance(request).get(session, pid);
+            String resultString = IOUtils.toString(results, "UTF-8");
+            assertEquals(documentContents, resultString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
     }
-    
+
     /**
      * Insert test doc using D1 API
      */
     public void d1InsertInvalidDoc() {
-		try {
-	    	String docid = this.generateDocumentId();
-			String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC_INVALID);
-			Session session = getTestSession();
-			Identifier pid = new Identifier();
-			pid.setValue(docid);
-			SystemMetadata sysmeta = this.createSystemMetadata(pid, session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
-			ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
-			formatId.setValue("http://datadryad.org/profile/v3.1");
-			sysmeta.setFormatId(formatId);
-			sysmeta.setSize(BigInteger.valueOf(documentContents.getBytes().length));
-			try {
-				MNodeService.getInstance(request).create(session, pid, IOUtils.toInputStream(documentContents, "UTF-8"), sysmeta);
-			} catch (Exception expectedException) {
-			    assertTrue(expectedException instanceof InvalidRequest);
-				return;
-			}
-			fail("Should not allow inserting invalid Dryad content");
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-    	
+        try {
+            String docid = this.generateDocumentId();
+            String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC_INVALID);
+            Session session = getTestSession();
+            Identifier pid = new Identifier();
+            pid.setValue(docid);
+            SystemMetadata sysmeta = this.createSystemMetadata(pid, session.getSubject(),
+                                                IOUtils.toInputStream(documentContents, "UTF-8"));
+            ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
+            formatId.setValue("http://datadryad.org/profile/v3.1");
+            sysmeta.setFormatId(formatId);
+            sysmeta.setSize(BigInteger.valueOf(documentContents.getBytes().length));
+            try {
+                MNodeService.getInstance(request).create(session, pid,
+                                        IOUtils.toInputStream(documentContents, "UTF-8"), sysmeta);
+            } catch (Exception expectedException) {
+                assertTrue(expectedException instanceof InvalidRequest);
+                return;
+            }
+            fail("Should not allow inserting invalid Dryad content");
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+
     }
-    
+
 }
