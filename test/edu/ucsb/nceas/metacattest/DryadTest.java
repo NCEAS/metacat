b363c8f5cd1123c06b117c0b4aad717cb063362f
Jing Tao <tao@nceas.ucsb.edu>
2024-03-12 11:51:05 -0700

    Change it to junit4.

--- test/edu/ucsb/nceas/metacattest/DryadTest.java
+++ test/edu/ucsb/nceas/metacattest/DryadTest.java
@@ -3,8 +3,7 @@ package edu.ucsb.nceas.metacattest;
 import java.io.InputStream;
 import java.math.BigInteger;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
 import org.dataone.service.exceptions.InvalidRequest;
@@ -12,62 +11,51 @@ import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.ObjectFormatIdentifier;
 import org.dataone.service.types.v1.Session;
 import org.dataone.service.types.v2.SystemMetadata;
+import org.junit.Before;
+import org.junit.Test;
 
+import edu.ucsb.nceas.LeanTestUtils;
+import edu.ucsb.nceas.MCTestCase;
 import edu.ucsb.nceas.metacat.dataone.D1NodeServiceTest;
 import edu.ucsb.nceas.metacat.dataone.MNodeService;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * A JUnit test for testing Dryad documents
  */
-public class DryadTest
-    extends D1NodeServiceTest {
-    
+public class DryadTest {
     private static final String DRYAD_TEST_DOC = "test/dryad-metadata-profile-sample.xml";
-    
     private static final String DRYAD_TEST_DOC_INVALID = "test/dryad-metadata-profile-invalid.xml";
+    private D1NodeServiceTest d1NodeTest;
+    private HttpServletRequest request;
 
     /**
-     * Constructor to build the test
-     *
-     * @param name the name of the test method
+     * Establish a testing framework by initializing appropriate objects
+     * @throws Exception
      */
-    public DryadTest(String name) {
-        super(name);
-    }
-
-    /**
-     * Create a suite of tests to be run together
-     */
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(new DryadTest("initialize"));
-        // Test basic functions
-        suite.addTest(new DryadTest("d1InsertDoc"));
-        suite.addTest(new DryadTest("d1InsertInvalidDoc"));
-
-        return suite;
-    }
-
-    /**
-     * Run an initial test that always passes to check that the test
-     * harness is working.
-     */
-    public void initialize() {
-        assertTrue(1 == 1);
+    @Before
+    public void setUp() throws Exception {
+        LeanTestUtils.initializePropertyService(LeanTestUtils.PropertiesMode.UNIT_TEST);
+        d1NodeTest = new D1NodeServiceTest("initialize");
+        request = d1NodeTest.getServletRequest();
     }
 
     /**
      * Insert test doc using D1 API
      */
+    @Test
     public void d1InsertDoc() {
         try {
-            String docid = this.generateDocumentId();
-            String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC);
-            Session session = getTestSession();
+            String docid = MCTestCase.generateDocumentId();
+            String documentContents = MCTestCase.getTestDocFromFile(DRYAD_TEST_DOC);
+            Session session = d1NodeTest.getTestSession();
             Identifier pid = new Identifier();
             pid.setValue(docid);
-            SystemMetadata sysmeta = this.createSystemMetadata(pid,
-                       session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
+            SystemMetadata sysmeta = D1NodeServiceTest.createSystemMetadata(pid,
+                            session.getSubject(), IOUtils.toInputStream(documentContents, "UTF-8"));
             ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
             formatId.setValue("http://datadryad.org/profile/v3.1");
             sysmeta.setFormatId(formatId);
@@ -78,7 +66,6 @@ public class DryadTest
             String resultString = IOUtils.toString(results, "UTF-8");
             assertEquals(documentContents, resultString);
         } catch (Exception e) {
-            e.printStackTrace();
             fail(e.getMessage());
         }
     }
@@ -86,14 +73,15 @@ public class DryadTest
     /**
      * Insert test doc using D1 API
      */
+    @Test
     public void d1InsertInvalidDoc() {
         try {
-            String docid = this.generateDocumentId();
-            String documentContents = this.getTestDocFromFile(DRYAD_TEST_DOC_INVALID);
-            Session session = getTestSession();
+            String docid = MCTestCase.generateDocumentId();
+            String documentContents = MCTestCase.getTestDocFromFile(DRYAD_TEST_DOC_INVALID);
+            Session session = d1NodeTest.getTestSession();
             Identifier pid = new Identifier();
             pid.setValue(docid);
-            SystemMetadata sysmeta = this.createSystemMetadata(pid, session.getSubject(),
+            SystemMetadata sysmeta = D1NodeServiceTest.createSystemMetadata(pid, session.getSubject(),
                                                 IOUtils.toInputStream(documentContents, "UTF-8"));
             ObjectFormatIdentifier formatId = new ObjectFormatIdentifier();
             formatId.setValue("http://datadryad.org/profile/v3.1");
@@ -108,7 +96,6 @@ public class DryadTest
             }
             fail("Should not allow inserting invalid Dryad content");
         } catch (Exception e) {
-            e.printStackTrace();
             fail(e.getMessage());
         }
 
