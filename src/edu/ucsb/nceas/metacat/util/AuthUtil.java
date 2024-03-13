9825d401ba910cec11357044393ac794be40afc8
Matthew B <106352182+artntek@users.noreply.github.com>
2024-03-13 16:21:03 -0700

    fix to use full orcid

--- src/edu/ucsb/nceas/metacat/util/AuthUtil.java
+++ src/edu/ucsb/nceas/metacat/util/AuthUtil.java
@@ -28,10 +28,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
-import com.hp.hpl.jena.rdf.model.EmptyListUpdateException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.xpath.operations.Bool;
 import org.dataone.portal.PortalCertificateManager;
 import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.v1.Group;
@@ -42,7 +40,6 @@ import org.dataone.service.types.v1.SubjectInfo;
 
 import edu.ucsb.nceas.metacat.dataone.D1AuthHelper;
 import edu.ucsb.nceas.metacat.properties.PropertyService;
-import edu.ucsb.nceas.metacat.service.SessionService;
 import edu.ucsb.nceas.metacat.shared.MetacatUtilException;
 import edu.ucsb.nceas.utilities.PropertyNotFoundException;
 
@@ -58,22 +55,8 @@ public class AuthUtil {
     private static Vector<String> moderators = null;
     private static Vector<String> allowedSubmitters = null;
     private static Vector<String> deniedSubmitters = null;
-    private static final int SESSION_TIMEOUT_MINUTES;
-    private static final int DEFAULT_SESSION_TIMEOUT_MINUTES = 180;
-
-    static {
-        int sessionTimeoutMins;
-        // get the timeout limit
-        try {
-            sessionTimeoutMins =
-                Integer.parseInt(PropertyService.getProperty("auth.timeoutMinutes"));
-        } catch (PropertyNotFoundException e) {
-            logMetacat.error("No properties value found for auth.timeoutMinutes. Defaulting to "
-                                 + DEFAULT_SESSION_TIMEOUT_MINUTES, e);
-            sessionTimeoutMins = DEFAULT_SESSION_TIMEOUT_MINUTES;
-        }
-        SESSION_TIMEOUT_MINUTES = sessionTimeoutMins;
-    }
+    private static final Pattern ORCID_PATTERN =
+        Pattern.compile("https?://orcid\\.org/\\d{4}-\\d{4}-\\d{4}-\\d{3}(\\d|X)");
 
     /**
      * private constructor - all methods are static so there is no need to instantiate.
@@ -140,7 +123,7 @@ public class AuthUtil {
         } catch (PropertyNotFoundException pnfe) {
             throw new MetacatUtilException("Could not get metacat property: auth.administrators. "
                                                + "There will be no registered metacat "
-                                               + "adminstrators: "
+                                               + "administrators: "
                                                + pnfe.getMessage());
         }
         administrators = split(administratorString, ";", ESCAPECHAR);
@@ -151,7 +134,7 @@ public class AuthUtil {
             administrators.add(d1NodeAdmin);
         } catch (PropertyNotFoundException e) {
             String msg = "Could not get metacat property: dataone.subject "
-                + "There will be no registered DataONE adminstrator";
+                + "There will be no registered DataONE administrator";
             logMetacat.error(msg, e);
 
         }
@@ -223,63 +206,6 @@ public class AuthUtil {
                 "Problem logging user in; getAuthenticatedAdminUserId returned: <" + userId + ">");
         }
     }
-
-    /**
-     * Checks to see if the user is logged in by retrieving the token from the auth header and
-     * seeing if a corresponding Session exists in the global session list.
-     *
-     * @param request the http request that holds the auth header
-     * @return org.dataone.service.types.v1.Session if the user is logged in; null otherwise
-     */
-//    public static Session getUserSession(HttpServletRequest request) {
-//
-//        SessionService sessionService = SessionService.getInstance();
-//        if (sessionService == null) {
-//            return null;
-//        }
-//        Session session = getAuthenticatedSessionFromRequest(request);
-//
-//        String sessionId = null;
-//        if (session != null) {
-//            sessionId = session.getSubject().getValue();
-//        }
-//
-//        if (sessionId != null && sessionService.isSessionRegistered(sessionId)) {
-//            // get the registered session data
-//            SessionData sessionData = SessionService.getInstance().getRegisteredSession
-//            (sessionId);
-//            // get the last time the session was accessed
-//            Calendar lastAccessedTime = sessionData.getLastAccessedTime();
-//            // get the current time and set back "sessionTimoutInt" minutes
-//            Calendar now = Calendar.getInstance();
-//            now.add(Calendar.MINUTE, -SESSION_TIMEOUT_MINUTES);
-//
-//            // if the last accessed time is before now minus the timeout,
-//            // the session has expired. Unregister it and return false.
-//            if (lastAccessedTime.before(now)) {
-//                sessionService.unRegisterSession(sessionId);
-//                return null;
-//            }
-//            return session;
-//        }
-//        return null;
-//    }
-
-    /**
-     * Attempts to authenticate the request using the auth token included in the request header as
-     * "Authorization: Bearer $TOKEN"
-     *
-     * @param request the HttpServletRequest
-     * @return org.dataone.service.types.v1.Session, if authentication is successful. Null otherwise
-     */
-//    private static Session getAuthenticatedSessionFromRequest(HttpServletRequest request) {
-//
-//        return PortalCertificateManager.getInstance().getSession(request);
-//    }
-    private static String getOrcidLast16(String orcid) {
-        return orcid.substring(1 + orcid.lastIndexOf('/'));
-    }
-
 
     /**
      * Checks to see if the user is logged in as admin by first checking if the user is logged in
@@ -433,53 +359,6 @@ public class AuthUtil {
     }
 
     /**
-     * Gets the user group names from the login session on the http request
-     *
-     * @param request
-     *            the http request that holds the login session
-     * @return String array that holds the user groups
-     */
-    public static String[] getGroupNames(HttpServletRequest request) {
-        String sessionId = request.getSession().getId();
-        ;
-        SessionData sessionData = SessionService.getInstance().getRegisteredSession(sessionId);
-        String[] groupNames = {""};
-
-        if (sessionData != null) {
-            groupNames = sessionData.getGroupNames();
-        }
-
-        return groupNames;
-    }
-
-    /**
-     * Creates an ldap credentail string from the username, organization
-     * and dn list.
-     *
-     * @param username the user name
-     * @param organization the organization
-     * @param dnList a list of dns
-     * @return String holding the ldap login string
-     */
-    public static String createLDAPString(
-        String username, String organization, Vector<String> dnList) throws MetacatUtilException {
-
-        if (username == null || organization == null || dnList == null || dnList.size() == 0) {
-            throw new MetacatUtilException(
-                "Could not generate LDAP user string.  One of the following is null: username, "
-                    + "organization or dnlist");
-        }
-
-        String ldapString = "uid=" + username + ",o=" + organization;
-
-        for (String dn : dnList) {
-            ldapString += "," + dn;
-        }
-
-        return ldapString;
-    }
-
-    /**
      * Reports whether LDAP is fully configured.
      *
      * @return a boolean that is true if all sections are configured and false
@@ -509,21 +388,20 @@ public class AuthUtil {
             logMetacat.warn("received null username AND groups - error in calling code?");
             return false;
         }
-        Pattern orcidPattern =
-            Pattern.compile("https?\\://orcid\\.org/\\d{4}-\\d{4}-\\d{4}-\\d{3}(\\d|X)");
 
-        boolean isOrcid = (username != null) && orcidPattern.matcher(username).matches();
-        String user = isOrcid ? getOrcidLast16(username) : username;
+        boolean isOrcid = (username != null) && ORCID_PATTERN.matcher(username).matches();
+        if (!isOrcid) {
+            logMetacat.warn("received an admin username that is not an ORCID: " + username);
+        }
 
         for (String nextAdmin : getAdministrators()) {
-            String admin = isOrcid ? getOrcidLast16(nextAdmin) : nextAdmin;
 
-            if (admin.equals(user)) {
+            if (nextAdmin.equals(username)) {
                 return true;
             }
             if (groups != null) {
                 for (String group : groups) {
-                    if (admin.equals(group)) {
+                    if (nextAdmin.equals(group)) {
                         return true;
                     }
                 }
@@ -536,7 +414,7 @@ public class AuthUtil {
      * Check if the specified user is part of the moderators list
      *
      * @param username
-     *            the user login credentails
+     *            the user login credentials
      * @param groups
      *            a list of the user's groups
      */
@@ -549,13 +427,13 @@ public class AuthUtil {
      * Check if the specified user is part of the moderators list
      *
      * @param username
-     *            the user login credentails
+     *            the user login credentials
      * @param groups
      *            a list of the user's groups
      */
     public static boolean isAllowedSubmitter(String username, String[] groups)
         throws MetacatUtilException {
-        if (getAllowedSubmitters().size() == 0) {
+        if (getAllowedSubmitters().isEmpty()) {
             // no allowedSubmitters list specified -
             // hence everyone should be allowed
             return true;
@@ -578,7 +456,7 @@ public class AuthUtil {
      * Check if the specified user is part of the moderators list
      *
      * @param username
-     *            the user login credentails
+     *            the user login credentials
      * @param groups
      *            a list of the user's groups
      */
@@ -591,7 +469,7 @@ public class AuthUtil {
      * Check if the specified user can insert the document
      *
      * @param username
-     *            the user login credentails
+     *            the user login credentials
      * @param groups
      *            a list of the user's groups
      */
@@ -607,8 +485,8 @@ public class AuthUtil {
                 } else {
                     logMetacat.debug(
                         "AuthUtil.canInsertOrUpdate -And this user is in the group(s)");
-                    for (int i = 0; i < groups.length; i++) {
-                        logMetacat.debug("AuthUtil.canInsertOrUpdate -Group " + groups[i]);
+                    for (String group : groups) {
+                        logMetacat.debug("AuthUtil.canInsertOrUpdate -Group " + group);
                     }
                 }
             }
@@ -629,7 +507,7 @@ public class AuthUtil {
         Vector<String> accessList, String username, String[] groups) {
 
         // this should never happen.  All calls to this method should use the
-        // appropriate getter to retrieve the accessList.  That should guarentee
+        // appropriate getter to retrieve the accessList.  That should guarantee
         // that the access is at least an empty Vector.
         if (accessList == null) {
             return false;
@@ -646,10 +524,10 @@ public class AuthUtil {
             // check the given admin dn is a group dn...
             if (groups != null) {
                 // is a group dn
-                for (int j = 0; j < groups.length; j++) {
-                    if (groups[j] != null && groups[j].equals(accessString)) {
+                for (String group : groups) {
+                    if (group != null && group.equals(accessString)) {
                         logMetacat.debug("AuthUtil.onAccessList - user " + username
-                                             + " has a grouup which is in the access list.");
+                                             + " has a group which is in the access list.");
                         return true;
                     }
                 }
@@ -667,16 +545,15 @@ public class AuthUtil {
      * or empty.
      */
     public static Vector<String> split(String text, String delimiter, String escapeChar) {
-        Vector<String> results = new Vector<String>();
-        if (text != null && text.length() > 0 && delimiter != null && escapeChar != null) {
+        Vector<String> results = new Vector<>();
+        if (text != null && !text.isBlank() && delimiter != null && escapeChar != null) {
             String regex = "(?<!" + Pattern.quote(escapeChar) + ")" + Pattern.quote(delimiter);
             logMetacat.debug("AuthUtil.split - The regex is " + regex);
             String[] strArray = text.split(regex);
             if (strArray != null) {
-                for (int i = 0; i < strArray.length; i++) {
-                    logMetacat.debug("AuthUtil.split - the splitted original value " + strArray[i]);
-                    String remove =
-                        strArray[i].replaceAll(Pattern.quote(escapeChar + delimiter), delimiter);
+                for (String s : strArray) {
+                    logMetacat.debug("AuthUtil.split - the split original value " + s);
+                    String remove = s.replaceAll(Pattern.quote(escapeChar + delimiter), delimiter);
                     logMetacat.debug(
                         "AuthUtil.split - the value after removing escaped char is " + remove);
                     results.add(remove);
@@ -700,7 +577,7 @@ public class AuthUtil {
         SubjectInfo subjectInfo = new SubjectInfo();
         Person person = new Person();
         person.setSubject(userSubject);
-        if (groups != null && groups.length > 0) {
+        if (groups != null) {
             for (String groupName : groups) {
                 Group group = new Group();
                 group.setGroupName(groupName);
