cc6d4d83ebac15d3622e7c5cda5dc729ab88a217
Matthew B <106352182+artntek@users.noreply.github.com>
2024-03-13 14:09:14 -0700

    LoginAdminTest. Need to fix logout tests

--- test/edu/ucsb/nceas/metacat/admin/LoginAdminTest.java
+++ test/edu/ucsb/nceas/metacat/admin/LoginAdminTest.java
@@ -2,22 +2,27 @@ package edu.ucsb.nceas.metacat.admin;
 
 import edu.ucsb.nceas.LeanTestUtils;
 import edu.ucsb.nceas.metacat.util.AuthUtil;
+import edu.ucsb.nceas.metacat.util.RequestUtil;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Enumeration;
-import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
@@ -55,8 +60,65 @@ public class LoginAdminTest {
     }
 
     @Test
-    public void handle() {
+    public void handle_null_param() throws Exception {
+        expectForwardURIRegex(LOGIN_JSP);
+
+        verify(requestDispatcher, times(0)).forward(request, response);
+
+        loginAdmin.handle(request, response);
+
+        verify(requestDispatcher, times(1)).forward(request, response);
+        assertNull(request.getAttributeNames());
     }
+
+    @Test
+    public void handle_unrecognized_param() throws Exception {
+        expectForwardURIRegex(LOGIN_JSP);
+
+        verify(requestDispatcher, times(0)).forward(request, response);
+
+        // unrecognized "configureType" param should default to login flow start page
+        when(request.getParameter("configureType")).thenReturn("nonsense");
+
+        loginAdmin.handle(request, response);
+
+        verify(requestDispatcher, times(1)).forward(request, response);
+        assertNull(request.getAttributeNames());
+    }
+
+    @Test
+    public void handle_orcidFlow_param() throws Exception {
+        expectForwardURIRegex(LOGIN_JSP);
+
+        verify(requestDispatcher, times(0)).forward(request, response);
+
+        when(request.getParameter("configureType")).thenReturn("orcidFlow");
+
+        loginAdmin.handle(request, response);
+
+        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(request, times(1)).setAttribute(MetacatAdminServlet.ACTION_ORCID_FLOW, true);
+
+        assertNull(request.getAttributeNames());
+    }
+
+    @Ignore("failing, needs fixing")
+    @Test
+    public void handle_logout_param() throws Exception {
+        expectForwardURIRegex(LOGIN_JSP);
+
+        verify(requestDispatcher, times(0)).forward(request, response);
+
+        when(request.getParameter("configureType")).thenReturn("logout");
+
+        loginAdmin.handle(request, response);
+
+        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(request, times(1)).setAttribute(MetacatAdminServlet.ACTION_LOGOUT, true);
+        verify(session, times(1)).removeAttribute("userId");
+    }
+
+
 
     @Test
     public void startLoginFlow() throws Exception {
@@ -108,19 +170,27 @@ public class LoginAdminTest {
         verify(requestDispatcher, times(1)).forward(request, response);
     }
 
+    @Ignore("failing, needs fixing")
     @Test
     public void logOutAdminUser() throws Exception {
         expectForwardURIRegex(LOGIN_JSP);
 
         verify(requestDispatcher, times(0)).forward(request, response);
 
-        loginAdmin.logOutAdminUser(request, response);
+        try (MockedStatic<RequestUtil> mockRequestUtil = mockStatic(RequestUtil.class)) {
+            Cookie cookie = new Cookie("jwtToken", "my-super-secret-admin-jwt-token");
+            mockRequestUtil
+                .when(() -> RequestUtil.getCookie(isA(HttpServletRequest.class), eq("jwtToken")))
+                .thenReturn(cookie);
 
+            loginAdmin.logOutAdminUser(request, response);
+
+            mockRequestUtil.verify(() -> RequestUtil.getCookie(any(HttpServletRequest.class), eq("jwtToken")), times(1));
+        }
         verify(requestDispatcher, times(1)).forward(request, response);
         verify(request, times(1)).setAttribute(MetacatAdminServlet.ACTION_LOGOUT, true);
         verify(session, times(1)).removeAttribute("userId");
     }
-
 
     private void expectForwardURIRegex(String regex) {
         Pattern uriPattern = Pattern.compile(regex);
