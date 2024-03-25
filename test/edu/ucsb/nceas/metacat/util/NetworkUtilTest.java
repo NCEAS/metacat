f7e0e7f96405ea01e901e4b1fd2d83878d49994b
Matthew B <106352182+artntek@users.noreply.github.com>
2024-03-25 11:37:21 -0700

    added tests for other method

--- test/edu/ucsb/nceas/metacat/util/NetworkUtilTest.java
+++ test/edu/ucsb/nceas/metacat/util/NetworkUtilTest.java
@@ -1,53 +1,111 @@
 package edu.ucsb.nceas.metacat.util;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class NetworkUtilTest {
 
-    private HttpURLConnection mockConnection;
-
-    @Before
-    public void setUp() throws Exception {
-        mockConnection = mock(HttpURLConnection.class);
-        URL mockURL = mock(URL.class);
-        when(mockURL.openConnection()).thenReturn(mockConnection);
-        when(mockConnection.getURL()).thenReturn(mockURL);
-    }
-
     @Test(expected = IOException.class)
-    public void testCheckUrlStatus_nullUrl() throws IOException {
+    public void checkUrlStatus_nullUrl() throws IOException {
 
-        when(mockConnection.getResponseCode()).thenThrow(MalformedURLException.class);
         try {
             NetworkUtil.checkUrlStatus(null);
-            fail("Test shouldn't get there since the url is null");
-        } catch (IOException e) {
-            assertTrue("The error message should have null.", e.getMessage().contains("null"));
+        } catch (Exception e) {
+            assertTrue("The error message should contain 'null'.", e.getMessage().contains("null"));
             throw e;
         }
     }
 
     @Test(expected = IOException.class)
-    public void testCheckUrlStatus_emptyUrl() throws IOException {
+    public void checkUrlStatus_emptyUrl() throws IOException {
 
-        when(mockConnection.getResponseCode()).thenThrow(MalformedURLException.class);
         try {
             NetworkUtil.checkUrlStatus("");
-            fail("Test shouldn't get there since the url is blank");
-        } catch (IOException e) {
+        } catch (Exception e) {
             assertTrue(
                 "The error message should contain 'blank': " + e.getMessage(),
                 e.getMessage().contains("blank"));
+            throw e;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkUrlStatus_badUrl() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus("thisIsNot//a/url:/:!");
+        } catch (Exception e) {
+            assertTrue(
+                "The error message should contain 'URI is not absolute': " + e.getMessage(),
+                e.getMessage().contains("URI is not absolute"));
+            throw e;
+        }
+    }
+
+    @Test(expected = UnknownHostException.class)
+    public void checkUrlStatus_unknownHost() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus("https://bogus.test.nceas.ucsb.edu");
+        } catch (Exception e) {
+            assertTrue(
+                "The error message should contain 'bogus': " + e.getMessage(),
+                e.getMessage().contains("bogus"));
+            throw e;
+        }
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void checkUrlStatusWithTimeout_nullUrl() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus(null, 1000);
+        } catch (Exception e) {
+            assertTrue("The error message should have null.", e.getMessage().contains("null"));
+            throw e;
+        }
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void checkUrlStatusWithTimeout_emptyUrl() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus("", 1000);
+        } catch (Exception e) {
+            assertTrue(
+                "The error message should contain 'blank': " + e.getMessage(),
+                e.getMessage().contains("blank"));
+            throw e;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkUrlStatusWithTimeout_badUrl() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus("thisIsNot//a/url:/:!", 1000);
+        } catch (Exception e) {
+            assertTrue(
+                "The error message should contain 'URI is not absolute': " + e.getMessage(),
+                e.getMessage().contains("URI is not absolute"));
+            throw e;
+        }
+    }
+
+    @Test(expected = UnknownHostException.class)
+    public void checkUrlStatusWithTimeout_unknownHost() throws Exception {
+
+        try {
+            NetworkUtil.checkUrlStatus("https://bogus.test.nceas.ucsb.edu", 1000);
+        } catch (Exception e) {
+            assertTrue(
+                "The error message should contain 'bogus': " + e.getMessage(),
+                e.getMessage().contains("bogus"));
             throw e;
         }
     }
