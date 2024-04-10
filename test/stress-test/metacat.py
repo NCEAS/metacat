8bff57c7c2f900dee1e54956f14b72d2fdeca9ab
Dou Mok <douming.mok@gmail.com>
2024-04-10 15:19:50 -0700

    Remove GNU GPL license section from 'test/stress-test/metacat.py'

--- test/stress-test/metacat.py
+++ test/stress-test/metacat.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python
 #
-#  '$RCSfile$'
-#  Copyright: 2000 Regents of the University of California
-#
-#   '$Author: perry $'
-#     '$Date: 2006-12-15 10:23:32 -0800 (Fri, 15 Dec 2006) $'
-# '$Revision: 3122 $'
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
 # TODO:
 #  validate
 #  getNextDocid
