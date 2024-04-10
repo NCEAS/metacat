27aaef7480717a201f1057f99bbebb52999ff9b9
Dou Mok <douming.mok@gmail.com>
2024-04-10 16:29:42 -0700

    Remove GNU GPL license section from java classes in 'src/edu/ucsb/nceas/metacat/service'

--- src/edu/ucsb/nceas/metacat/service/XMLSchemaParser.java
+++ src/edu/ucsb/nceas/metacat/service/XMLSchemaParser.java
@@ -1,28 +1,3 @@
-/**
- *  '$RCSfile$'
- *    Purpose: A Class that stores xml schema information 
- *  Copyright: 2008 Regents of the University of California and the
- *             National Center for Ecological Analysis and Synthesis
- *    Authors: Jing Tao
- * 
- *   '$Author: leinfelder $'
- *     '$Date: 2008-09-26 15:43:57 -0700 (Fri, 26 Sep 2008) $'
- * '$Revision: 4399 $'
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 package edu.ucsb.nceas.metacat.service;
 
 import java.io.IOException;
