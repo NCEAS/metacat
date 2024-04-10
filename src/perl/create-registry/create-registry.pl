4430ea49a30016433545036cd3f572d4d8b404a3
Dou Mok <douming.mok@gmail.com>
2024-04-10 16:45:30 -0700

    Remove GNU GPL license section from java classes in 'src/perl'

--- src/perl/create-registry/create-registry.pl
+++ src/perl/create-registry/create-registry.pl
@@ -1,28 +1,6 @@
 
 #!/usr/bin/perl
 #
-#  '$RCSfile$'
-#  Copyright: 2000 Regents of the University of California
-#
-#   '$Author$'
-#     '$Date$'
-# '$Revision$'
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#
 # This is a CGI application for inserting metadata documents into
 # the Metacat database.  It utilizes the Metacat.pm module for most work.
 # You can specify two metacats and a list of documents. This script will
