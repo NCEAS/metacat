b149d9980e333f2931290b9560245505592cd1f3
Dou Mok <douming.mok@gmail.com>
2024-04-10 15:18:41 -0700

    Remove GNU GPL license section from test classes in 'test/tools'

--- test/tools/1.7_xmltables-postgres.sql
+++ test/tools/1.7_xmltables-postgres.sql
@@ -1,27 +1,3 @@
-/**
- *  '$RCSfile$'
- *  Copyright: 2004 Regents of the University of California and the
- *             National Center for Ecological Analysis and Synthesis
- *
- *   '$Author: jones $'
- *     '$Date: 2006-09-27 16:21:06 -0700 (Wed, 27 Sep 2006) $'
- * '$Revision: 3052 $'
- *
- * This program is free software, you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY, without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program, if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 /*
  * this is sql script does the same as the sql script named
  * xmltables.sql except that this script is to be use to
