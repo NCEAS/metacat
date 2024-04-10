8c424e120d0d88cd08f7e5a574ef9c27e76ddbc3
Dou Mok <douming.mok@gmail.com>
2024-04-10 14:41:27 -0700

    Remove GNU GPL license section from test classes in 'test/edu/ucsb/nceas/metacattest/'

--- test/edu/ucsb/nceas/metacattest/ISO119139Test.java
+++ test/edu/ucsb/nceas/metacattest/ISO119139Test.java
@@ -1,27 +1,3 @@
-/**
- *  '$RCSfile$'
- *  Copyright: 2010 Regents of the University of California and the
- *             National Center for Ecological Analysis and Synthesis
- *
- *   '$Author: jones $'
- *     '$Date: 2010-02-03 17:58:12 -0900 (Wed, 03 Feb 2010) $'
- * '$Revision: 5211 $'
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 package edu.ucsb.nceas.metacattest;
 
 import java.io.FileInputStream;
