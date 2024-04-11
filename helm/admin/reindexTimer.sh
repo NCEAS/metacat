a8b4e56095b71c3df61cc0c347d41ec87bebb2d2
Matthew B <106352182+artntek@users.noreply.github.com>
2024-04-11 10:47:59 -0700

    provide monitor-only option and display values

--- helm/admin/reindexTimer.sh
+++ helm/admin/reindexTimer.sh
@@ -10,6 +10,7 @@
 base_url=${1}
 pwd=$(kubectl get secret --namespace brooke metacatbrooke-metacat-secrets \
     -o jsonpath="{.data.rabbitmq-password}" | base64 -d)
+monitor_period_sec=30
 
 ####################################################################################################
 # FUNCTIONS
@@ -21,15 +22,35 @@ function portWarning() {
     exit 3
 }
 
-function getQueueSize() {
-    curl -u  metacat-rmq-guest:"$pwd"  \
-      http://localhost:15672/api/queues/%2f/index 2>/dev/null | \
-      jq  -r '.messages'
-}
+####################################################################################################
+# INPUT & VALIDATION
+####################################################################################################
 
-####################################################################################################
-# VALIDATION
-####################################################################################################
+if [[ $(nc -zv localhost 15672 2>&1 | grep -c "refused") -gt 0 ]]; then
+  portWarning
+else
+  echo "RMQ port is open"
+fi
+
+echo
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo
+echo "    BEWARE! This will trigger a FULL RE-INDEX of all the datasets on:"
+echo "        $base_url"
+echo "    ...which may take a very long time (hours or days), and will likely affect"
+echo "    the responsiveness of your metacat instance during that time period!"
+echo
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+echo
+reindex_command="re-index-all"
+echo "Available Options:"
+echo "* FULL RE-INDEX: type '$reindex_command' and hit 'Enter'."
+echo "* ONLY MONITOR queue size & # indexer workers, every $monitor_period_sec sec: hit 'Enter'."
+echo "* EXIT: ctrl-c"
+action=""
+read -p "Option: " action
 
 echo
 if [[ -z $base_url ]]; then
@@ -44,54 +65,53 @@ if [ -z "$TOKEN" ]; then
     exit 2
 fi
 
-if [[ $(nc -zv localhost 15672 2>&1 | grep -c "refused") -gt 0 ]]; then
-  portWarning
-else
-  echo "RMQ port is open"
-fi
-
 ####################################################################################################
 # MAIN
 ####################################################################################################
-echo
-echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-echo
-echo "    BEWARE! This will trigger a FULL RE-INDEX of all the datasets on:"
-echo "        $base_url"
-echo "    ...which may take a very long time (hours or days), and will likely affect"
-echo "    the responsiveness of your metacat instance during that time period!"
-echo
-echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-echo "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
-echo
-echo "Hit 'Enter' to continue, or ctrl-c to exit"
-read -p ""
-
-echo "calling $base_url/d1/mn/v2/index?all=true"
-
-# Start reindex-all
-curl -X PUT -H "Authorization: Bearer $TOKEN" "$base_url/d1/mn/v2/index?all=true"
 
 start=$(date +%s)
 echo "Starting at: $(date)"
 
-# delay for reindex to begin populating queue, otherwise we exit too early
-sleep 10
+if [[ "$action" == "$reindex_command" ]]; then
+    echo "calling $base_url/d1/mn/v2/index?all=true"
+    # Start reindex-all
+    curl -X PUT -H "Authorization: Bearer $TOKEN" "$base_url/d1/mn/v2/index?all=true"
+
+    # delay for reindex to begin populating queue, otherwise we exit too early
+    sleep 10
+fi
+
+max_queue_size=0
+max_worker_count=0
 
 while true; do
-    queueSize=$(getQueueSize)
-    echo "Queue size: $queueSize"
-    if [ "$queueSize" -ne 0 ]; then
-        sleep 10
-    else
+    queue_size=$(curl -u  metacat-rmq-guest:"$pwd" \
+                   http://localhost:15672/api/queues/%2f/index 2>/dev/null | \
+                   jq  -r '.messages')
+
+    if [ "$queue_size" -gt "$max_queue_size" ]; then
+        max_queue_size=$queue_size
+    fi
+
+    idx_worker_count=$(kubectl get pods | grep -c d1index)
+    if [ "$idx_worker_count" -gt "$max_worker_count" ]; then
+        max_worker_count=$idx_worker_count
+    fi
+
+    echo -ne "$(date)\t\t Queue size: $queue_size (max: $max_queue_size)\t\t \
+              Indexer count: $idx_worker_count (max: $max_worker_count)\n\n\r\033[1A\033[1A"
+
+    if [ "$queue_size" -eq 0 ] && [ "$action" == "$reindex_command" ]; then
         break
+    else
+        sleep $monitor_period_sec
     fi
 done
 
 finish=$(date +%s)
-echo "Finished at: $(date)"
+time_min=$(((finish - start)/60))
 
-time=$((finish - start))
-
-echo "Total time for reindex = $time seconds"
+echo; echo; echo "Finished at: $(date)"
+echo "Max Queue size: $max_queue_size"
+echo "Max index worker count: $max_worker_count"
+echo "Total time for reindex = $time_min minutes"
