23ecbfdf676af3845125187c6675d5631f4bd7eb
Dou Mok <douming.mok@gmail.com>
2024-04-10 17:16:40 -0700

    Remove GNU GPL license section from java classes in 'metacat-common/src/main'

--- metacat-common/src/main/resources/solr-home/conf/velocity/js/jquery.autocomplete.js
+++ metacat-common/src/main/resources/solr-home/conf/velocity/js/jquery.autocomplete.js
@@ -1,13 +1,7 @@
 /*
  * Autocomplete - jQuery plugin 1.1pre
  *
- * Copyright (c) 2007 Dylan Verheul, Dan G. Switzer, Anjesh Tuladhar, Jörn Zaefferer
- *
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- *
- * Revision: Id: jquery.autocomplete.js 5785 2008-07-12 10:37:33Z joern.zaefferer $
+ * Authors: Dylan Verheul, Dan G. Switzer, Anjesh Tuladhar, Jörn Zaefferer
  *
  */
 
