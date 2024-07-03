69797d1ad039eb51f119695452638d1970fd8ca2
Jing Tao <tao@nceas.ucsb.edu>
2024-07-03 14:54:35 -0700

    Made it junit 4. Renamed to IT.

--- test/edu/ucsb/nceas/metacat/admin/upgrade/UpdateDOIIT.java
+++ test/edu/ucsb/nceas/metacat/admin/upgrade/UpdateDOIIT.java
@@ -1,27 +1,3 @@
-/**
- *  '$RCSfile$'
- *  Copyright: 2019 Regents of the University of California and the
- *              National Center for Ecological Analysis and Synthesis
- *  Purpose: To test the Access Controls in metacat by JUnit
- *
- *   '$Author$'
- *     '$Date$'
- * '$Revision$'
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 package edu.ucsb.nceas.metacat.admin.upgrade;
 
 import java.io.FileInputStream;
@@ -35,6 +11,7 @@ import java.util.Vector;
 import edu.ucsb.nceas.LeanTestUtils;
 import edu.ucsb.nceas.utilities.PropertyNotFoundException;
 import org.apache.commons.io.IOUtils;
+import org.apache.wicket.protocol.http.mock.MockHttpServletRequest;
 import org.dataone.client.v2.formats.ObjectFormatCache;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.Session;
@@ -46,33 +23,30 @@ import edu.ucsb.nceas.metacat.dataone.MNodeService;
 import edu.ucsb.nceas.metacat.doi.ezid.EzidDOIService;
 import edu.ucsb.nceas.metacat.doi.ezid.RegisterDOIIT;
 import edu.ucsb.nceas.metacat.properties.PropertyService;
-import junit.framework.Test;
-import junit.framework.TestSuite;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.MockedStatic;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-public class UpdateDOITest extends D1NodeServiceTest {
+public class UpdateDOIIT {
     private static final String UPDATETIMEKEY = "_updated";
     MockedStatic<PropertyService> closeableMock;
-    /**
-     * Constructor
-     * @param name
-     */
-    public UpdateDOITest(String name) {
-        super(name);
-    }
-    
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(new UpdateDOITest("testUpdate"));
-        return suite;
-    }
+    private D1NodeServiceTest d1NodeTest;
+    private MockHttpServletRequest request;
+
 
     @Before
     public void setUp() throws Exception {
-        super.setUp();
+        d1NodeTest = new D1NodeServiceTest("initialize");
+        request = (MockHttpServletRequest)d1NodeTest.getServletRequest();
+        d1NodeTest.setUp();
         final String passwdMsg =
             """
             \n* * * * * * * * * * * * * * * * * * *
@@ -100,19 +74,20 @@ public class UpdateDOITest extends D1NodeServiceTest {
         } catch (Exception e) {
             //no need to handle - just a housekeeping failure
         }
-        super.tearDown();
+        d1NodeTest.tearDown();
     }
 
 
     /*
      * Test the update of a pid and sid
      */
+    @Test
     public void testUpdate() throws Exception {
      // get ezid config properties
         String ezidUsername = PropertyService.getProperty("guid.doi.username");
         String ezidPassword = PropertyService.getProperty("guid.doi.password");
         String ezidServiceBaseUrl = PropertyService.getProperty("guid.doi.baseurl");
-        Session session = getTestSession();
+        Session session = d1NodeTest.getTestSession();
         String emlFile = "test/eml-multiple-creators.xml";
         InputStream content = null;
         //Test the case that the identifier is a doi but no sid.
@@ -124,13 +99,13 @@ public class UpdateDOITest extends D1NodeServiceTest {
         try {
             Identifier publishedPID = MNodeService.getInstance(request).generateIdentifier(session, scheme, null);
             publishedPIDStr = publishedPID.getValue();
-            //System.out.println("The doi on the identifier is "+publishedPID.getValue());
             content = new FileInputStream(emlFile);
-            SystemMetadata sysmeta = createSystemMetadata(publishedPID, session.getSubject(), content);
+            SystemMetadata sysmeta = D1NodeServiceTest
+                                .createSystemMetadata(publishedPID, session.getSubject(), content);
             content.close();
             sysmeta.setFormatId(ObjectFormatCache.getInstance().getFormat("eml://ecoinformatics.org/eml-2.1.0").getFormatId());
             content = new FileInputStream(emlFile);
-            Identifier pid = MNodeService.getInstance(request).create(session, publishedPID, content, sysmeta);
+            Identifier pid = d1NodeTest.mnCreate(session, publishedPID, content, sysmeta);
             content.close();
             assertEquals(publishedPID.getValue(), pid.getValue());
             // check for the metadata explicitly, using ezid service
@@ -143,19 +118,16 @@ public class UpdateDOITest extends D1NodeServiceTest {
                 Thread.sleep(2000);
                 count++;
             } while ((metadata == null || metadata.get(EzidDOIService.DATACITE) == null) && count < 30);
-            //System.out.println("The doi on the identifier is "+publishedPID.getValue());
             assertNotNull(metadata);
             String result = metadata.get(EzidDOIService.DATACITE);
-            //System.out.println("the result is \n"+result);
             assertTrue(result.contains("Test EML package - public-readable from morpho"));
             assertTrue(result.contains(RegisterDOIIT.creatorsStr));
-            //System.out.println("publisher =======is"+publisher);
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
             String year = sdf.format(sysmeta.getDateUploaded());
             assertTrue(result.contains(year));
             assertTrue(result.contains("Dataset"));
             String updateTime = metadata.get(UPDATETIMEKEY);
-            PIDUpdateTime = (new Long(updateTime)).longValue();
+            PIDUpdateTime = Long.parseLong(updateTime);
             content.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -171,12 +143,13 @@ public class UpdateDOITest extends D1NodeServiceTest {
             Identifier publishedSID = MNodeService.getInstance(request).generateIdentifier(session, scheme, null);
             publishedSIDStr = publishedSID.getValue();
             content = new FileInputStream(emlFile);
-            SystemMetadata sysmeta = createSystemMetadata(guid, session.getSubject(), content);
+            SystemMetadata sysmeta = D1NodeServiceTest
+                                        .createSystemMetadata(guid, session.getSubject(), content);
             content.close();
             sysmeta.setFormatId(ObjectFormatCache.getInstance().getFormat("eml://ecoinformatics.org/eml-2.1.0").getFormatId());
             sysmeta.setSeriesId(publishedSID);
             content = new FileInputStream(emlFile);
-            Identifier pid = MNodeService.getInstance(request).create(session, guid, content, sysmeta);
+            Identifier pid = d1NodeTest.mnCreate(session, guid, content, sysmeta);
             content.close();
             assertEquals(guid.getValue(), pid.getValue());
             // check for the metadata explicitly, using ezid service
@@ -192,18 +165,14 @@ public class UpdateDOITest extends D1NodeServiceTest {
 
             assertNotNull(metadata);
             String result = metadata.get(EzidDOIService.DATACITE);
-            //System.out.println("the result is \n"+result);
             assertTrue(result.contains("Test EML package - public-readable from morpho"));
             assertTrue(result.contains(RegisterDOIIT.creatorsStr));
-            //System.out.println("publisher =======is"+publisher);
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
             String year = sdf.format(sysmeta.getDateUploaded());
             assertTrue(result.contains(year));
-            //System.out.println("publishing year =======is"+publishingYear);
-            //System.out.println("resource type =======is"+resourceType);
             assertTrue(result.contains("Dataset"));
             String updateTime = metadata.get(UPDATETIMEKEY);
-            SIDUpdateTime = (new Long(updateTime)).longValue();
+            SIDUpdateTime = Long.parseLong(updateTime);
             content.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -229,20 +198,20 @@ public class UpdateDOITest extends D1NodeServiceTest {
         do {
             metadata = ezid.getMetadata(publishedPIDStr);
             updateTime = metadata.get(UPDATETIMEKEY);
-            pidUpdate1 = (new Long(updateTime)).longValue();
+            pidUpdate1 = Long.parseLong(updateTime);
             Thread.sleep(2000);
             count++;
         } while (pidUpdate1 ==  PIDUpdateTime && count < 30);
         assertNotNull(metadata);
         updateTime = metadata.get(UPDATETIMEKEY);
-        pidUpdate1 = (new Long(updateTime)).longValue();
+        pidUpdate1 = Long.parseLong(updateTime);
         assertTrue(pidUpdate1 > PIDUpdateTime);
 
         long sidUpdate1 = -1;
         do {
             metadata = ezid.getMetadata(publishedSIDStr);
             updateTime = metadata.get(UPDATETIMEKEY);
-            sidUpdate1 = (new Long(updateTime)).longValue();
+            sidUpdate1 = Long.parseLong(updateTime);
             Thread.sleep(2000);
             count++;
         } while ((sidUpdate1 == SIDUpdateTime) && count < 30);
