ded0466202ebc8a920877a2ba5f69dfd40c215e5
Jing Tao <tao@nceas.ucsb.edu>
2024-07-23 16:26:47 -0700

    Add a method to remove a pid from the deadlock.

--- src/edu/ucsb/nceas/metacat/admin/MetacatAdminServlet.java
+++ src/edu/ucsb/nceas/metacat/admin/MetacatAdminServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ucsb.nceas.metacat.systemmetadata.SystemMetadataManager;
 import edu.ucsb.nceas.utilities.PropertyNotFoundException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -26,6 +27,7 @@ import edu.ucsb.nceas.metacat.util.ConfigurationUtil;
 import edu.ucsb.nceas.metacat.util.RequestUtil;
 import edu.ucsb.nceas.metacat.util.SystemUtil;
 import edu.ucsb.nceas.utilities.GeneralPropertyException;
+import org.dataone.service.types.v1.Identifier;
 
 /**
  * Entry servlet for the metadata configuration utility
@@ -180,6 +182,7 @@ public class MetacatAdminServlet extends HttpServlet {
                 case "quota" -> QuotaAdmin.getInstance().configureQuota(request, response);
                 case "solrserver" -> SolrAdmin.getInstance().configureSolr(request, response);
                 case "refreshStylesheets" -> clearStylesheetCache(response);
+                case "removeSysmetaLock" -> removeSysmetaLock(request, response);
                 default -> {
                     String errorMessage =
                         "MetacatAdminServlet - Invalid action in configuration request: " + action;
@@ -287,5 +290,33 @@ public class MetacatAdminServlet extends HttpServlet {
                 request.setAttribute("contextURL", SystemUtil.getContextURL());
             }
         }
+    }
+
+    /**
+     * Remove only ONE pid from the system metadata lock queue. This is an admin method to
+     * fix a deadlock in the system metadata modification lock.
+     * @param response
+     * @throws IOException
+     */
+    private void removeSysmetaLock(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+            String pid = request.getParameter("pid");
+            logMetacat.debug("Remove the pid " + pid + " from the system metadata lock");
+            if (pid != null && !pid.isBlank()) {
+                Identifier id = new Identifier();
+                id.setValue(pid);
+                SystemMetadataManager.unLock(id);
+                try (PrintWriter out = response.getWriter()) {
+                    out.print("<success>");
+                    out.print("Metacat has removed the pid " + pid + " from the system metadata lock.");
+                    out.print("</success>");
+                }
+            } else {
+                try (PrintWriter out = response.getWriter()) {
+                    out.print("<error>");
+                    out.print("The pid should not be blank.");
+                    out.print("</error>");
+                }
+            }
     }
 }
