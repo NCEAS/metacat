b5960555f7aa540bca0ae4b13547a5ed9965b2fc
Jing Tao <tao@nceas.ucsb.edu>
2024-08-15 13:31:11 -0700

    Added the method to get the updatestatus object from a given string.

--- src/edu/ucsb/nceas/metacat/admin/UpdateStatus.java
+++ src/edu/ucsb/nceas/metacat/admin/UpdateStatus.java
@@ -2,14 +2,15 @@ package edu.ucsb.nceas.metacat.admin;
 
 /**
  * @author Tao
- * enum of the update status
+ * enum represents the update status
  */
 public enum UpdateStatus {
-    PENDING("pending"),
-    NOT_REQUIRED("not required"),
-    IN_PROGRESS("in progress"),
-    FAILED("failed"),
-    COMPLETE("complete");
+
+    PENDING(MetacatAdmin.PENDING),
+    NOT_REQUIRED(MetacatAdmin.NOT_REQUIRED),
+    IN_PROGRESS(MetacatAdmin.IN_PROGRESS),
+    FAILED(MetacatAdmin.FAILED),
+    COMPLETE(MetacatAdmin.COMPLETE);
 
     private String value;
     private UpdateStatus(String value) {
@@ -22,5 +23,33 @@ public enum UpdateStatus {
      */
     public String getValue() {
         return this.value;
+    }
+
+    /**
+     * Get the UpgradeStatus object from the given string
+     * @param status  the string value of the status
+     * @return the UpgradeStatus object
+     * @throws IllegalArgumentException
+     */
+    public static UpdateStatus getStatus(String status) throws IllegalArgumentException {
+        switch (status) {
+            case MetacatAdmin.COMPLETE -> {
+                return COMPLETE;
+            }
+            case MetacatAdmin.FAILED -> {
+                return FAILED;
+            }
+            case MetacatAdmin.PENDING -> {
+                return PENDING;
+            }
+            case MetacatAdmin.NOT_REQUIRED -> {
+                return NOT_REQUIRED;
+            }
+            case MetacatAdmin.IN_PROGRESS -> {
+                return IN_PROGRESS;
+            }
+            default -> throw new IllegalArgumentException(
+                "UpdateStatus cannot recognize this status: " + status);
+        }
     }
 }
