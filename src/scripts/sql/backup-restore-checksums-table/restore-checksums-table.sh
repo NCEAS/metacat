6dcdaf07b4e46fe83c806013df5df6c4b42bfb15
Jing Tao <tao@nceas.ucsb.edu>
2024-10-30 09:51:20 -0700

    Use heredoc to execute the multiple sql commands to make sure the commands can be roll back if one of them fails.

--- src/scripts/sql/backup-restore-checksums-table/restore-checksums-table.sh
+++ src/scripts/sql/backup-restore-checksums-table/restore-checksums-table.sh
@@ -5,19 +5,20 @@ if [ $# -ne 2 ]; then
 fi
 databaseName=$1
 backupFilePath=$2
-psql  -d $databaseName -c "\set AUTOCOMMIT OFF"
-###Drop indexes and other stuff
-psql  -d $databaseName -c "ALTER TABLE checksums ALTER COLUMN checksum_id DROP DEFAULT;"
-psql  -d $databaseName -c "DROP INDEX IF EXISTS checksums_guid;"
-psql  -d $databaseName -c "DROP INDEX IF EXISTS checksums_checksum_algorithm;"
-psql  -d $databaseName -c "DROP INDEX IF EXISTS checksums_checksum;"
-psql  -d $databaseName -c "ALTER TABLE checksums DROP CONSTRAINT checksums_fk;"
-psql  -d $databaseName -c "\COPY checksums FROM $backupFilePath"
-psql  -d $databaseName -c "ALTER TABLE checksums ALTER COLUMN checksum_id SET DEFAULT nextval('checksums_id_seq');"
-psql  -d $databaseName -c "SELECT setval('checksums_id_seq', (SELECT max(checksum_id) FROM checksums));"
-psql  -d $databaseName -c "CREATE INDEX IF NOT EXISTS checksums_guid on checksums(guid);"
-psql  -d $databaseName -c "CREATE INDEX IF NOT EXISTS checksums_checksum_algorithm on checksums(checksum_algorithm);"
-psql  -d $databaseName -c "CREATE INDEX IF NOT EXISTS checksums_checksum on checksums(checksum);"
-psql  -d $databaseName -c "ALTER TABLE checksums ADD CONSTRAINT checksums_fk FOREIGN KEY (guid) REFERENCES systemMetadata DEFERRABLE;"
-psql  -d $databaseName -c "COMMIT;"
+psql -d "$databaseName" <<EOF
+BEGIN;
+ALTER TABLE checksums ALTER COLUMN checksum_id DROP DEFAULT;
+DROP INDEX IF EXISTS checksums_guid;
+DROP INDEX IF EXISTS checksums_checksum_algorithm;
+DROP INDEX IF EXISTS checksums_checksum;
+ALTER TABLE checksums DROP CONSTRAINT IF EXISTS checksums_fk;
+\COPY checksums FROM '$backupFilePath';
+ALTER TABLE checksums ALTER COLUMN checksum_id SET DEFAULT nextval('checksums_id_seq');
+SELECT setval('checksums_id_seq', (SELECT max(checksum_id) FROM checksums));
+CREATE INDEX IF NOT EXISTS checksums_guid ON checksums(guid);
+CREATE INDEX IF NOT EXISTS checksums_checksum_algorithm ON checksums(checksum_algorithm);
+CREATE INDEX IF NOT EXISTS checksums_checksum ON checksums(checksum);
+ALTER TABLE checksums ADD CONSTRAINT checksums_fk FOREIGN KEY (guid) REFERENCES systemMetadata DEFERRABLE;
+COMMIT;
+EOF
 echo "The checksums table was restored from the file ${backupFilePath} successfully"
