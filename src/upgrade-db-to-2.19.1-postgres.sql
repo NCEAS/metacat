bbb6b26280b1e35912cd48f3bdac4b045683b7d3
Jing Tao <tao@nceas.ucsb.edu>
2024-11-19 12:54:54 -0800

    Added the code to create a new index on the access_log table to improve the log query performance.

--- src/upgrade-db-to-2.19.1-postgres.sql
+++ src/upgrade-db-to-2.19.1-postgres.sql
@@ -5,6 +5,11 @@
 SELECT setval('xml_catalog_id_seq', (SELECT max(catalog_id) from xml_catalog));
 
 /*
+ * Create an index to improve the performance of the access_log query
+ */
+ CREATE INDEX IF NOT EXISTS access_log_date_logged ON access_log (date_logged);
+
+/*
  * update the database version
  */
 UPDATE db_version SET status=0;
