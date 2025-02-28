8e03af553021346718bf22111488073ef4648b08
Jing Tao <tao@nceas.ucsb.edu>
2025-02-28 14:32:29 -0800

    Bump solr version to 9.8.0 and made changes based on the api changes.

--- metacat-common/src/main/java/edu/ucsb/nceas/metacat/common/query/SolrQueryService.java
+++ metacat-common/src/main/java/edu/ucsb/nceas/metacat/common/query/SolrQueryService.java
@@ -1,21 +1,3 @@
-/**
- *  Copyright: 2013 Regents of the University of California and the
- *             National Center for Ecological Analysis and Synthesis
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
 package edu.ucsb.nceas.metacat.common.query;
 
 import java.io.InputStream;
@@ -35,7 +17,6 @@ import org.apache.solr.schema.SchemaField;
 import org.dataone.service.types.v1.Subject;
 import org.dataone.service.util.Constants;
 
-import edu.ucsb.nceas.metacat.common.query.SolrQueryResponseWriterFactory;
 
 
 /**
@@ -85,15 +66,14 @@ public abstract class SolrQueryService {
      * is null, there will be no access rules for the query. This is for the embedded solr server.
      * @param query the query params. 
      * @param subjects the user's identity which sent the query
-     * @param method  the method such as GET, POST and et al will be used in this query. This only works for the HTTP Solr server.
+     * @param method  the method such as GET, POST and et al will be used in this query. This only
+     *                works for the HTTP Solr server.
      * @return the response
      * @throws Exception
      */
-    public abstract InputStream query(SolrParams query, Set<Subject> subjects, SolrRequest.METHOD method) throws Exception;
-    
-    
-  
-    
+    public abstract InputStream query(
+        SolrParams query, Set<Subject> subjects, SolrRequest.METHOD method) throws Exception;
+
     /**
      * Get the fields list of the index schema
      * @return
@@ -160,7 +140,7 @@ public abstract class SolrQueryService {
                 log.info("=================== fq query is "+query.toString());
                 NamedList fq = new NamedList();
                 fq.add(FILTERQUERY, query.toString());
-                SolrParams fqParam = SolrParams.toSolrParams(fq);
+                SolrParams fqParam = fq.toSolrParams();
                 append = AppendedSolrParams.wrapAppended(solrParams, fqParam);
             } else {
                 append = solrParams;
@@ -179,22 +159,42 @@ public abstract class SolrQueryService {
                     if(subjectName != null && !subjectName.trim().equals("")) {
                         if(first) {
                             first = false;
-                            query.append(OPENPARENTHESES + READPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            query.append(OR + OPENPARENTHESES + WRITEPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            query.append(OR + OPENPARENTHESES + CHANGEPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            if(!subjectName.equals(Constants.SUBJECT_PUBLIC) && !subjectName.equals(Constants.SUBJECT_AUTHENTICATED_USER)) {
-                                query.append(OR + OPENPARENTHESES + RIGHTSHOLDER + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
+                            query.append(
+                                OPENPARENTHESES + READPERMISSION + COLON + "\"" + subjectName + "\""
+                                    + CLOSEPARENTHESES);
+                            query.append(
+                                OR + OPENPARENTHESES + WRITEPERMISSION + COLON + "\"" + subjectName
+                                    + "\"" + CLOSEPARENTHESES);
+                            query.append(
+                                OR + OPENPARENTHESES + CHANGEPERMISSION + COLON + "\"" + subjectName
+                                    + "\"" + CLOSEPARENTHESES);
+                            if (!subjectName.equals(Constants.SUBJECT_PUBLIC)
+                                && !subjectName.equals(Constants.SUBJECT_AUTHENTICATED_USER)) {
+                                query.append(
+                                    OR + OPENPARENTHESES + RIGHTSHOLDER + COLON + "\"" + subjectName
+                                        + "\"" + CLOSEPARENTHESES);
                             } else if (subjectName.equals(Constants.SUBJECT_PUBLIC)) {
-                                query.append(OR + OPENPARENTHESES + IS_PUBLIC+COLON + "true" + CLOSEPARENTHESES);
+                                query.append(OR + OPENPARENTHESES + IS_PUBLIC + COLON + "true"
+                                                 + CLOSEPARENTHESES);
                             }
                         } else {
-                            query.append(OR + OPENPARENTHESES + READPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            query.append(OR + OPENPARENTHESES + WRITEPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            query.append(OR + OPENPARENTHESES + CHANGEPERMISSION + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
-                            if(!subjectName.equals(Constants.SUBJECT_PUBLIC) && !subjectName.equals(Constants.SUBJECT_AUTHENTICATED_USER)) {
-                                query.append(OR + OPENPARENTHESES + RIGHTSHOLDER + COLON + "\"" + subjectName + "\"" + CLOSEPARENTHESES);
+                            query.append(
+                                OR + OPENPARENTHESES + READPERMISSION + COLON + "\"" + subjectName
+                                    + "\"" + CLOSEPARENTHESES);
+                            query.append(
+                                OR + OPENPARENTHESES + WRITEPERMISSION + COLON + "\"" + subjectName
+                                    + "\"" + CLOSEPARENTHESES);
+                            query.append(
+                                OR + OPENPARENTHESES + CHANGEPERMISSION + COLON + "\"" + subjectName
+                                    + "\"" + CLOSEPARENTHESES);
+                            if (!subjectName.equals(Constants.SUBJECT_PUBLIC)
+                                && !subjectName.equals(Constants.SUBJECT_AUTHENTICATED_USER)) {
+                                query.append(
+                                    OR + OPENPARENTHESES + RIGHTSHOLDER + COLON + "\"" + subjectName
+                                        + "\"" + CLOSEPARENTHESES);
                             } else if (subjectName.equals(Constants.SUBJECT_PUBLIC)) {
-                                query.append(OR + OPENPARENTHESES + IS_PUBLIC + COLON + "true" + CLOSEPARENTHESES);
+                                query.append(OR + OPENPARENTHESES + IS_PUBLIC + COLON + "true"
+                                                 + CLOSEPARENTHESES);
                             }
                         }
                     }
