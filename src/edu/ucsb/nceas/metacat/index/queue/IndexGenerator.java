b53ffea75986e99d14f7d5407f2efd14ee47142e
Jing Tao <tao@nceas.ucsb.edu>
2025-03-06 19:05:51 -0800

    Removed an unused import.

--- src/edu/ucsb/nceas/metacat/index/queue/IndexGenerator.java
+++ src/edu/ucsb/nceas/metacat/index/queue/IndexGenerator.java
@@ -13,7 +13,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import edu.ucsb.nceas.metacat.admin.upgrade.HashStoreUpgrader;
 import edu.ucsb.nceas.metacat.index.queue.pool.RabbitMQChannelFactory;
 import edu.ucsb.nceas.metacat.properties.PropertyService;
 import edu.ucsb.nceas.utilities.PropertyNotFoundException;
