8fbc80779b1994d1953777c0d8a0feefc5bf07f6
Matthew B <106352182+artntek@users.noreply.github.com>
2026-01-21 15:41:23 -0800

    removed smReplicationPolicy_guid index

--- src/upgrade-db-to-3.3.1-postgres.sql
+++ src/upgrade-db-to-3.3.1-postgres.sql
@@ -1,7 +1,6 @@
 /*
  * Add a new index on the smReplicationPolicy table
  */
-CREATE INDEX IF NOT EXISTS smReplicationPolicy_guid on smReplicationPolicy(guid);
 CREATE INDEX IF NOT EXISTS smReplicationPolicy_guid_policy ON smReplicationPolicy(guid, policy);
 CREATE INDEX IF NOT EXISTS smReplicationStatus_guid ON smReplicationStatus(guid);
 
