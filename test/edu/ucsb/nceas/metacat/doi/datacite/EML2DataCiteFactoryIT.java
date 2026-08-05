7446680a2fa4f15b1426482194ae24537e65aacd
Jing Tao <tao@nceas.ucsb.edu>
2026-08-05 11:34:20 -0700

    Split the long statements. And reorder the test text since the xml elements were reordered.

--- test/edu/ucsb/nceas/metacat/doi/datacite/EML2DataCiteFactoryIT.java
+++ test/edu/ucsb/nceas/metacat/doi/datacite/EML2DataCiteFactoryIT.java
@@ -30,17 +30,53 @@ import static org.junit.Assert.assertTrue;
 public class EML2DataCiteFactoryIT {
     
     public static final String section = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-    public static final String section0 = "<resource xmlns=\"http://datacite.org/schema/kernel-4\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://datacite.org/schema/kernel-4 https://schema.datacite.org/meta/kernel-4.5/metadata.xsd\"><identifier identifierType=\"DOI\">";
-    public static final String section1 = "</identifier><creators><creator><creatorName>Lehman, Clarence</creatorName></creator><creator><creatorName>Inouye, Richard</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" schemeURI=\"http://orcid.org/\">0001-0005-9751-1234</nameIdentifier></creator><creator><creatorName>Idaho State University</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" schemeURI=\"http://orcid.org/\">0001-0005-9751-1111</nameIdentifier></creator><creator>" +
-                                           "<creatorName>Smith, Mike</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" schemeURI=\"https://orcid.org/\">0001-0005-9751-6987</nameIdentifier><affiliation>National Center for Ecological Analysis and Synthesis</affiliation></creator><creator><creatorName>Ed, John</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" schemeURI=\"https://orcid.org/\">0001-0005-9751-0000</nameIdentifier><affiliation>NCEAS</affiliation></creator>" + 
-                                           "<creator><creatorName>Data manager</creatorName><affiliation>NCEAS, University of California</affiliation></creator></creators><titles><title xml:lang=\"en\">Data from Cedar Creek LTER on productivity and species richness";
-    public static final String section2 = "for use in a workshop titled \"An Analysis of the Relationship between";
-    public static final String section3 = "Productivity and Diversity using Experimental Results from the Long-Term";
-    public static final String section4 = "Ecological Research Network\" held at NCEAS in September 1996.</title></titles><publisher>Jefferson, John. Director. Institute of Ecology";
-    public static final String section41 = "</publisher><publicationYear>2009</publicationYear><subjects><subject xml:lang=\"en\">Old field grassland</subject><subject xml:lang=\"en\">biomass</subject><subject xml:lang=\"en\">productivity</subject><subject xml:lang=\"en\">species-area</subject><subject xml:lang=\"en\">species richness</subject></subjects><language>en</language><resourceType resourceTypeGeneral=\"Dataset\"/><descriptions><description descriptionType=\"Abstract\" xml:lang=\"en\">";
-    public static final String section5 = "This data package contains information about the first year of participation in SNAPP synthesis working groups for different institutions";
-    public static final String section6 = "The data source is NCEAS administrative database. The R Script compute the yearly statistics and generate the bar chart. The scripts outputs 1 csv file with the yearly stats and one png file wit the plot.";
-    public static final String section7 = "</description></descriptions><formats><format>text/xml</format></formats></resource>";
+    public static final String section0 =
+        "<resource xmlns=\"http://datacite.org/schema/kernel-4\" xmlns:xsi=\"http://www.w3"
+            + ".org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://datacite"
+            + ".org/schema/kernel-4 https://schema.datacite.org/meta/kernel-4.5/metadata"
+            + ".xsd\"><identifier identifierType=\"DOI\">";
+    public static final String section1 =
+        "</identifier><creators><creator><creatorName>Lehman, "
+            + "Clarence</creatorName></creator><creator><creatorName>Inouye, "
+            + "Richard</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" "
+            + "schemeURI=\"http://orcid.org/\">0001-0005-9751-1234</nameIdentifier></creator"
+            + "><creator><creatorName>Idaho State University</creatorName><nameIdentifier "
+            + "nameIdentifierScheme=\"ORCID\" schemeURI=\"http://orcid"
+            + ".org/\">0001-0005-9751-1111</nameIdentifier></creator><creator>"
+            + "<creatorName>Smith, Mike</creatorName><nameIdentifier "
+            + "nameIdentifierScheme=\"ORCID\" schemeURI=\"https://orcid"
+            + ".org/\">0001-0005-9751-6987</nameIdentifier><affiliation>National Center for "
+            + "Ecological Analysis and Synthesis</affiliation></creator><creator><creatorName>Ed,"
+            + " John</creatorName><nameIdentifier nameIdentifierScheme=\"ORCID\" "
+            + "schemeURI=\"https://orcid.org/\">0001-0005-9751-0000</nameIdentifier><affiliation"
+            + ">NCEAS</affiliation></creator>"
+            + "<creator><creatorName>Data manager</creatorName><affiliation>NCEAS, University of "
+            + "California</affiliation></creator></creators><titles><title xml:lang=\"en\">Data "
+            + "from Cedar Creek LTER on productivity and species richness";
+    public static final String section2 =
+        "for use in a workshop titled \"An Analysis of the Relationship between";
+    public static final String section3 =
+        "Productivity and Diversity using Experimental Results from the Long-Term";
+    public static final String section4 =
+        "Ecological Research Network\" held at NCEAS in September 1996"
+            + ".</title></titles><publisher>Jefferson, John. Director. Institute of Ecology";
+    public static final String section41 =
+        "</publisher><publicationYear>2009</publicationYear><resourceType "
+            + "resourceTypeGeneral=\"Dataset\"/><subjects><subject xml:lang=\"en\">Old field "
+            + "grassland</subject><subject xml:lang=\"en\">biomass</subject><subject "
+            + "xml:lang=\"en\">productivity</subject><subject "
+            + "xml:lang=\"en\">species-area</subject><subject xml:lang=\"en\">species "
+            + "richness</subject></subjects><language>en</language>";
+    public static final String section5 =
+        "This data package contains information about the first year of participation in SNAPP "
+            + "synthesis working groups for different institutions";
+    public static final String section6 =
+        "The data source is NCEAS administrative database. The R Script compute the yearly "
+            + "statistics and generate the bar chart. The scripts outputs 1 csv file with the "
+            + "yearly stats and one png file wit the plot.";
+    public static final String section7 =
+        "<formats><format>text/xml</format></formats><descriptions><description "
+            + "descriptionType=\"Abstract\" xml:lang=\"en\">";
 
     private D1NodeServiceTest d1NodeTest;
     private HttpServletRequest request;
